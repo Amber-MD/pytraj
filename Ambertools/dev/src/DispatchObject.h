473ab64e876105f6904cccc701856fff95751008
mdtools <nhai.qn@gmail.com>
2015-02-05 06:17:58 -0500

    add cpptraj-dev version, 02-05-2015

--- Ambertools/dev/src/DispatchObject.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_DISPATCHOBJECT_H
-#define INC_DISPATCHOBJECT_H
-/// Abstract base class that all dispatchable objects will inherit.
-class DispatchObject {
-  public:
-    /// Function pointer for allocating a DispatchObject. 
-    typedef DispatchObject* (*DispatchAllocatorType)();
-};
-#endif
