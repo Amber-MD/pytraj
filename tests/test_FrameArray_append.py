d9b7fbf355987d7ac661c72e603a8482a24cc1ee
mdtools <nhai.qn@gmail.com>
2015-02-05 20:33:51 -0500

    update example

--- tests/test_FrameArray_append.py
+++ tests/test_FrameArray_append.py
@@ -1,20 +1,57 @@
 import unittest
+from pytraj._utils import _tease_FrameArray
+from time import time
 from pytraj.base import *
 from pytraj import io as mdio
 from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj import adict
 
 class Test(unittest.TestCase):
     def test_0(self):
         traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         farray = FrameArray()
+
+        t0 =  time()
         for i in range(1000):
             for frame in traj:
                 farray.append(frame, copy=False)
 
+        print (time() - t0)
         print (farray)
-
         farray.join(farray)
         print (farray)
+
+    def test_1(self):
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        farray = FrameArray()
+        farray.top = traj.top.copy()
+        t0 = time()
+        _tease_FrameArray(farray, traj[:], 10000)
+        print (time()-t0)
+        print (farray)
+
+        farray2 = FrameArray()
+        farray2.top = traj.top.copy()
+        t0 = time()
+        for frame in farray:
+            farray2.append(frame, copy=False)
+        print (time()-t0)
+
+        t0 = time()
+        for frame in farray:
+            farray2.append(frame, copy=True)
+        print (time()-t0)
+        print (farray2)
+
+        # try doing action
+        farray3 = farray2[:1000]
+        farray3.join((farray3[:], farray3[:], farray3[:]))
+        print (farray3)
+        t0 = time()
+        d0 = adict['distance'](":2@CA :10@CA", farray3,
+                               farray3.top, quick_get=True)
+        print (time()-t0)
+        print (d0.size)
 
 if __name__ == "__main__":
     unittest.main()
