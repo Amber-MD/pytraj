9e87b40d34bc6844c51e93c9950177c6950a42f7
mdtools <nhai.qn@gmail.com>
2015-02-12 18:00:37 -0500

    update common_actions.py and fix example

--- pytraj/common_actions.py
+++ pytraj/common_actions.py
@@ -1,9 +1,12 @@
 """having common actions such as rmsd, fitting, ...
 >>> from pytraj.common_actions import calc_rmsd, do_fitting
 >>> from pytraj.common_actions import translate
+# TODO : use __all__
 """
 from functools import partial
 from pytraj import adict
+from pytraj.Topology import Topology
+from pytraj.DataSetList import DataSetList
 from pytraj.misc import calculate, calc_dssp
 from pytraj.DistRoutines import distance 
 from pytraj.gdt.calc_score import calc_score
@@ -16,5 +19,23 @@ calc_angle = partial(calculate, 'angle')
 calc_molsurf = partial(calculate, 'molsurf')
 calc_molsurf = partial(calculate, 'molsurf')
 calc_distrmsd = partial(calculate, 'distrmsd')
-calc_watershell = partial(calculate, 'watershell')
 calc_protein_score = calc_score
+
+def calc_watershell(command, traj, top=Topology()):
+    """return a DataSetList object having the number of water 
+    in 1st and 2nd water shell for each frame
+    >>> d0 = calc_watershell(":WAT", traj)
+    >>> # get 1st shell
+    >>> d0_0 = d0[0]
+    >>> print (d0_0[:])
+    >>> # get 2nd shell
+    >>> d0_1 = d0[1]
+    >>> print (d0_1[:])
+    """
+    if 'out' not in command:
+        # current Watershell action require specifying output
+        # 
+        command += ' out .tmp'
+    dslist = DataSetList()
+    adict['watershell'](command, traj, top, dslist=dslist)
+    return dslist
--- tests/test_watershell.py
+++ tests/test_watershell.py
@@ -18,5 +18,17 @@ class Test(unittest.TestCase):
             print (dslist[0][:])
             print (dslist[1][:])
 
+    def test_0(self):
+        path = "../NOGIT/CpptrajTest"
+        if os.path.exists("../NOGIT/CpptrajTest"):
+            print (path)
+            traj = mdio.load("../NOGIT/CpptrajTest/tz2.truncoct.nc", 
+                             "../NOGIT/CpptrajTest/tz2.truncoct.parm7")
+            from pytraj.common_actions import calc_watershell
+
+            d0 = calc_watershell('!:WAT', traj)
+            print (d0[0][:])
+            print (d0[1][:])
+
 if __name__ == "__main__":
     unittest.main()
