0377c61e1448c36eb789f610b5f85d2e7fb4c8cc
mdtools <nhai.qn@gmail.com>
2015-02-16 20:50:09 -0500

    dont allow inherit FrameArray from cython to avoid memory error

---
+++ tests/test_FrameArray_inherit.py
@@ -0,0 +1,16 @@
+import unittest
+from pytraj.base import *
+from pytraj import io as mdio
+from pytraj.utils.check_and_assert import assert_almost_equal
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        def test_class(self):
+            class FA(FrameArray):
+                pass
+            fa = FA("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+            print (fa)
+        self.assertRaises(TypeError, lambda: test_class())
+
+if __name__ == "__main__":
+    unittest.main()
