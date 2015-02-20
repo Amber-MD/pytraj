35705f2b047a4c557f1692d348c153bf2873d132
mdtools <nhai.qn@gmail.com>
2015-02-20 17:46:29 -0500

    add test_Action_Hbond: not sure how to interpret

--- tests/test_Action_Hbond.py
+++ tests/test_Action_Hbond.py
@@ -1,4 +1,4 @@
-import unitest
+import unittest
 from pytraj.base import *
 from pytraj import adict
 from pytraj import io as mdio
@@ -7,12 +7,16 @@ from pytraj.utils.check_and_assert import assert_almost_equal
 class Test(unittest.TestCase):
     def test_0(self):
         # TODO : add assert
-        traj = mdio.load("./data/tz2.ortho.nc", "./data/tz2.ortho.parm7")
-        act = adict['areapermol']
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        act = adict['hbond']
         dslist = DataSetList()
-        act("", traj, dslist=dslist)
-        print (dslist.size)
-        print (dslist[0][:])
+        act("series", traj, dslist=dslist)
+        print ('dslist size = ', dslist.size)
+        for d0 in dslist:
+            print (d0[:])
+            print (d0.name)
+
+        act.help()
 
 if __name__ == "__main__":
     unittest.main()
