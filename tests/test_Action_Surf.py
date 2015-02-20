ed18fc6770a83394dbd6ed25c5b0f8d357a7f2cb
mdtools <nhai.qn@gmail.com>
2015-02-20 18:03:34 -0500

    updat test_Surf

--- tests/test_Action_Surf.py
+++ tests/test_Action_Surf.py
@@ -1,10 +1,12 @@
 import unittest
+from pytraj.common_actions import calc_molsurf
 from pytraj.base import *
 from pytraj.actions.Action_Surf import Action_Surf
 from pytraj.actions import Action
 from pytraj.misc import strip
 from pytraj.TrajReadOnly import TrajReadOnly
 from pytraj import cast_dataset
+from pytraj import adict
 
 print(dir(Action_Surf()))
 
@@ -13,25 +15,27 @@ farray = FrameArray(top=Topology("./data/Tc5b.top"), filename='data/md1_prod.Tc5
 class TestSurf(unittest.TestCase):
     def test_0(self):
         print("newtop")
+
         farray0 = farray.copy()
         newtop = farray0.top.copy()
         oldtop = farray0.top
+
+        d0 = calc_molsurf("@CA", farray[0], farray.top)
+        print (d0[:])
+        adict['molsurf'].help()
         
         toplist = TopologyList()
         toplist.add_parm(newtop)
         dslist = DataSetList()
         dflist = DataFileList()
         
-        stripact = Action_Surf()
-        stripact.read_input("surf @CA \n name test", oldtop, dslist=dslist)
-        stripact.process(oldtop, newtop)
-
+        act = Action_Surf()
+        act.read_input("@CA", oldtop, dslist=dslist)
+        act.process(oldtop, newtop)
         frame0 = Frame(farray.top.n_atoms)
-        stripact.do_action(farray[0], frame0)
-        #d0db = cast_dataset(d0, dtype='dataset_double')
-        d0db = cast_dataset(dslist[0], dtype='general')
-        print(d0db.data)
-        print(cast_dataset)
+        act.do_action(farray[0], frame0)
+        print (dslist[0][:])
+        act.help()
 
 if __name__ == "__main__":
     unittest.main()
