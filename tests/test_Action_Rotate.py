54bff7c001e4bf76567d6de9f0b8b09ecb4ba805
mdtools <nhai.qn@gmail.com>
2015-02-21 02:26:46 -0500

    update test_Action_Rotate: timing

--- tests/test_Action_Rotate.py
+++ tests/test_Action_Rotate.py
@@ -1,8 +1,10 @@
 import unittest
+import os
 from pytraj.base import *
 from pytraj import adict
 from pytraj import io as mdio
 from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj.common_actions import do_rotation
 
 class Test(unittest.TestCase):
     def test_0(self):
@@ -52,10 +54,56 @@ class Test(unittest.TestCase):
         from pytraj.common_actions import do_rotation
         # make mutable FrameArray object from TrajReadOnly object
         farray = traj[:]
+        farray_cp1 = farray.copy()
+        farray_cp2 = farray.copy()
         # perform rotation
         do_rotation("@CA x 60 y 120 z 50", farray)
         # assert
         assert farray[0].rmsd(fsaved) < 1E-3
+
+        # test timing
+        from pytraj.utils.Timer import Timer
+        with Timer() as t1:
+            do_rotation("@CA x 60 y 120 z 50", farray_cp1)
+
+        with Timer() as t2:
+            for frame in farray_cp2:
+                frame.rotate(60, 120, 50, traj.top('@CA'))
+
+        # make sure get the same result
+        for f1, f2 in zip(farray_cp1, farray_cp2):
+            assert f1.rmsd(f2) < 1E-3
+
+        print ("time t1 = %s (s), t2 = %s (s) " %(t1.time_gap, t2.time_gap))
+
+    def test_2(self):
+        if os.path.exists("./data/NuG2/test.x.000"):
+            # do local test (not in travis) due to big file
+            print ("test much bigger file")
+            # test timing
+            from pytraj.utils.Timer import Timer
+
+            traj = mdio.load("./data/NuG2/test.x.000", "./data/NuG2/NuG2.top")
+            farray = traj[:]
+            farray_cp1 = farray.copy()
+            farray_cp2 = farray.copy()
+
+            with Timer() as t1:
+                do_rotation("@CA x 60 y 120 z 50", farray_cp1)
+
+            with Timer() as t2:
+                for frame in farray_cp2:
+                    frame.rotate(60, 120, 50, traj.top('@CA'))
+
+            # make sure get the same result
+            for f1, f2 in zip(farray_cp1, farray_cp2):
+                assert f1.rmsd(f2) < 1E-3
+
+            tgap1 = t1.time_gap
+            tgap2 = t2.time_gap
+            print ("time for do_rotation: t1 = %s (s), frame.rotate: t2 = %s (s) " 
+                  %(t1.time_gap, t2.time_gap))
+            print ("speed up using do_rotation vs frame.rotate = %s" % (tgap2/tgap1))
 
 if __name__ == "__main__":
     unittest.main()
