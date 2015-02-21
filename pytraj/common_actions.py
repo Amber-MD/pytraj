ee887c05926980dcf1b95db87acfc6298b0b139f
mdtools <nhai.qn@gmail.com>
2015-02-21 02:12:43 -0500

    test Action_Rotate: pass

--- pytraj/common_actions.py
+++ pytraj/common_actions.py
@@ -11,17 +11,24 @@ from pytraj.misc import calculate, calc_dssp
 from pytraj.DistRoutines import distance 
 from pytraj.gdt.calc_score import calc_score
 
+list_of_cal = ['calc_distance', 'calc_dih', 'calc_dihedral', 'calc_radgyr', 'calc_angle',
+           'calc_molsurf', 'calc_distrmsd', 'calc_protein_score', 'calc_watershell']
+
+list_of_do = ['do_translation', 'do_fitting', 'do_rotation']
+
+__all__ = list_of_do + list_of_cal
+
 calc_distance = partial(calculate, 'distance')
 calc_dih = partial(calculate, 'dihedral')
 calc_dihedral = calc_dih
 calc_radgyr = partial(calculate, 'radgyr')
 calc_angle = partial(calculate, 'angle')
 calc_molsurf = partial(calculate, 'molsurf')
-calc_molsurf = partial(calculate, 'molsurf')
 calc_distrmsd = partial(calculate, 'distrmsd')
 calc_protein_score = calc_score
 
 do_translation = partial(calculate, 'translate')
+do_rotation = partial(calculate, 'rotate')
 
 def calc_watershell(command, traj, top=Topology()):
     """return a DataSetList object having the number of water 
@@ -41,3 +48,9 @@ def calc_watershell(command, traj, top=Topology()):
     dslist = DataSetList()
     adict['watershell'](command, traj, top, dslist=dslist)
     return dslist
+
+def do_translation(command="", traj=None, top=Topology()):
+    adict['translate'](command, traj, top)
+
+def do_rotation(command="", traj=None, top=Topology()):
+    adict['rotate'](command, traj, top)
--- tests/test_Action_Rotate.py
+++ tests/test_Action_Rotate.py
@@ -1,20 +1,61 @@
 import unittest
-from pytraj import adict
 from pytraj.base import *
+from pytraj import adict
 from pytraj import io as mdio
 from pytraj.utils.check_and_assert import assert_almost_equal
 
 class Test(unittest.TestCase):
     def test_0(self):
-        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")[:]
-        traj2 = traj.copy()
-        print (traj['@CA'][0, 0])
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        act = adict['rotate']
+        f0 = traj[0]
+        f1 = traj[1]
+        f0cp = f0.copy()
+        f0saved = f0.copy()
+        f0cp2 = f0.copy()
+        f1cp = f1.copy()
+        print (f0cp.rmsd(f0))
+        print (f1cp.rmsd(f1))
+        # rotate all CA atoms (I know it's very dummy test)
+        act("@CA x 60 y 120 z 50", f0, traj.top)
+        act.do_action(f1)
+        print (f0cp.rmsd(f0))
+        print (f1cp.rmsd(f1))
 
-        # test action without expliti specifying Topology
-        adict['rotate']('@CA x 60 y 60 z 60', traj)
-        print (traj['@CA'][0, 0])
-        adict['rotate']('@CA x 60 y 60 z 60', traj2, traj2.top)
-        assert_almost_equal(traj2['@CA'][0, 0], traj['@CA'][0, 0])
+        f0cp2.rotate(60, 120, 50, traj.top('@CA'))
+        print ("f0cp2.rmsd(f0saved) = ", f0cp2.rmsd(f0saved))
+
+        # do more
+        # create mutable FrameArray
+        farray = traj[:]
+        # perform action on farray
+        act.do_action(farray)
+
+        for f1, f2 in zip(farray, traj):
+            print (f1.rmsd(f2))
+
+    def test_1(self):
+        print ("test 1: assert")
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        f0 = traj[0]
+        f0cp = f0.copy()
+        act = adict['rotate']
+        f0.rotate(60, 120, 50, traj.top('@CA'))
+        act("@CA x 60 y 120 z 50", f0cp, traj.top)
+        fsaved = mdio.load("./data/rotated_frame0.x60y120z50.Tc5b.r", "./data/Tc5b.top")[0]
+        print (f0.rmsd(fsaved))
+        print (f0cp.rmsd(fsaved))
+        assert f0.rmsd(fsaved) < 1E-3
+        assert f0cp.rmsd(fsaved) < 1E-3
+
+        # do_rotation
+        from pytraj.common_actions import do_rotation
+        # make mutable FrameArray object from TrajReadOnly object
+        farray = traj[:]
+        # perform rotation
+        do_rotation("@CA x 60 y 120 z 50", farray)
+        # assert
+        assert farray[0].rmsd(fsaved) < 1E-3
 
 if __name__ == "__main__":
     unittest.main()
