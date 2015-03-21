8f1180b3980c12f24587b33a28f50f38d73c716a
Hai Nguyen <hainm.comp@gmail.com>
2015-03-21 03:29:49 -0400

    pytraj: update test_Action_Rmsd_with_reference.py

--- tests/test_Action_Rmsd_with_reference.py
+++ tests/test_Action_Rmsd_with_reference.py
@@ -1,6 +1,7 @@
 # turn off unittest to pass travis
 # still need to fix this
-#import unittest
+import unittest
+import numpy as np
 from pytraj.base import *
 from pytraj import adict
 from pytraj import io as mdio
@@ -11,8 +12,16 @@ class Test(unittest.TestCase):
         traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         act = adict['rmsd']
         dslist = DataSetList()
-        # FIXME: segmentation fault
-        act("parm ./data/Tc5b.top reference ./data/Tc5b.crd rms reference @CA", traj, dslist=dslist)
+        act(":3-18@CA reftraj ./data/Tc5b.crd ./data/Tc5b.top", traj, dslist=dslist)
+        d0 = dslist[0]
+        print (d0[:])
+        cpptraj_rmsd = np.loadtxt("./data/rmsd_CA_to_nat_res3_18.dat",
+                                  skiprows=1).transpose()[1]
+        first10_rmsd = cpptraj_rmsd[:10]
+        print(first10_rmsd)
+
+        # make sure to reprofuce cpptraj
+        assert_almost_equal(d0[:], first10_rmsd)
 
 if __name__ == "__main__":
     unittest.main()
