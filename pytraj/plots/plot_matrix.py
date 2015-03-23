09d37bc0ed7edaf908d82ec643dcc858140c351e
Hai Nguyen <hainm.comp@gmail.com>
2015-03-23 18:25:45 -0400

    pytraj: update plot_matrix.py

--- pytraj/plots/plot_matrix.py
+++ pytraj/plots/plot_matrix.py
@@ -3,17 +3,20 @@ from pytraj.datasets.DataSet_MatrixDbl import DataSet_MatrixDbl
 from .base import plt
 import numpy as np
 
-def plot_matrix(dset):
+def plot_matrix(dset, interpolation='nearest'):
     """plot matrix and return pyplot object
     Parameters
     ---------
     dset: {DataSet_MatrixDbl, ...}
     """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
     if isinstance(dset, DataSet_MatrixDbl):
         # get matrix data
         # need to reshape since dset stores data in 1D
         mat = np.asarray(dset.get_full_matrix()).reshape(dset.n_rows, dset.n_cols)
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.matshow(mat)
-        return ax
+    if isinstance(dset, np.ndarray) and len(dset.shape) == 2:
+        mat = dset
+    ax.matshow(mat, interpolation=interpolation)
+    return ax
