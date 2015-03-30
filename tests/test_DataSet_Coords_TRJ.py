87682a34a49fa2beaf0919a22e2e67f0f174eacf
Hai Nguyen <hainm.comp@gmail.com>
2015-03-30 19:50:35 -0400

    PYTRAJ: update test_DataSet_Coords_TRJ.py test

--- tests/test_DataSet_Coords_TRJ.py
+++ tests/test_DataSet_Coords_TRJ.py
@@ -1,76 +1,59 @@
 import unittest
 from pytraj.base import *
 from pytraj.decorators import no_test
-from pytraj.datasets.DataSet_Coords_TRJ import DataSet_Coords_TRJ as Trajectory
+from pytraj.datasets.DataSet_Coords_TRJ import DataSet_Coords_TRJ
 from pytraj.datasets.DataSet_Coords_TRJ import DataSet_Coords_TRJ
 from pytraj.utils import assert_almost_equal
 
 class Test(unittest.TestCase):
-    @no_test
     def test_0(self):
         TRAJ = TrajReadOnly(filename="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top")
         print(TRAJ.size)
+        Nframe = TRAJ.size
 
         # creat DataSet_Coords_TRJ object
         # man, should we use FrameArray?
         # might not be a big matter since adding frames are not that expensive vs clustering
-        traj = Trajectory()
-        traj.top = TRAJ.top.copy()
+        coords_traj = DataSet_Coords_TRJ()
+        coords_traj.top = TRAJ.top.copy()
+        coords_traj.add_trajin(TRAJ)
 
         # DataSet_Coords_TRJ does not have GetFrame method
         # what should I do?
-        Nframe = 10
-        for frame in TRAJ:
-            traj.append(frame)
-
-        # seriously I need to set size for DataSet_Coords_TRJ?
-        traj.size = TRAJ.size
-        assert traj.size == Nframe
-        for f0 in traj:
-            print (f0)
-        #print (traj[0])
-        #frame = Frame()
-        #print traj.get_frame(3, frame, traj.top)
-        #print (frame)
-
-        ## test negative indexing
-        #assert traj[-1][0] == traj[Nframe-1][0]
-        #assert traj[-2][0] == traj[Nframe-2][0]
-
-        ## test set Frame
-        #traj[0] = traj[9]
-        #assert traj[0][0] == traj[9][0]
+        assert coords_traj.size == TRAJ.n_frames
+        for frame in coords_traj:
+            print (frame)
 
     def test_1(self):
-        traj = DataSet_Coords_TRJ()
-        traj.top = Topology("./data/Tc5b.top")
-        traj.load("./data/md1_prod.Tc5b.x")
-        assert traj.size == 10
-        traj.load("./data/md1_prod.Tc5b.x", arg="1 5 2")
-        assert traj.size == 13
+        coords_traj = DataSet_Coords_TRJ()
+        coords_traj.top = Topology("./data/Tc5b.top")
+        coords_traj.load("./data/md1_prod.Tc5b.x")
+        assert coords_traj.size == 10
+        coords_traj.load("./data/md1_prod.Tc5b.x", arg="1 5 2")
+        assert coords_traj.size == 13
 
-        for f0 in traj:
+        for f0 in coords_traj:
             print (f0)
 
         print ("getitem")
-        print (traj[12])
+        print (coords_traj[12])
 
-        print (traj)
-        ds0 = traj.alloc()
+        print (coords_traj)
+        ds0 = coords_traj.alloc()
         print (ds0)
         print (dir(ds0))
 
         # try perform action
         from pytraj.common_actions import calc_distance
-        d0 = calc_distance(":2@CA :10@CA", traj)
+        d0 = calc_distance(":2@CA :10@CA", coords_traj)
         print (d0.size)
         print (d0[:])
 
         # make sure we load correct frames
-        assert_almost_equal(traj[0].coords, traj[10].coords)
+        assert_almost_equal(coords_traj[0].coords, coords_traj[10].coords)
         assert d0[0] == d0[10]
 
-        print (traj.dtype)
+        print (coords_traj.dtype)
         
 if __name__ == "__main__":
     unittest.main()
