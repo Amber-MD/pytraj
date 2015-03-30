a0a36e472b2119c79d2fb343566cb3216878664f
Hai Nguyen <hainm.comp@gmail.com>
2015-03-30 07:41:13 -0400

    PYTRAJ2: @Dan: add test_DataSetList_add_set_question.py
    Got weird result. What I am supposed to get when calling
    AddSet(DataSet*) from DataSetList class?

---
+++ tests/test_DataSetList_add_set_question.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+import unittest
+from pytraj.base import *
+from pytraj import adict
+from pytraj import io as mdio
+from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj.datasets.DataSet_Coords_TRJ import DataSet_Coords_TRJ
+from pytraj.datasets.DataSet_MatrixDbl import DataSet_MatrixDbl
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        dset_traj = DataSet_Coords_TRJ()
+        dslist = DataSetList()
+
+        # wrapper of "AddSet(DataSet*)
+        dslist.add_existing_set(dset_traj)
+        print (dslist.size) # = 0, but I expected "=1"
+
+    def test_1(self):
+        dset_traj = DataSet_MatrixDbl()
+        dslist = DataSetList()
+
+        # wrapper of "AddSet(DataSet*)
+        dslist.add_existing_set(dset_traj)
+        print (dslist.size) # = 0, but I expected "=1"
+
+    def test_2(self):
+        dslist = DataSetList()
+              
+        # wrapper of "AddSet(DataType, name, default_name)"
+        dslist.add_set("coords", "name", "funny_name")
+        print (dslist[0])
+        print (dslist.size) # = 1 (is what I expected)
+              
+if __name__ == "__main__":
+    unittest.main()
+
