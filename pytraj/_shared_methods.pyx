553baf26f34d9fafb2adcdceb5023920998bbe68
Hai Nguyen <hainm.comp@gmail.com>
2015-03-30 19:48:36 -0400

    PYTRAJ: add _frame_iter ot _shared_methods.pyx

--- pytraj/_shared_methods.pyx
+++ pytraj/_shared_methods.pyx
@@ -1,6 +1,8 @@
 # distutils: language = c++
-
-"shared methods betwween FrameArray and Trajin classes"""
+#
+cimport cython
+from pytraj.Frame cimport _Frame, Frame
+from pytraj.AtomMask cimport AtomMask
 from pytraj.trajs.Trajout import Trajout
 from pytraj.six_2 import set
 
@@ -18,3 +20,36 @@ def _savetraj(self, filename="", fmt='unknown', overwrite=False):
 
 def _get_temperature_set(self):
     return set(self.temperatures) 
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.infer_types(True)
+def _frame_iter(self, int start=0, int stop=-1, int stride=1, mask=None):
+    """iterately get Frames with start, stop, stride 
+    Parameters
+    ---------
+    start : int (default = 0)
+    stop : int (default = max_frames - 1)
+    """
+    cdef int i
+    cdef Frame frame = Frame(self.n_atoms)
+    cdef Frame frame2
+    cdef AtomMask atm
+    cdef int _end
+
+    if stop == -1:
+        _end = <int> self.n_frames
+    else:
+        _end = stop + 1
+
+    i = start
+    while i < _end:
+        frame = self[i]
+        if mask is not None:
+            atm = self.top(mask)
+            frame2 = Frame(atm.n_atoms)
+            frame2.thisptr.SetCoordinates(frame.thisptr[0], atm.thisptr[0])
+            yield frame2
+        else:
+            yield frame
+            i += stride
