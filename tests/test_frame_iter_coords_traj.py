9dd3e694bd33d46aca8c8faad867f278b1d420f5
Hai Nguyen <hainm.comp@gmail.com>
2015-03-30 19:51:35 -0400

    PYTRAJ: add test_frame_iter_coords_traj.py for testing

---
+++ tests/test_frame_iter_coords_traj.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import unittest
+from pytraj.base import *
+from pytraj import adict, cast_dataset
+from pytraj import io as mdio
+from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj.six_2 import izip
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        print ("coords")
+        dslist = DataSetList()
+        coords = cast_dataset(dslist.add_set("coords", "my_coords", ""), dtype='coords')
+        trajin_and_top = ("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        traj = mdio.load(*trajin_and_top)
+        coords.load(*trajin_and_top)
+        assert coords.size == traj.size
+
+        for f0, f1 in izip(coords(2, 8, 1), traj(2, 8, 1)):
+            print (f0[0, :], f1[0, :])
+            assert_almost_equal(f0.coords, f1.coords)
+
+        for f0, f1 in izip(coords(), traj()):
+            print (f0[0, :], f1[0, :])
+            assert_almost_equal(f0.coords, f1.coords)
+
+    def test_1(self):
+        print ("coords_trj")
+        dslist = DataSetList()
+        dslist.add_set("traj", "my_coords", "")
+        trajin_and_top = ("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        traj = mdio.load(*trajin_and_top)
+        coords = dslist[0]
+        coords.load(*trajin_and_top)
+        print (coords.top)
+        print (coords)
+        assert coords.size == traj.size
+
+        for i in range(coords.size):
+            print (coords[i])
+
+        for f0, f1 in izip(coords(2, 8, 1), traj(2, 8, 1)):
+            print (f0[0, :], f1[0, :])
+            assert_almost_equal(f0.coords, f1.coords)
+
+        for f0, f1 in izip(coords(), traj()):
+            print (f0[0, :], f1[0, :])
+            assert_almost_equal(f0.coords, f1.coords)
+
+if __name__ == "__main__":
+    unittest.main()
