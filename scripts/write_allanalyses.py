74cb1616072bd017c1bb203e10707f7eb6af7d27
Hai Nguyen <hainm.comp@gmail.com>
2015-03-30 18:05:46 -0400

    PYTRAJ: add write_allanalyses for myself

---
+++ scripts/write_allanalyses.py
@@ -0,0 +1,10 @@
+from glob import glob
+
+pyx_files = glob("*.pyx")
+pyx_files.remove("__init__.pyx")
+pyx_files.remove("Analysis.pyx")
+
+for pyx in sorted(pyx_files):
+    pyx = pyx.replace(".pyx", "")
+    txt = "from pytraj.analyses.%s import %s" % (pyx, pyx)
+    print (txt)
