116f1dbd3947973a15624859fc293c5408739403
Hai Nguyen <hainm.comp@gmail.com>
2015-03-30 19:49:58 -0400

    PYTRAJ: add DataSet_Coords_TRJ to pytraj_ipython.py for my testing

--- tests/pytraj_ipython.py
+++ tests/pytraj_ipython.py
@@ -1,5 +1,6 @@
 from pytraj import *
 from pytraj.common_actions import *
+from pytraj.datasets import DataSet_Coords_TRJ
 
 traj = FrameArray(filename="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top")
 traj2 = io.load('./data/DPDP.nc', "./data/DPDP.parm7")
@@ -9,3 +10,7 @@ frame0 = traj[0]
 act = adict['matrix']
 dslist = DataSetList()
 act("byres @CA", traj, dslist=dslist)
+
+coords_traj = DataSet_Coords_TRJ()
+coords_traj.top = traj2.top
+coords_traj.add_trajin(traj2)
