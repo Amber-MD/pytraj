d1dff39a9cda1b4a9a0846dae368ae71d0054acf
Hai Nguyen <hainm.comp@gmail.com>
2015-03-31 12:08:14 -0400

    PYTRAJ: add utils/amber_test.py to look cpptraj test folder

---
+++ pytraj/utils/amber_test.py
@@ -0,0 +1,11 @@
+import os
+
+try:
+    amberhome = os.environ['AMBERHOME']
+    has_amberhome = True
+    cpptraj_test_dir = amberhome + "/AmberTools/test/cpptraj/"
+except EnvironmentError:
+    amberhome = None
+    has_amberhome = False
+    cpptraj_test_dir = None
+
