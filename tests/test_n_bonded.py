468bb05c03cc0f21048be73d52d83dffb20e8a79
Hai Nguyen <hainm.comp@gmail.com>
2015-04-04 19:31:53 -0400

    pytraj: add test_n_bonded.py test

---
+++ tests/test_n_bonded.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import unittest
+from pytraj.base import *
+from pytraj import adict
+from pytraj import io as mdio
+from pytraj.utils.check_and_assert import assert_almost_equal
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        top = traj.top
+
+        print (top.indices_n_bonded_to("H"))
+
+if __name__ == "__main__":
+    unittest.main()
