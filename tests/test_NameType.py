c35f24f84c0c11955d6cc0595c1fbda74682584d
Hai Nguyen <hainm.comp@gmail.com>
2015-04-04 18:55:14 -0400

    PYTRAJ: add decode in NameType to convert to python string

--- tests/test_NameType.py
+++ tests/test_NameType.py
@@ -1,21 +1,16 @@
+from __future__ import print_function
 import unittest
 from pytraj.base import *
+from pytraj import adict
 from pytraj import io as mdio
-from pytraj.NameType import NameType
+from pytraj.utils.check_and_assert import assert_almost_equal
 
-class TestNameType(unittest.TestCase):
+class Test(unittest.TestCase):
     def test_0(self):
-        nt = NameType("CA")
-        print(dir(nt))
-        print(nt[2])
-
-        nt2 = nt.copy()
-        assert (nt2 == nt) == True
-        assert nt.match(nt2) == True
-        assert nt2 == 'CA'
-        print(nt.truncated())
-
-        assert nt == 'CA'
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        top = traj.top
+        for atom in top.atomlist:
+            print (atom.type)
 
 if __name__ == "__main__":
     unittest.main()
