f0bdd524827ab721f81f8eba9bef56291a47c319
Hai Nguyen <hainm.comp@gmail.com>
2015-04-06 20:42:22 -0400

    PYTRAJ: add decorators in template_unittest.py

--- tests/template_unittest.py
+++ tests/template_unittest.py
@@ -4,6 +4,7 @@ from pytraj.base import *
 from pytraj import adict
 from pytraj import io as mdio
 from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj.decorators import no_test, test_if_having
 
 class Test(unittest.TestCase):
     def test_0(self):
