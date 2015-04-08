4206f92ea25ad9dd573b17d049ce2604b93a7207
Hai Nguyen <hainm.comp@gmail.com>
2015-04-08 01:40:42 -0400

    pytraj: add test_calc_matrix.py: still need assert

---
+++ tests/test_calc_matrix.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+import unittest
+from pytraj.base import *
+from pytraj import adict
+from pytraj import io as mdio
+from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj.decorators import no_test, test_if_having
+from pytraj.utils.check_and_assert import is_word_in_class_name
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        from pytraj.common_actions import calc_matrix
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        d0 = calc_matrix("@CA", traj)
+        assert is_word_in_class_name(d0, 'DataSetList') == True
+        print (d0)
+
+if __name__ == "__main__":
+    unittest.main()
