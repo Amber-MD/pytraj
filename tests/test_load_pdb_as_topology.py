0f51b3732deb11cc4021c55660d9f2141f3c5b65
Hai Nguyen <hainm.comp@gmail.com>
2015-04-15 14:42:46 -0400

    PYTRAJ: SupressErrorMsg for pytraj/cpptraj
    update what Dan did. Work now. thanks.

--- tests/test_load_pdb_as_topology.py
+++ tests/test_load_pdb_as_topology.py
@@ -4,18 +4,23 @@ from pytraj.base import *
 from pytraj import adict
 from pytraj import io as mdio
 from pytraj.utils.check_and_assert import assert_almost_equal
-from pytraj import set_world_silent
+from pytraj import set_world_silent, set_error_silent
 
 class Test(unittest.TestCase):
     def test_0(self):
         print ("turn-on set_world_silent")
         # just want to test for not printing out cpptraj warning
         top = Topology("./data/saxs_test/test.pdb")
+        print ("you should not see anything from cpptraj before")
 
     def test_1(self):
+        print ()
+        print ()
+        print ()
         print ("turn-off set_world_silent")
         set_world_silent(False)
-        # just want to test for not printing out cpptraj warning
+        set_error_silent(False)
+        print ("you should see a bunch of cpptraj's messages after this line")
         top = Topology("./data/saxs_test/test.pdb")
 
 if __name__ == "__main__":
