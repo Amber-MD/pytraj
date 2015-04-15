16c7b062ffe9c772c01cb36c951138bd4d986a00
Hai Nguyen <hainm.comp@gmail.com>
2015-04-15 12:09:40 -0400

    PYTRAJ: simplify _load_mdtraj.py

--- pytraj/_load_mdtraj.py
+++ pytraj/_load_mdtraj.py
@@ -1,15 +1,9 @@
 """Load mdtraj traj object
-
 """
-from pytraj.utils import has_
-from pytraj import FrameArray, Topology, Atom, Frame
-
-# not sure if we need this `load_mdtraj` since cpptraj can do anything :D
-# might need to move to Cython level for faster loading
-
-if not has_("mdtraj") and not has_("numpy"):
-    # mdtraj need numpy
-    print ("need to have mdtraj and numpy")
+from __future__ import absolute_import
+from .utils import has_, require
+from .FrameArray import FrameArray
+from ._load_pseudo_parm import load_pseudo_parm
 
 def load_mdtraj(m_traj):
     """load_mdtraj traj object
@@ -18,20 +12,13 @@ def load_mdtraj(m_traj):
     ---------
     m_traj : Trajectory object from mdtraj 
     """
-    farray = FrameArray()
-
-    # convert to pseudo-topology
-    # to fully use Topology object in pytraj, we can do:
-    # >>> farray.top = Topology(top_name) # or
-    # >>> from pytraj import io
-    # >>> farray.top = io.load(top_name) 
-
-    pseudotop = Topology()
-    for m_atom in m_traj.top.atoms:
-        atom = Atom(m_atom.name, m_atom.name)
-        m_res = m_atom.residue
-        pseudotop.add_atom(atom=atom, resid=m_res.index, resname=m_res.name)
-
-    # load coords
-    farray = FrameArray(m_traj.xyz, pseudotop)
-    return farray
+    if not has_("mdtraj"):
+        # we dont need checking `numpy` since mdtraj needs numpy 
+        require("mdtraj")
+    else:
+        from mdtraj import Trajectory
+        if not isinstance(m_traj, Trajectory):
+            raise PyTrajRequireObject("Trajectory")
+        else:
+            pseudotop = load_pseudo_parm(m_traj.top)
+            return FrameArray(m_traj.xyz, pseudotop)
