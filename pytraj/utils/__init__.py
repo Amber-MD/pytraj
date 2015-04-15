0a21d25e43f86ceffc4abdc07dc15893e6458b05
Hai Nguyen <hainm.comp@gmail.com>
2015-04-15 11:51:04 -0400

    PYTRAJ: add `require` to utils namespace

--- pytraj/utils/__init__.py
+++ pytraj/utils/__init__.py
@@ -1,7 +1,7 @@
 """"""
 from __future__ import absolute_import
 from .check_and_assert import assert_almost_equal, file_exist, is_generator
-from .check_and_assert import _import, _import_numpy, is_int
+from .check_and_assert import _import, _import_numpy, is_int, require
 from .check_and_assert import has_
 from .list_misc import flatten_list
 from .Timer import Timer
