866bec095d4fd2fdfbd962c14eb262161ce6b35c
Hai Nguyen <hainm.comp@gmail.com>
2015-04-16 20:37:38 -0400

    PYTRAJ: add decorator: test_if_path_exists
    for testing purpose. I need to use cpptraj test cases in AMBER.

--- tests/template_unittest.py
+++ tests/template_unittest.py
@@ -5,6 +5,7 @@ from pytraj import adict
 from pytraj import io as mdio
 from pytraj.utils.check_and_assert import assert_almost_equal
 from pytraj.decorators import no_test, test_if_having
+from pytraj.testing import cpptraj_test_dir
 
 class Test(unittest.TestCase):
     def test_0(self):
