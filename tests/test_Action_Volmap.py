89bf112adc97deed579cd582fd9c568adb7ad117
Hai Nguyen <hainm.comp@gmail.com>
2015-04-17 01:24:51 -0400

    PYTRAJ: play with Action_Volmap
    need to add DataSet_GridFlt to take the data from DataSetlist

---
+++ tests/test_Action_Volmap.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import unittest
+from pytraj.base import *
+from pytraj import adict
+from pytraj import io as mdio
+from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj.decorators import no_test, test_if_having, test_if_path_exists
+from pytraj.testing import cpptraj_test_dir
+
+txt="""
+parm ./data/tz2.ortho.parm7
+trajin ./data/tz2.ortho.nc 1 2
+rms first :1-13
+center :1-13 mass origin 
+volmap volmap.dx 0.5 0.5 0.5 :WAT@O buffer 2.0 centermask !:1-13 \\
+radscale 1.36
+volmap volmap2.dx 0.5 0.5 0.5 :WAT@O buffer 2.0 centermask !:1-13 \\
+radscale 1.36 peakcut 0.10 peakfile peaks.xyz
+"""
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        with open("./output/volmap.in", 'w') as f:
+            f.write(txt)
+        state = mdio.load_cpptraj_file("./output/volmap.in")
+        print (state.toplist[0])
+        state.run()
+        dslist = state.datasetlist
+        for d0 in dslist._base_dataset_iter():
+            print (d0.dtype, d0.name)
+
+if __name__ == "__main__":
+    unittest.main()
