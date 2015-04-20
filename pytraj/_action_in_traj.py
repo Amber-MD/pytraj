449f353cc6c1f16717a8c45a744c22362d7ac707
Hai Nguyen <hainm.comp@gmail.com>
2015-04-20 00:39:09 -0400

    PYTRAJ: add "calc_..." methods for FrameArray too

--- pytraj/_action_in_traj.py
+++ pytraj/_action_in_traj.py
@@ -5,40 +5,40 @@ import pytraj.common_actions as pyca
 
 class ActionInTraj(object):
     def calc_distance(self, mask=""):
-        return pyca.calc_distance(mask, self)
+        return pyca.calc_distance(mask, self).tolist()
 
     def calc_distrmsd(self, mask=""):
-        return pyca.calc_distrmsd(mask, self)
+        return pyca.calc_distrmsd(mask, self).tolist()
 
     def calc_radgyr(self, mask=""):
-        return pyca.calc_radgyr(mask, self)
+        return pyca.calc_radgyr(mask, self).tolist()
 
     def calc_angle(self, mask=""):
-        return pyca.calc_angle(mask, self)
+        return pyca.calc_angle(mask, self).tolist()
 
     def calc_matrix(self, mask=""):
-        return calculate("matrix", mask, self)
+        return pyca.calc_matrix(mask, self)
 
     def calc_dssp(self, mask="", *args, **kwd):
         return pyca.calc_dssp(mask, self, *args, **kwd)
 
     def calc_dihedral(self, mask=""):
-        return pyca.calc_dihedral(mask, self)
+        return pyca.calc_dihedral(mask, self).tolist()
 
     def calc_molsurf(self, mask=""):
-        return pyca.calc_molsurf(mask, self)
+        return pyca.calc_molsurf(mask, self).tolist()
 
     def calc_center_of_mass(self, mask=""):
-        return pyca.calc_center_of_mass(mask, self)
+        return pyca.calc_center_of_mass(mask, self).tolist()
 
     def calc_COM(self, mask=""):
-        return pyca.calc_center_of_mass(mask, self)
+        return pyca.calc_center_of_mass(mask, self).tolist()
 
     def calc_center_of_geometry(self, mask=""):
-        return pyca.calc_center_of_geometry(mask, self)
+        return pyca.calc_center_of_geometry(mask, self).tolist()
 
     def calc_COG(self, mask=""):
-        return pyca.calc_center_of_geometry(mask, self)
+        return pyca.calc_center_of_geometry(mask, self).tolist()
 
     def calc_vector(self, mask=""):
         return pyca.calc_vector(mask, self)
@@ -50,4 +50,4 @@ class ActionInTraj(object):
         return pyca.get_average_frame(mask, self)
 
     def calc_watershell(self, mask=""):
-        return pyca.calc_watershell(mask, self)
+        return pyca.calc_watershell(mask, self).tolist()
