62553b0f5a86f42accac782f744a963be08df2c6
Hai Nguyen <hainm.comp@gmail.com>
2015-04-20 02:16:47 -0400

    PYTRAJ: fix segmentation fault for calc_vector

--- pytraj/_action_in_traj.py
+++ pytraj/_action_in_traj.py
@@ -24,6 +24,9 @@ class ActionInTraj(object):
 
     def calc_dihedral(self, mask=""):
         return pyca.calc_dihedral(mask, self).tolist()
+    
+    def calc_multidihedral(self, mask=""):
+        return pyca.calc_multidihedral(mask, self)
 
     def calc_molsurf(self, mask=""):
         return pyca.calc_molsurf(mask, self).tolist()
@@ -41,7 +44,17 @@ class ActionInTraj(object):
         return pyca.calc_center_of_geometry(mask, self).tolist()
 
     def calc_vector(self, mask=""):
-        return pyca.calc_vector(mask, self)
+        from pytraj.actions.Action_Vector import Action_Vector
+        from pytraj.DataSetList import DataSetList
+        act = Action_Vector()
+        dslist = DataSetList()
+
+        if 'name' not in mask:
+            # for some reasons, I got segmentation fault without 'name' keyword
+            # need to check cpptraj code
+            mask = "name myvector " + mask
+        act(mask, self, dslist=dslist)
+        return [d0.tolist() for d0 in dslist]
 
     def search_hbonds(self, mask="*"):
         return pyca.search_hbonds(self, mask)
--- tests/test_calc_vector.py
+++ tests/test_calc_vector.py
@@ -8,14 +8,12 @@ from pytraj.decorators import no_test, test_if_having, test_if_path_exists
 from pytraj.testing import cpptraj_test_dir
 
 class Test(unittest.TestCase):
-    @test_if_having("mdtraj")
     def test_0(self):
         import pytraj.common_actions as ca
-        import mdtraj as md
         traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
-        m_top = md.load_prmtop("./data/Tc5b.top")
-        m_traj = md.load_mdcrd("./data/md1_prod.Tc5b.x", m_top)
         v0 = ca.calc_vector("@CA @N,C,O mass", traj)
+        print (v0.tolist())
+        v1 = traj.calc_vector("@CA @N,C,O mass")
 
 if __name__ == "__main__":
     unittest.main()
