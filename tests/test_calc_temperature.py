f96154d6c5aca38488947413e6697b17af479628
Hai Nguyen <hainm.comp@gmail.com>
2015-04-22 18:05:32 -0400

    pytraj: minor

--- tests/test_calc_temperature.py
+++ tests/test_calc_temperature.py
@@ -7,7 +7,12 @@ class Test(unittest.TestCase):
         # is_ensemble = False
         topn = "./data/Test_RemdTraj/ala2.99sb.mbondi2.parm7"
         traj = io.load("./data/Test_RemdTraj/rem.nc.000", topn)
-        print (traj.get_temperatures())
+
+        # based on velocity
+        print (traj.calc_temperatures())
+
+        # based on existing T
+        print (traj.temperatures)
 
 if __name__ == "__main__":
     unittest.main()
