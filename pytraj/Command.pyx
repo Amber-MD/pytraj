8b667f3ca49603cc144d56068de372c5a5b7958b
Hai Nguyen <hainm.comp@gmail.com>
2015-04-24 13:27:33 -0400

    PYTRAJ: delete Command.pxd
    We don't need to share Command.pxd in Cython level.

--- pytraj/Command.pyx
+++ pytraj/Command.pyx
@@ -1,5 +1,20 @@
 # distutils: language = c++
+from __future__ import absolute_import
+from .CpptrajState cimport *
+from .ArgList cimport *
+from .DispatchObject cimport *
+from ._FunctPtr cimport FunctPtr
 
+
+cdef extern from "src/Command.h": 
+    ctypedef enum RetType "Command::RetType":
+        pass
+    cdef cppclass _Command "Command":
+        @staticmethod
+        RetType ProcessInput(_CpptrajState&, const string&)
+
+cdef class Command:
+    cdef _Command* thisptr
 
 cdef class Command:
     def __cinit__(self):
