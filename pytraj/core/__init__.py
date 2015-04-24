9f9cb7faafbb2c497e6de43e2cb6509086f1ff0c
Hai Nguyen <hainm.comp@gmail.com>
2015-04-24 17:56:21 -0400

    PYTRAJ: add "core"

---
+++ pytraj/core/__init__.py
@@ -0,0 +1,5 @@
+""""""
+from .Atom import Atom
+from .Residue import Residue
+from .Molecule import Molecule
+from .Box import Box
---
+++ pytraj/math/__init__.py
@@ -0,0 +1,5 @@
+""""""
+from __future__ import absolute_import 
+from .Vec3 import Vec3 
+from .Matrix_3x3 import Matrix_3x3
+from .Grid import Grid
