774815e40f70d9aa547ee9c9f7d79419a0bc35b9
Hai Nguyen <hainm.comp@gmail.com>
2015-04-28 21:02:45 -0400

    PYTRAJ: update test for autoconvert=Fasle for loading mdtraj

--- tests/test_load_mdtraj_0.py
+++ tests/test_load_mdtraj_0.py
@@ -7,7 +7,8 @@ from pytraj import common_actions
 from pytraj import io as mdio
 from pytraj.utils import has_
 from pytraj.misc import get_atts
-from pytraj.utils.check_and_assert import assert_almost_equal, eq
+from pytraj.utils.check_and_assert import assert_almost_equal as aa_eq
+from pytraj.utils.check_and_assert import eq
 from pytraj.six_2 import izip
 from pytraj.testing import test_if_having
 
@@ -80,6 +81,10 @@ class Test(unittest.TestCase):
         print (traj.top.box)
         print (true_traj.top.box)
         print (traj.n_atoms)
+
+        traj2 = mdio.load_mdtraj(m_traj, False)
+        aa_eq(traj2.xyz.ravel, m_traj.xyz)
+        aa_eq(10*traj.xyz.ravel, m_traj.xyz)
 
 if __name__ == "__main__":
     unittest.main()
