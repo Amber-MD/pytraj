ef2c0202cbe827c233b7e25a7115c01155b25bd9
Hai Nguyen <hainm.comp@gmail.com>
2015-05-01 23:26:07 -0400

    PYRAJ: minor changes when changing API

--- pytraj/plots/base.py
+++ pytraj/plots/base.py
@@ -3,5 +3,8 @@ from __future__ import absolute_import
 from .font_config import * # call rc('font',**font)
 from ..utils import _import
 from ..utils.check_and_assert import require
-_, plt = _import("matplotlib.pyplot")
+try:
+    from matplotlib import pyplot as plt
+except ImportError:
+    plt = None
 _, np = _import("numpy")
