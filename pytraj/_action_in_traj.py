70927b30c64edc766edd0983063542aae1a853ec
Hai Nguyen <hainm.comp@gmail.com>
2015-05-02 01:59:37 -0400

    PYTRAJ: add dtype stuff

--- pytraj/_action_in_traj.py
+++ pytraj/_action_in_traj.py
@@ -2,49 +2,53 @@
 
 from pytraj._common_actions import calculate
 import pytraj.common_actions as pyca
+from pytraj.common_actions import _get_data_from_dtype
 from pytraj.utils import is_int
 
 class ActionInTraj(object):
-    def calc_distance(self, mask=""):
-        return pyca.calc_distance(self, mask)
+    def calc_distance(self, mask="", *args, **kwd):
+        return pyca.calc_distance(self, mask, *args, **kwd)
 
-    def calc_distrmsd(self, mask=""):
-        return pyca.calc_distrmsd(self, mask)
+    def calc_distrmsd(self, mask="", *args, **kwd):
+        return pyca.calc_distrmsd(self, mask, *args, **kwd)
 
-    def calc_radgyr(self, mask=""):
-        return pyca.calc_radgyr(self, mask)
+    def calc_radgyr(self, mask="", *args, **kwd):
+        return pyca.calc_radgyr(self, mask, *args, **kwd)
 
-    def calc_angle(self, mask=""):
+    def calc_angle(self, mask="", *args, **kwd):
         return pyca.calc_angle(self, mask)
 
-    def calc_matrix(self, mask=""):
-        return pyca.calc_matrix(self, mask)
+    def calc_matrix(self, mask="", *args, **kwd):
+        return pyca.calc_matrix(self, mask, *args, **kwd)
 
     def calc_dssp(self, mask="", *args, **kwd):
         return pyca.calc_dssp(self, mask, *args, **kwd)
 
-    def calc_dihedral(self, mask=""):
-        return pyca.calc_dihedral(self, mask)
+    def calc_dihedral(self, mask="", *args, **kwd):
+        return pyca.calc_dihedral(self, mask, *args, **kwd)
     
-    def calc_multidihedral(self, mask=""):
-        return pyca.calc_multidihedral(self, mask)
+    def calc_multidihedral(self, mask="", *args, **kwd):
+        return pyca.calc_multidihedral(self, mask, *args, **kwd)
 
-    def calc_molsurf(self, mask=""):
+    def calc_molsurf(self, mask="", *args, **kwd):
         return pyca.calc_molsurf(self, mask)
 
-    def calc_center_of_mass(self, mask=""):
-        return pyca.calc_center_of_mass(self, mask)
+    def calc_center_of_mass(self, mask="", *args, **kwd):
+        return pyca.calc_center_of_mass(self, mask, *args, **kwd)
 
-    def calc_COM(self, mask=""):
-        return pyca.calc_center_of_mass(self, mask)
+    def calc_COM(self, mask="", *args, **kwd):
+        return pyca.calc_center_of_mass(self, mask, *args, **kwd)
 
-    def calc_center_of_geometry(self, mask=""):
-        return pyca.calc_center_of_geometry(self, mask)
+    def calc_center_of_geometry(self, mask="", *args, **kwd):
+        return pyca.calc_center_of_geometry(self, mask, *args, **kwd)
 
-    def calc_COG(self, mask=""):
-        return pyca.calc_center_of_geometry(self, mask)
+    def calc_COG(self, mask="", *args, **kwd):
+        return pyca.calc_center_of_geometry(self, mask, *args, **kwd)
 
-    def calc_vector(self, mask=""):
+    def calc_vector(self, mask="", dtype='dataset'):
+        """
+        dtype = {'dataset', 'list', 'ndarray'}
+        """
         from pytraj.actions.Action_Vector import Action_Vector
         from pytraj.DataSetList import DataSetList
         act = Action_Vector()
@@ -56,10 +60,12 @@ class ActionInTraj(object):
             mask = "myvector " + mask
         act(mask, self, dslist=dslist)
         dslist.set_py_free_mem(False)
-        return dslist[0]
+        d0 = dslist[0]
+        dtype = dtype.lower()
+        return _get_data_from_dtype(d0, dtype)
 
-    def calc_pairwise_rmsd(self, mask=""):
-        return pyca.calc_pairwise_rmsd(self, mask)
+    def calc_pairwise_rmsd(self, mask="", *args, **kwd):
+        return pyca.calc_pairwise_rmsd(self, mask, *args, **kwd)
 
     def calc_rmsd(self, ref=None, mask="", mass=False, fit=True):
         """"""
@@ -68,17 +74,21 @@ class ActionInTraj(object):
             ref = self[ref]
         return pyca.calc_rmsd(command=mask, traj=self, ref=ref, mass=mass, fit=fit)
 
-    def search_hbonds(self, mask="*"):
-        return pyca.search_hbonds(self, mask)
+    def search_hbonds(self, mask="*", *args, **kwd):
+        """return DataSetList
+        """
+        return pyca.search_hbonds(self, mask, *args, **kwd)
 
-    def get_average_frame(self, mask=""):
-        return pyca.get_average_frame(self, mask)
+    def get_average_frame(self, mask="", *args, **kwd):
+        """return Frame
+        """
+        return pyca.get_average_frame(self, mask, *args, **kwd)
 
     def calc_temperatures(self, mask=""):
         return pyca.calc_temperatures(self, mask)
 
-    def calc_watershell(self, mask=""):
-        return pyca.calc_watershell(self, mask)
+    def calc_watershell(self, mask="", *args, **kwd):
+        return pyca.calc_watershell(self, mask, *args, **kwd)
 
     @property
     def temperatures(self):
