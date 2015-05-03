5ec6fae063d6db253ba88677f7a8e6cd87bffa05
Hai Nguyen <hainm.comp@gmail.com>
2015-05-03 19:36:33 -0400

    PYTRAJ: trick `cpptraj` to use internal reference
    I don't know yet how to pass Frame object to cpptraj to be used
    as "reference" for some actions like Action_Rmsd, ...
    but cpptraj does use first Frame as reference for default. so this
    is ugly solution:
    
    ref = traj[100]
    act(mask, [[ref,], traj])

---
+++ tests/test_reference.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import unittest
+from pytraj.base import *
+from pytraj import adict
+from pytraj import io as mdio
+from pytraj.utils import eq, aa_eq
+from pytraj.decorators import no_test, test_if_having, test_if_path_exists
+from pytraj.testing import cpptraj_test_dir
+import pytraj.common_actions as pyca
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        # Aim: `cpptraj` use `first` frame as default for some Actions (Action_Rmsd,
+        # Action_NativeContacts, ...). We can specify reference by adding reference in
+        # 1st frame
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        ref = traj[5]
+        act = adict['rmsd']
+        mask = ":3-18@CA"
+        dslist = act(mask, [[ref,], traj], traj.top)
+        saved_rmsd = traj.calc_rmsd(5, mask)
+        aa_eq(saved_rmsd, dslist[0].tolist()[1:]) # exclude 1st value for ref (=0.0)
+
+if __name__ == "__main__":
+    unittest.main()
