165376bfa9e3cf2522b239170dd45e66bb03ebae
Hai Nguyen <hainm.comp@gmail.com>
2015-05-04 17:23:41 -0400

    PYTRAJ: update examples and doc for hbonds

--- pytraj/hbonds.py
+++ pytraj/hbonds.py
@@ -3,34 +3,62 @@ from __future__ import absolute_import, print_function
 from pytraj.action_dict import ActionDict
 from .externals.six import string_types
 from pytraj.DataSetList import DataSetList
+from pytraj._get_common_objects import _get_data_from_dtype
 
 adict = ActionDict()
 
-def search_hbonds(traj, mask="", *args, **kwd):
-    """return a DataSetList object storing all pair of h-bond
+def search_hbonds(traj, mask="", dtype='dataset', *args, **kwd):
+    """search hbonds for a given mask
     Parameters
     ----------
-    mask : str (Amber atom mask)
     traj : {Trajectory-like object, frame_iter object, list of traj}
-    top : optional, Topology file
-
+    mask : str 
+        Amber atom mask
+    dtype : str {'list', 'pyarray', 'dataset', 'ndarray'}, default='dataset'
     *args, **kwd: optional
 
     Returns
-    ------
-    DataSetList object
+    -------
+    out : DataSetList | pyarray | ndarray | list | dict (depend on 'dtype')
+
+    Examples
+    --------
+    * The syntax was adapted from http://ambermd.org/doc12/Amber15.pdf (page 575)
+    * The explaniation in " " is direct excerpt from this manual
+
+    * "search for all hydrogen bonds within residues 1-22"
+        dslist = search_hbonds(traj, ":1-22")
+        
+    * "search for all hydrogen bonds within residues 1-22, writing the number of 
+    hydrogen bonds per frame to “nhb.dat” and information on each hydrogen bond found to “avghb.dat”"
+
+        dslist = search_hbonds(traj, ":1-22 out nhb.dat avgout avghb.dat", dflist=dflist)
+        dflist.write_all_datafile()
+
+    * "search for all hydrogen bonds formed between donors in residue 1 and acceptors in residue 2" 
+
+        dslist = search_hbonds(traj, "donormask :1 acceptormask :2", dtype='ndarray'))
+   
+    See Also
+    --------
+    http://ambermd.org/doc12/Amber15.pdf (page 575)
     """
     dslist = DataSetList()
     act = adict['hbond']
     command = "series " + mask
     act(command, traj, dslist=dslist, *args, **kwd)
     act.print_output()
-    return dslist
+    return _get_data_from_dtype(dslist, dtype=dtype)
 
-def search_nointramol_hbonds(traj, mask="", *args, **kwd):
+def search_nointramol_hbonds(traj, mask="", dtype='dataset', *args, **kwd):
+    """
+    See Also
+    --------
+       search_hbonds
+    """
     dslist = DataSetList()
     act = adict['hbond']
     command = "series nointramol" + mask
     act(command, traj, dslist=dslist, *args, **kwd)
     act.print_output()
-    return dslist
+    return _get_data_from_dtype(dslist, dtype=dtype)
