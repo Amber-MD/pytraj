c40401b9ebfec810d224e0b73d138fdaf43fc4b0
Hai Nguyen <hainm.comp@gmail.com>
2015-05-07 18:26:11 -0400

    PYTRAJ: try to subclass Trajectory
    Why?
    previously I got segmentation fault when subclassing `Trajectory` class
    (which is a Cython-C++ translated class).

---
+++ tests/test_return_based_on_dtype.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+import unittest
+from pytraj.base import *
+from pytraj import adict
+from pytraj import io as mdio
+from pytraj.utils import eq, aa_eq
+from pytraj.decorators import no_test, test_if_having, test_if_path_exists
+from pytraj.testing import cpptraj_test_dir
+from pytraj.utils.check_and_assert import is_word_in_class_name as is_in_class_name
+import pytraj.common_actions as pyca
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        import numpy as np
+        traj = mdio.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        d0 = traj.calc_COG() # default = 'dataset'
+        print (d0)
+        assert is_in_class_name(d0, 'DataSet_Vector') == True
+        mylist = traj.calc_COG(dtype='list') # default = 'dataset'
+        print (mylist)
+        assert isinstance(mylist, list) == True
+        mynp = traj.calc_COG(dtype='ndarray') # default = 'dataset'
+        assert is_in_class_name(mynp, 'ndarray')
+        aa_eq(np.asarray(mylist).flatten(), mynp.flatten())
+
+if __name__ == "__main__":
+    unittest.main()
