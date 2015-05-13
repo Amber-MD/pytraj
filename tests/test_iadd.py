70065fc0d03028dd1d536e298dabd1e124b39e15
Hai Nguyen <hainm.comp@gmail.com>
2015-05-13 14:43:59 -0400

    PYTRAJ: update speed comparison for test_iadd

--- tests/test_iadd.py
+++ tests/test_iadd.py
@@ -7,6 +7,7 @@ from pytraj.utils import eq, aa_eq
 from pytraj.decorators import no_test, test_if_having, test_if_path_exists
 from pytraj.testing import cpptraj_test_dir
 import pytraj.common_actions as pyca
+from pytraj.utils import Timer
 
 class Test(unittest.TestCase):
     def test_0(self):
@@ -18,6 +19,21 @@ class Test(unittest.TestCase):
         print (traj)
         assert  traj.n_frames == 2 * old_size
         aa_eq(traj[0].xyz, traj[old_size].xyz)
+
+        @Timer()
+        def normal(fa):
+            fa += fa
+
+        @Timer()
+        def make_copy(fa):
+            fa += fa.copy()
+
+        fa = traj.copy()
+        normal(fa)
+        fa = traj.copy()
+        make_copy(fa)
+
+        # result: similiar speed
 
 if __name__ == "__main__":
     unittest.main()
