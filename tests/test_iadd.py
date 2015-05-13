1320b5484488c92e5d775b9533c304e4c2bfbeb2
Hai Nguyen <hainm.comp@gmail.com>
2015-05-13 14:30:03 -0400

    PYTRAJ: udpate FIXED for bugs: traj += traj

---
+++ tests/test_iadd.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import unittest
+from pytraj.base import *
+from pytraj import adict
+from pytraj import io as mdio
+from pytraj.utils import eq, aa_eq
+from pytraj.decorators import no_test, test_if_having, test_if_path_exists
+from pytraj.testing import cpptraj_test_dir
+import pytraj.common_actions as pyca
+
+class Test(unittest.TestCase):
+    def test_0(self):
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        old_size = traj.n_frames
+        print (traj)
+
+        traj += traj
+        print (traj)
+        assert  traj.n_frames == 2 * old_size
+        aa_eq(traj[0].xyz, traj[old_size].xyz)
+
+if __name__ == "__main__":
+    unittest.main()
