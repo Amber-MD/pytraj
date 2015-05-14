888a8bfa83338818f77f52842dc4e5715519f861
Hai Nguyen <hainm.comp@gmail.com>
2015-05-13 20:37:18 -0400

    PYTRAJ: timing for slicing Trajectory from python's `range`
    traj[0:100:2] == traj[range(0, 100, 2)] and similiar speed
    (but about 15 times slower than traj._fast_slice(slice(0, 100, 2)))

--- tests/test_fast_slice.py
+++ tests/test_fast_slice.py
@@ -8,13 +8,15 @@ from pytraj.decorators import no_test, test_if_having, test_if_path_exists
 from pytraj.testing import cpptraj_test_dir
 import pytraj.common_actions as pyca
 from pytraj.six_2 import zip
+from pytraj.externals.six.moves import range
 from timeit import timeit
 
 class Test(unittest.TestCase):
     def test_0(self):
         traj = mdio.iterload("./data/tz2.ortho.nc", "./data/tz2.ortho.parm7")[:]
+        print (traj.n_frames)
 
-        s = slice(3, 100, 4)
+        s = slice(3, traj.n_frames, 4)
         t0 = traj[s]
         t1 = traj._fast_slice(s)
         print (traj[3, 0], t0[0, 0], t1[0, 0])
@@ -25,11 +27,23 @@ class Test(unittest.TestCase):
         def normal_slice():
             traj[s]
 
+        sr = range(3, traj.n_frames, 4)
+        def test_range():
+            traj[sr]
+
         def fast_slice():
             traj._fast_slice(s)
 
+        print ("normal_slice")
         print (timeit(normal_slice, number=100))
+        print ("test_range")
+        print (timeit(test_range, number=100))
+        print ("fast_slice")
         print (timeit(fast_slice, number=100))
+
+        #
+        aa_eq(traj[sr].xyz, traj[s].xyz)
+        aa_eq(traj._fast_slice(s).xyz, traj[s].xyz)
 
 if __name__ == "__main__":
     unittest.main()
