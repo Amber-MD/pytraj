e537876c3fef7183241fd72ad9aee48c8d6bf75e
Hai Nguyen <hainm.comp@gmail.com>
2015-05-15 21:30:32 -0400

    PYTRAJ: tune speed for traj['*'] = xyz
    don't create AtomMask object

--- pytraj/utils/__init__.py
+++ pytraj/utils/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 from .check_and_assert import assert_almost_equal, file_exist, is_generator
 from .check_and_assert import assert_almost_equal as aa_eq
-from .check_and_assert import eq, a_isinstance
+from .check_and_assert import eq, a_isinstance, eq_coords
 from .check_and_assert import _import, _import_numpy, is_int, require
 from .check_and_assert import has_, is_array
 from .check_and_assert import ensure_not_none_or_string
--- tests/test_speed_general.py
+++ tests/test_speed_general.py
@@ -3,7 +3,7 @@ import unittest
 from pytraj.base import *
 from pytraj import adict
 from pytraj import io as mdio
-from pytraj.utils import eq, aa_eq
+from pytraj.utils import eq, aa_eq, eq_coords
 from pytraj.decorators import no_test, test_if_having, test_if_path_exists
 from pytraj.testing import cpptraj_test_dir
 import pytraj.common_actions as pyca
@@ -13,6 +13,20 @@ class Test(unittest.TestCase):
     def test_0(self):
         traj = mdio.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         fa = traj[:]
+
+        @Timer()
+        def test_time_aa_eq():
+            aa_eq(fa.xyz, traj.xyz)
+
+        @Timer()
+        def test_time_eq_coords():
+            eq_coords(fa, traj)
+
+        print ("test_time_aa_eq")
+        test_time_aa_eq()
+        print ("test_time_eq_coords")
+        test_time_eq_coords()
+
         print (fa)
         atm = traj.top("@CA")
         indices = atm.indices
@@ -38,9 +52,9 @@ class Test(unittest.TestCase):
         aa_eq(fa['@CA'].xyz, X[:, indices])
 
         # assignment speed
-        # 50K frames
+        # 10240 frames
         FA = fa.copy()
-        for _ in range(4):
+        for _ in range(3):
             FA += FA + FA + FA
         FA += FA[:10000].copy()
         fa = FA[:50000]
@@ -58,10 +72,9 @@ class Test(unittest.TestCase):
         speed_test_np() # 
         speed_test_pytraj() # similiar speed
         # make sure ther are equal
-        aa_eq(fa['@CA'].xyz, X[:, indices])
+        #aa_eq(fa['@CA'].xyz, X[:, indices])
 
         # assignment speed
-        # 50K frames, different mask
         mask = "@H=,CB"
         indices = traj.top(mask).indices
         del xyz
@@ -81,7 +94,6 @@ class Test(unittest.TestCase):
         aa_eq(fa[mask].xyz, X[:, indices])
 
         # assignment speed
-        # 50K frames, different mask
         mask = "!@H=,CB"
         indices = traj.top(mask).indices
         del xyz
@@ -99,6 +111,24 @@ class Test(unittest.TestCase):
         speed_test_pytraj() # similiar speed
         # make sure ther are equal
         aa_eq(fa[mask].xyz, X[:, indices])
+
+        # assignment speed
+        xyz = fa.xyz + 10.
+        fa2 = fa.copy()
+
+        @Timer()
+        def update_xyz_version():
+            fa.update_xyz(xyz)
+
+        @Timer()
+        def fancy_indexing_version():
+            fa2['*'] = xyz
+
+        print ("update_xyz_version")
+        update_xyz_version()
+        print ("fancy_indexing_version")
+        fancy_indexing_version()
+        aa_eq(fa.xyz ,fa2.xyz) # ok
 
 
 if __name__ == "__main__":
