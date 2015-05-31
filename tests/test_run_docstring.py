52bf6c022453deff6d40418e927c97674af8d29d
Hai Nguyen <hainm.comp@gmail.com>
2015-05-31 14:20:48 -0400

    pytraj: even better docstring test

--- tests/test_run_docstring.py
+++ tests/test_run_docstring.py
@@ -12,20 +12,23 @@ def silly_doc_func():
 
 class Test(unittest.TestCase):
     def test_0(self):
+        print ("silly_doc_func")
         run_docstring(silly_doc_func)
 
-        # _frame_iter_master
+        print ("_frame_iter_master")
         from pytraj._shared_methods import _frame_iter_master as fi
         run_docstring(fi)
 
-        # matrix_analysis
+        print ("matrix_analysis")
         from pytraj import matrix_analysis as ma
         func_names = ma.default_key_dict.keys()
         for key in func_names:
             run_docstring(ma.__dict__[key])
 
-        # pyca.calc_rmsd
+        print ("pyca.calc_rmsd")
         run_docstring(pyca.calc_rmsd)
+
+        print ("Trajectory.calc_rmsd")
         from pytraj import Trajectory
         run_docstring(Trajectory.calc_rmsd)
 
