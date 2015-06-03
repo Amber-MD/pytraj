a3edf7eec0db902ce38b6760167d363b916b50cd
Hai Nguyen <hainm.comp@gmail.com>
2015-06-03 18:12:48 -0400

    PYTRAJ: add seperate command for each dihedral calculation (phi, psi,
    using template (rather than manual typing), creat function as text
    and then using exec(func_template). A bit slow to load but just fine
    
    In [10]: from pytraj import dihedral_analysis
    
    In [11]: dihedral_analysis.calc_phi(traj, "resrange 3-10")
    
    dihedral_analysis.calc_alpha    dihedral_analysis.calc_chip
    dihedral_analysis.calc_gamma    dihedral_analysis.calc_omega
    dihedral_analysis.calc_zeta
    dihedral_analysis.calc_beta     dihedral_analysis.calc_delta
    dihedral_analysis.calc_nu1      dihedral_analysis.calc_phi
    dihedral_analysis.calc_chin     dihedral_analysis.calc_epsilon
    dihedral_analysis.calc_nu2      dihedral_analysis.calc_psi

---
+++ pytraj/dihedral_analysis.py
@@ -0,0 +1,55 @@
+from __future__ import print_function, absolute_import
+
+from .actions.Action_MultiDihedral import Action_MultiDihedral
+from ._get_common_objects import _get_top, _get_data_from_dtype
+from .externals.six import iteritems
+from .DataSetList import DataSetList
+
+
+__all__ = []
+
+supported_dihedral_types = [x for x in 
+'phi psi chip omega alpha beta gamma delta epsilon zeta nu1 nu2 chin'.split()]
+
+template = '''
+def calc_%s(traj=None, command="", top=None, *args, **kwd):
+    """
+    Parameters
+    ----------
+    traj : Trajectory-like or anything that makes _frame_iter_master(traj) return Frame
+    command : cpptraj command
+    top : {str, Topology}, optional, default None
+    *args, **kwd: more arguments
+
+    Examples
+    --------
+
+    See Also
+    --------
+    """
+
+    if 'dtype' in kwd.keys():
+        dtype = kwd['dtype']
+        del kwd['dtype']
+    else:
+        dtype = None
+
+    _top = _get_top(traj, top)
+    dslist = DataSetList()
+    template_command = '%s '
+    template_command += command 
+
+    act = Action_MultiDihedral()
+    act(template_command, traj, top=_top, dslist=dslist, *args, **kwd)
+    # need to call `print_output` so cpptraj can normalize some data
+    # check cpptraj's code
+    act.print_output()
+    return _get_data_from_dtype(dslist, dtype=dtype)
+'''
+
+for key in supported_dihedral_types:
+    my_func_str = template % (key, key)
+    g_dict = globals()
+    exec(my_func_str)
+
+del key
