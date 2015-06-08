18d40b3bd697b7aa078daf52070c31a017c1b75a
Hai Nguyen <hainm.comp@gmail.com>
2015-06-08 14:12:52 -0400

    pytraj: find_library

--- tests/test_find_libcpptraj.py
+++ tests/test_find_libcpptraj.py
@@ -1,4 +1,5 @@
-from pytraj.misc import find_libcpptraj
+import unittest
+from pytraj.misc import find_libcpptraj, find_library
 from ctypes import cdll
 import unittest
 
@@ -7,3 +8,5 @@ p = find_libcpptraj()
 print (p)
 plib = cdll.LoadLibrary(p[-1])
 print (plib)
+print (find_libcpptraj(unique=True))
+print (find_library('netcdf', unique=True))
