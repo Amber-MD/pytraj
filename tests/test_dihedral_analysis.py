9b1ff1073eb2eae67021b2da889516d2023d5e61
Hai Nguyen <hainm.comp@gmail.com>
2015-06-09 14:32:52 -0400

    pytraj: test_dihedral_analysis.py : add dict

--- tests/test_dihedral_analysis.py
+++ tests/test_dihedral_analysis.py
@@ -22,11 +22,17 @@ class Test(unittest.TestCase):
         dslist1 = da.calc_phi(traj, 'resrange 7')
         arr1 = dslist1.to_ndarray()
         arr2 = da.calc_phi(traj, 'resrange 7', dtype='ndarray')
+        arr3 = da.calc_phi(traj, 'resrange 7').to_dict()
+        print (arr3)
 
         # assert to cpptraj output
         aa_eq(arr0, saved_data_phi)
         aa_eq(arr1, saved_data_phi)
         aa_eq(arr2, saved_data_phi)
+
+        for key in dslist1.keys():
+            print (dslist1[key].values)
+            aa_eq(dslist1[key].values, arr3[key])
 
 if __name__ == "__main__":
     unittest.main()
