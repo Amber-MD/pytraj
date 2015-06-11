dc287d7ddac6dc4a7da6222570c71793f00a4755
Hai Nguyen <hainm.comp@gmail.com>
2015-06-11 16:37:30 -0400

    pytraj: on/off guess_bond

--- pytraj/externals/_load_pseudo_parm.py
+++ pytraj/externals/_load_pseudo_parm.py
@@ -11,7 +11,7 @@ _, np = _import_numpy()
 
 __all__ = ['load_pseudo_parm']
 
-def load_pseudo_parm(parm):
+def load_pseudo_parm(parm, guess_bond=True):
     """load_external's parm objects
 
     Parameters
@@ -117,6 +117,12 @@ def load_pseudo_parm(parm):
             pseudotop.box = Box()
     try:
         pseudotop._bonds_ndarray[0]
-    except:
-        pseudotop.guess_bond()
+    except IndexError:
+        # TODO, FIXME: really slow if loading parmed
+        # >>> import parmed as pmd
+        # >>> pdb = pmd.download_PDB("1o15")
+        # >>> top = io.load_pseudo_parm(pdb) $ 46 s. Really?
+        if guess_bond:
+            pseudotop.guess_bond()
+        pass
     return pseudotop
