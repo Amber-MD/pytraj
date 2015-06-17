4da089076d9f994a715af3f78e73c78d844c92f0
Hai Nguyen <hainm.comp@gmail.com>
2015-06-16 20:29:00 -0400

    pytraj: fix atom.__str__

--- tests/test_Atom.py
+++ tests/test_Atom.py
@@ -2,31 +2,10 @@ import unittest
 from pytraj.base import *
 from pytraj import io as mdio
 from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj.testing import no_test
 
 class Test(unittest.TestCase):
-    def test_0(self):
-        at = Atom()
-        
-        print(at)
-        print(at.atomic_number)
-        
-        top = Topology("./data/HP36.top")
-        a0 = top.atomlist[0]
-        print(a0)
-        print(a0.element)
-        print(len(a0.element_short_name))
-        print(a0.atomic_number)
-        print(dir(a0))
-        print(a0.molnum)
-        print(a0.nbonds)
-        print(a0.typeindex)
-
-    def test_0(self):
-        at = Atom()
-        
-        print(at)
-        print(at.atomic_number)
-        
+    def test_1(self):
         top = Topology("./data/HP36.top")
         a0 = top.atomlist[0]
         a100 = top.atomlist[100]
@@ -43,10 +22,19 @@ class Test(unittest.TestCase):
         print(a100.is_bonded_to(120))
         print(a100.n_bonds)
 
-    def test_0(self):
+    def test_2(self):
         top = Topology("./data/HP36.top")
         for atom in top:
             print (atom)
+
+    def test_3(self):
+        # test Atom() from `pytraj` namespace
+        # Aim: test if having segmentation fault
+        import pytraj as pt
+        #pt.Atom()
+        # FIXME: segmentation fault
+        #print (pt.Atom())
+        pass
 
 if __name__ == "__main__":
     unittest.main()
