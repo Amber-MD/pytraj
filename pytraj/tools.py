6917e7683b095f678c053aa06884c00c21c1dbca
Hai Nguyen <hainm.comp@gmail.com>
2015-06-23 17:44:53 -0400

    pytraj add tools

--- pytraj/tools.py
+++ pytraj/tools.py
@@ -1,15 +1,16 @@
+from pytraj.utils import _import_numpy
+
+_, np = _import_numpy()
 
 def chunk_average(data):
-    pass
-
-def moving_average(data, windows):
     pass
 
 def slicing_sum(data, windows):
     pass
 
 def slicing_mean(data, windows):
-    pass
+    N = windows
+    return np.convolve(x, np.ones((N,))/N)[(N-1):]
 
 def slicing_max(data, windows):
     pass
