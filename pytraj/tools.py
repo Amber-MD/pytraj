9371c64c2ebc32cd5fa86dbf10657e41e9ec2d61
Hai Nguyen <hainm.comp@gmail.com>
2015-06-23 18:30:20 -0400

    pytraj tools: udate

--- pytraj/tools.py
+++ pytraj/tools.py
@@ -5,22 +5,41 @@ from pytraj.utils import _import_numpy
 
 _, np = _import_numpy()
 
-def chunk_average(data):
-    pass
+def _dispatch_value(func):
+    def inner(data, *args, **kwd):
+        if hasattr(data, 'values'):
+            _data = data.values
+        else:
+            _data = data
+        return func(_data, *args, **kwd)
+    inner.__doc__ = func.__doc__
+    return inner
 
-def slicing_sum(data, windows):
-    pass
+@_dispatch_value
+def split(data, n_chunks_or_array):
+    """split `self.data` to n_chunks
 
-def slicing_mean(data, windows):
-    N = windows
-    return np.convolve(data, np.ones((N,))/N)[(N-1):]
+    Notes : require numpy (same as `array_split`)
+    """
+    return np.array_split(data, n_chunks_or_array)
 
-def slicing_max(data, windows):
-    pass
+@_dispatch_value
+def chunk_average(self, n_chunk):
+    import numpy as np
+    return np.array(list(map(np.mean, split(self, n_chunk))))
 
-def slicing_min(data, windows):
-    pass
+@_dispatch_value
+def moving_average(data, n):
+    # http://stackoverflow.com/questions/11352047/finding-moving-average-from-data-points-in-python
+    """
+    Note
+    ----
+    not assert yet
+    """
+    window = np.ones(int(n))/float(n)
+    return np.convolve(data, window, 'same')
 
+@_dispatch_value
 def pipe(self, *funcs):
     """apply a series of functions to self's data
     """
