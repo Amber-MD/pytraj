e061626634222c96bc7c2d1917094cb9eb3a7732
Hai Nguyen <hainm.comp@gmail.com>
2015-06-23 17:34:13 -0400

    pytraj: add tools

---
+++ pytraj/tools.py
@@ -0,0 +1,18 @@
+
+def chunk_average(data):
+    pass
+
+def moving_average(data, windows):
+    pass
+
+def slicing_sum(data, windows):
+    pass
+
+def slicing_mean(data, windows):
+    pass
+
+def slicing_max(data, windows):
+    pass
+
+def slicing_min(data, windows):
+    pass
