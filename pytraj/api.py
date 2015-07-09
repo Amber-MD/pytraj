57cd1ab7b31f1e354d92149c68c45d90043b983b
Hai Nguyen <hainm.comp@gmail.com>
2015-07-09 03:50:36 -0400

    pt: NONE

--- pytraj/api.py
+++ pytraj/api.py
@@ -89,7 +89,7 @@ class Trajectory(ActionTrajectory):
         for i in range(self.xyz.shape[0]):
             frame = Frame(self.n_atoms)
             frame[:] = self.xyz[i]
-            if self._boxes:
+            if self._boxes is not None:
                 frame.box = Box(self._boxes[i])
             self._life_holder = frame
             yield self._life_holder
@@ -100,7 +100,7 @@ class Trajectory(ActionTrajectory):
             arr0 = self.xyz[idx]
             frame = Frame(self.n_atoms)
             frame[:] = arr0
-            if self._boxes:
+            if self._boxes is not None:
                 frame.box = Box(self._boxes[idx])
             self._life_holder = frame
         else:
