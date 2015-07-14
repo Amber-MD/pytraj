99a082972af49cd3107af2fc7009e6c3027bf110
Hai Nguyen <hainm.comp@gmail.com>
2015-07-14 17:55:32 -0400

    pt : example_load_REMD_trajectory.py

---
+++ examples/example_load_REMD_trajectory.py
@@ -0,0 +1,8 @@
+import pytraj as pt
+
+traj = pt.iterload_remd("../tests/data/Test_RemdTraj/rem.nc.000",
+        top="../tests/data/Test_RemdTraj/ala2.99sb.mbondi2.parm7",
+                        T="300.0")
+
+# cpptraj will automatically find frames with 300K
+print (traj.temperatures)
