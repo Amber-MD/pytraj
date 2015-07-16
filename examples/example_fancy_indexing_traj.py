87c10e32aa842515c9722aeaaf0692b347f64be3
Hai Nguyen <hainm.comp@gmail.com>
2015-07-16 01:33:20 -0400

    pt: update example_fancy_indexing_traj.py

--- examples/example_fancy_indexing_traj.py
+++ examples/example_fancy_indexing_traj.py
@@ -5,17 +5,9 @@ traj = mdio.load("../tests/data/md1_prod.Tc5b.x", "../tests/data/Tc5b.top")[:]
 # get x coord of 0-th atom of 0-th frame
 print(traj[0, 0, 0])
 
-# get new array with shape = (traj.n_frames, n_atoms, 3)
-print(traj[:, :, :].shape)
+# get new Trajectory with only CA atoms
+print (traj['@CA'])
 
-# get new array with shape (n_atoms, 3) for 0-th frame
-print(traj[0, :, :].shape)
-
-# get new Trajectory object with strip atoms
-# keeping only CA atoms
-print (traj['@CA :frame'])
-
-# get coordinate of all CA atoms for all frames?
-# don't use `:frame`
-print (traj['@CA'].shape)
-# result: (10, 20, 3)
+# get new Trajectory with only CA with several frames
+print (traj[2:7, '@CA'])
+print (traj[[1, 5, 7, 9], '@CA'])
