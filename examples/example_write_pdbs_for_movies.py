cbca9726617e31eba6797789fd747d7a1acc53a4
Hai Nguyen <hainm.comp@gmail.com>
2015-07-16 01:16:07 -0400

    pt: add example_write_pdbs_for_movies.py

---
+++ examples/example_write_pdbs_for_movies.py
@@ -0,0 +1,19 @@
+import pytraj as pt
+
+# turn on this to see cpptraj's stdout
+#pt.set_cpptraj_verbose(True)
+
+# use iterload for memory saving
+traj = pt.iterload("../tests/data/md1_prod.Tc5b.x",
+                   "../tests/data/Tc5b.top")
+
+# write multiple pdbs to a single file
+# just like multiple pdbs when you load from rcsb
+# you can use this pdb file to view in VMD without loading
+# prmtop/gro/psf/...
+
+pt.write_traj("./output/test.pdb", 
+              traj,
+              format='pdb',
+              mode='model',
+              overwrite=True)
