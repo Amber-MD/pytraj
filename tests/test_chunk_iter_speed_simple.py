3b34029069803703d803ea1ddba91a826f5ab25f
Hai Nguyen <hainm.comp@gmail.com>
2015-07-24 19:30:22 -0400

    pt: simple

--- tests/test_chunk_iter_speed_simple.py
+++ tests/test_chunk_iter_speed_simple.py
@@ -10,43 +10,6 @@ import pytraj.common_actions as pyca
 
 
 class Test(unittest.TestCase):
-
-    def test_1(self):
-        from pytraj.testing import make_fake_traj
-        from pytraj.compat import zip
-
-        fa = make_fake_traj(1000, 1000)
-        fa2 = fa.__class__(top=fa.top)
-
-        for chunk in fa.chunk_iter(chunksize=100):
-            fa2.join(chunk, copy=False)
-
-        print(fa2.n_frames, fa.n_frames)
-        assert fa2.n_frames == fa.n_frames
-        eq_coords(fa2, fa)
-
-        from pytraj.utils import Timer
-
-        @Timer()
-        def test_chunk(chunksize=2):
-            print("chunk = %s" % chunksize)
-            for chunk in fa.chunk_iter(chunksize=chunksize):
-                pass
-
-        @Timer()
-        def test_frame_iter():
-            for frame in fa:
-                pass
-
-        print("test_chunk")
-        test_chunk(2)
-        test_chunk(10)
-        test_chunk(100)
-        test_chunk(200)
-        test_chunk(500)
-        print("test frame_iter")
-        test_frame_iter()
-
     def test_0(self):
         fa = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         f_list = []
