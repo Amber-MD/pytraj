79bc6dcb4fcd088bfb162771f239e82a42ef0d4b
Hai Nguyen <hainm.comp@gmail.com>
2015-07-27 22:53:08 -0400

    pt: add test_Frame_construct_from_pointer.py

---
+++ tests/test_Frame_construct_from_pointer.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import unittest
+import pytraj as pt
+from pytraj.utils import eq, aa_eq
+from pytraj.decorators import no_test, test_if_having, test_if_path_exists
+import pytraj.common_actions as pyca
+
+
+class Test(unittest.TestCase):
+
+    def test_0(self):
+        traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        print(traj)
+        for xyz0 in traj.xyz:
+            frame = pt.Frame(traj.n_atoms, xyz0, _as_ptr=True)
+            aa_eq(frame.xyz, xyz0)
+            print(frame.xyz[0, 0])
+
+if __name__ == "__main__":
+    unittest.main()
