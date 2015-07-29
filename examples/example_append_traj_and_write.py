54a5a223d87a3f8811f79701532ef7900692ef02
Hai Nguyen <hainm.comp@gmail.com>
2015-07-29 01:29:21 -0400

    pt: add example_append_traj_and_write.py

---
+++ examples/example_append_traj_and_write.py
@@ -0,0 +1,15 @@
+import pytraj as pt
+import os
+
+traj = pt.iterload("../tests/data/md1_prod.Tc5b.x", "../tests/data/Tc5b.top")
+
+# write new traj with frames from 0 to 2, and 5 to 8
+# skip last frame to follow python's convention.
+
+# you can use traj[0:3], traj[5:9] but this does not save memory
+# traj(0,3) like `range` in python3 or `xrange` in python2
+
+pt.write_traj('traj_append.nc', 
+              [traj(0, 3), traj(5, 9)], top=traj.top,
+              overwrite=True,
+              )
