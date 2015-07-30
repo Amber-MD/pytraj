8666f18d642ee75119e03c9836f044d4499d5c7e
Hai Nguyen <hainm.comp@gmail.com>
2015-07-30 15:02:42 -0400

    pt: lazy select frame

--- pytraj/api.py
+++ pytraj/api.py
@@ -636,3 +636,14 @@ class Trajectory(ActionTrajectory):
         """esimated MB of data will be loaded to memory
         """
         return self.n_frames * self.n_atoms * 3 * 8 / (1024 ** 2)
+
+    @classmethod
+    def from_iterable(cls, iterables, top=None):
+        if top is None:
+            raise ValueError("require a Topology")
+        traj = cls()
+        traj.top = top
+        for f in iterables:
+            # todo: avoid copying
+            traj.append(f)
+        return traj
