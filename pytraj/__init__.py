a253351b786c47f659701430b3649cdcda959b7c
Hai Nguyen <hainm.comp@gmail.com>
2015-07-30 11:20:02 -0400

    pt: load_parmed --> from_parmed

--- pytraj/__init__.py
+++ pytraj/__init__.py
@@ -153,6 +153,7 @@ center_of_mass = calc_center_of_mass
 center_of_geometry = calc_center_of_geometry
 get_average_structure = get_average_frame
 load_parmed = load_ParmEd
+from_parmed = load_ParmEd
 
 from . matrix_analysis import distance_matrix
 from . dihedral_analysis import (
