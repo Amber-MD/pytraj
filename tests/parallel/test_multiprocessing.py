2ec5425f1117e5e67b866e06c554a7bdb35467f3
Hai Nguyen <hainm.comp@gmail.com>
2015-07-30 21:11:30 -0400

    pt: update test

--- tests/parallel/test_multiprocessing.py
+++ tests/parallel/test_multiprocessing.py
@@ -1,14 +1,27 @@
 import pytraj as pt
+import sys
 
-traj = pt.iterload('./data/tz2.ortho.nc', './data/tz2.ortho.parm7')
+trajname = './data/nogit/remd/remd.x.000'
+topname = './data/nogit/remd/myparm.top'
+traj = pt.iterload(trajname, topname)
 
-def worker(traj=traj):
-    print(pt.rmsd(traj))
+n_jobs = int(sys.argv[1])
 
-import threading
+def worker(i):
+    traj = pt.iterload(trajname, topname)
+    print(pt.radgyr(traj(stop=1000)))
 
-threads = []
-for i in range(100):
-   t = threading.Thread(target=worker)
-   threads.append(t)
-   t.start()
+if sys.argv[2] == 'p': 
+    print('parallel')
+    from multiprocessing import Pool
+
+    p = Pool(n_jobs)
+    p.map(worker, [_ for _ in range(n_jobs)])
+
+elif sys.argv[2] == 's': 
+    print('serial')
+    for i in range(n_jobs):
+        worker(i)
+
+else:
+    raise ValueError('must have sys.argv[2]')
