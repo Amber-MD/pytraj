ed024d00faf009a2352a0011ca14c9ce583d54a2
Hai Nguyen <hainm.comp@gmail.com>
2015-08-03 17:23:43 -0400

    pt: add example_parallelize.py

---
+++ examples/example_parallelize.py
@@ -0,0 +1,10 @@
+import pytraj as pt
+
+# use `iterload` to save memory
+traj = pt.iterload("../tests/data/tz2.ortho.nc",
+                   "../tests/data/tz2.ortho.parm7")
+
+# use 4 available cores
+# calculate radgyr for all atoms
+result = pt.pmap(n_cores=4, func=pt.radgyr, traj=traj)
+print(result)
