3ebf63e508236403c2a432327903f647d1f25d7c
Hai Nguyen <hainm.comp@gmail.com>
2015-08-04 22:07:26 -0400

    pt: example_minimum_distance.py

--- examples/example_minimum_distance.py
+++ examples/example_minimum_distance.py
@@ -5,3 +5,7 @@ pdb = pt.load_pdb_rcsb("1l2y")
 # calculate minimum distance betwee two sets of atoms
 out = pt.mindist(pdb, '@CA @H')
 print (out)
+
+# use 2d array-like for two atom groups
+out = pt.mindist(pdb, [[0, 3, 7], [6, 9, 100, 200]])
+print (out)
