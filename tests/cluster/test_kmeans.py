bc39c6d9a2c3ac17330df57ccde3cee4c217deec
Hai Nguyen <hainm.comp@gmail.com>
2015-08-07 17:32:34 -0400

    pt: move to seperate cluster module

---
+++ tests/cluster/test_kmeans.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import unittest
+import pytraj as pt
+from pytraj.utils import eq, aa_eq
+from pytraj.decorators import no_test, test_if_having, test_if_path_exists
+import pytraj.common_actions as pyca
+
+
+class Test(unittest.TestCase):
+
+    def test_0(self):
+        from pytraj import cluster
+        pt.set_cpptraj_verbose()
+
+        kmeans = cluster.kmeans
+        traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+
+        dset = kmeans(traj, n_clusters=10, kseed=2, random_point=True,
+                      distance_metric='rms',
+                      mask='@CA')
+
+if __name__ == "__main__":
+    unittest.main()
