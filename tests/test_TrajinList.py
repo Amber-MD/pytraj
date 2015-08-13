08f5e92995e0a1f968dbf4561191ba6fe066ceed
Hai Nguyen <hainm.comp@gmail.com>
2015-08-13 15:10:28 -0400

    pt: fix

--- tests/test_TrajinList.py
+++ tests/test_TrajinList.py
@@ -27,13 +27,11 @@ class TestTrajinList(unittest.TestCase):
         trajlist.add_traj("./data/md1_prod.Tc5b.x", top, "4 9 2")
         trajlist.add_traj("./data/md1_prod.Tc5b.x", top, "5 7")
         trajlist.add_traj("./data/md1_prod.Tc5b.x", top, "1 last")
-        print(trajlist.max_frames)
         print(trajlist[0].n_frames)
 
         trajlist2 = TrajinList()
         trajlist2.add_traj("./data/Test_RemdTraj/rem.nc.000",
                            "./data/Test_RemdTraj/ala2.99sb.mbondi2.parm7", "*")
-        print(trajlist2.max_frames)
 
     def test_1(self):
         topname = "./data/Tc5b.top"
@@ -44,7 +42,6 @@ class TestTrajinList(unittest.TestCase):
         trajlist.add_traj("./data/md1_prod.Tc5b.x", top, "4 9 2")
         trajlist.add_traj("./data/md1_prod.Tc5b.x", top, "5 7")
         trajlist.add_traj("./data/md1_prod.Tc5b.x", top, "1 last")
-        print(trajlist.max_frames)
         print(trajlist[0].n_frames)
 
         trajlist.top = top.copy()
