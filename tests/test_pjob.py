7b1419e05d0e374a9d7b560e63dbe0b454da8577
Hai Nguyen <hainm.comp@gmail.com>
2015-08-13 23:37:24 -0400

    pt: only test py3 for pjob for now

--- tests/test_pjob.py
+++ tests/test_pjob.py
@@ -3,10 +3,12 @@ import unittest
 import pytraj as pt
 from pytraj.utils import eq, aa_eq
 from pytraj.decorators import no_test, test_if_having, test_if_path_exists
+from pytraj.tools import PY2
 
 
 class Test(unittest.TestCase):
 
+    @unittest.skipIf(PY2, 'only work python3')
     def test_0(self):
         from pytraj.parallel import PJob
         traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
