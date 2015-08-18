2fca7468f99420af946758e5c2f31aba99a788d6
Hai Nguyen <hainm.comp@gmail.com>
2015-08-18 00:40:41 -0400

    pt: add memory folder

---
+++ tests/memory/new_frame_constructor.py
@@ -0,0 +1,19 @@
+import pytraj as pt
+from memory_profiler import profile
+
+@profile
+def test():
+    fname = '../data/nogit/remd/remd.x.000'
+    topname = '../data/nogit/remd/myparm.parm7'
+    traj = pt.api.Trajectory(fname, top=topname)
+    print(traj.xyz.dtype)
+
+    for f in traj:
+        pass
+
+    pt.radgyr(traj)
+    pt.molsurf(traj, '@CA')
+    pt.rmsd(traj, mask='@CA')
+    pt.multidihedral(traj)
+
+test()
