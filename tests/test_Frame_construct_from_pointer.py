ba21dd82375e2b2236a227f583970b3be46ccac7
Hai Nguyen <hainm.comp@gmail.com>
2015-08-18 00:28:44 -0400

    Frame_pt_constructor update test_Frame_construct_from_pointer.py

--- tests/test_Frame_construct_from_pointer.py
+++ tests/test_Frame_construct_from_pointer.py
@@ -11,10 +11,32 @@ class Test(unittest.TestCase):
     def test_0(self):
         traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         print(traj)
+
         for xyz0 in traj.xyz:
             frame = pt.Frame(traj.n_atoms, xyz0, _as_ptr=True)
             aa_eq(frame.xyz, xyz0)
             print(frame.xyz[0, 0])
+
+    def test_1(self):
+        print('api.Trajectory iterating')
+        traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        api_t0 = pt.api.Trajectory(traj)
+        print(api_t0)
+
+        # __iter__
+        for f in api_t0:
+            pass
+
+        f.xyz[0, 0] = 10.
+        assert f.xyz[0, 0] == 10.
+        assert api_t0.xyz[-1, 0, 0] == 10.
+
+        # __getitem__
+        # make a new copy
+        api_t0 = pt.api.Trajectory(traj)
+        f0 = api_t0[0]
+        f0.xyz[0, 0] = 200.
+        assert api_t0.xyz[0, 0, 0] == 200.
 
 if __name__ == "__main__":
     unittest.main()
