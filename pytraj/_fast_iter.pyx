688710ad36096b6adfc262a2ebda0a205215de20
Hai Nguyen <hainm.comp@gmail.com>
2015-08-25 15:00:10 -0400

    pt: update

--- pytraj/_fast_iter.pyx
+++ pytraj/_fast_iter.pyx
@@ -1,36 +1,14 @@
 
 from .Frame cimport Frame, _Frame
 
-def _fastiter(self, int n_atoms):
-    cdef double[:, :, ::1] view = self.xyz
+def _fastiter_ptr(double[:, :, :] xyz, int n_atoms):
     cdef int i
-    cdef int n_frames = view.shape[0]
+    cdef int n_frames = xyz.shape[0]
 
     # just create a pointer
-    cdef Frame frame = Frame()
+    cdef Frame frame = Frame(n_atoms, xyz[0], _as_ptr=True)
     cdef _Frame _frame
 
-    #frame._as_view = True
-
     for i in range(n_frames):
-        #_frame = _Frame(n_atoms, &view[i, 0, 0])
-        frame = Frame(n_atoms, view[i], _as_ptr=True)
+        frame.thisptr.SetXptr(n_atoms, &xyz[i, 0, 0])
         yield frame
-        #del _frame # don't deallocate, will get double-free
-
-def _fastiter_ptr(self, int n_atoms):
-    cdef double[:, :, ::1] view = self.xyz
-    cdef int i
-    cdef int n_frames = view.shape[0]
-
-    # just create a pointer
-    cdef Frame frame = Frame(n_atoms, view[0], _as_ptr=True)
-    cdef _Frame _frame
-
-    #frame._as_view = True
-
-    for i in range(n_frames):
-        #_frame = _Frame(n_atoms, &view[i, 0, 0])
-        frame.thisptr.SetXptr(&view[i, 0, 0])
-        yield frame
-        #del _frame # don't deallocate, will get double-free
