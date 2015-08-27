b38246254230f3ca7d4a9419051dfda3a4baead6
Hai Nguyen <hainm.comp@gmail.com>
2015-08-27 15:36:00 -0400

    pt:fix

--- tests/test_fancy_indexing_traj_frame_atommask.py
+++ tests/test_fancy_indexing_traj_frame_atommask.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import pytraj as pt
 import unittest
 from pytraj import io as mdio
 from pytraj.utils import eq, aa_eq
@@ -7,7 +8,9 @@ from pytraj.utils import eq, aa_eq
 class Test(unittest.TestCase):
     def test_0(self):
         traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
-        aa_eq(traj[0, '@CA'].xyz, traj['@CA'][0].xyz)
+        f0 = traj[0, '@CA']
+        f1 = traj['@CA'][0]
+        assert pt.tools.rmsd(f0.xyz, f1.xyz) == 0.0
 
 
 if __name__ == "__main__":
