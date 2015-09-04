fff737e546fc48897b1dc82f087e303003af2b4a
Hai Nguyen <hainm.comp@gmail.com>
2015-09-04 17:10:45 -0400

    pt: assert for test_Action_Jcoupling.py

--- tests/test_Action_Jcoupling.py
+++ tests/test_Action_Jcoupling.py
@@ -1,33 +1,25 @@
 import os
 import unittest
-from pytraj.base import *
-from pytraj import io as mdio
-from pytraj import adict
-from pytraj.utils.check_and_assert import assert_almost_equal
-from pytraj.core.DataFileList import DataFileList
-from pytraj.datasets.DataSetList import DataSetList
-from pytraj.testing import cpptraj_test_dir, aa_eq
+import pytraj as pt
+from pytraj.testing import aa_eq, cpptraj_test_dir
 
 
 class Test(unittest.TestCase):
     def test_0(self):
-        kfile = os.path.join(cpptraj_test_dir, "Test_Jcoupling", "Karplus.txt")
-        traj = mdio.iterload("./data/tz2.nc", "./data/tz2.parm7")
-        frame = traj[0]
-        command = "kfile %s" % kfile
-        dslist = DataSetList()
-        dflist = DataFileList()
-        d0 = adict['jcoupling'](command, frame, traj.top,
-                                dslist=dslist,
-                                dflist=dflist)
+        kfile = os.path.abspath(os.path.join(cpptraj_test_dir, "Test_Jcoupling",
+            "Karplus.txt"))
+        traj = pt.iterload("./data/tz2.nc", "./data/tz2.parm7")
 
-        # another way, and assert
-        dslist = DataSetList()
-        from pytraj.common_actions import calc_jcoupling
-        d0 = calc_jcoupling(traj, command)
-        d1 = calc_jcoupling(traj, kfile=kfile)
-        aa_eq(d0.values, d1.values)
+        d1 = pt.jcoupling(traj, kfile=kfile)
 
+        # load cpptraj
+        txt = '''
+        parm ./data/tz2.parm7
+        trajin data/tz2.nc
+        jcoupling kfile %s
+        ''' % kfile
+        cpptraj_out = pt.datafiles.load_cpptraj_output(txt)
+        aa_eq(d1.values, cpptraj_out.values)
 
 if __name__ == "__main__":
     unittest.main()
