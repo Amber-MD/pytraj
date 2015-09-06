2fcc43db4feb2580ca21dfe9bdbc9a471d3285eb
Hai Nguyen <hainm.comp@gmail.com>
2015-09-06 04:39:23 -0400

    pt: alias _fast_iter_ptr to iterframe_from_array

--- pytraj/__init__.py
+++ pytraj/__init__.py
@@ -222,6 +222,8 @@ def iterframe(traj, *args, **kwd):
     """
     return traj.iterframe(*args, **kwd)
 
+from ._fast_iterframe import _fast_iterptr as iterframe_from_array
+
 
 def iterchunk(traj, *args, **kwd):
     """
