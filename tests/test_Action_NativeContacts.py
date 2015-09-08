aa5bb01c09620e4f9a27b344b28c06751eabc597
Hai Nguyen <hainm.comp@gmail.com>
2015-09-08 18:59:10 -0400

    pt: assert for nativecontacts

--- tests/test_Action_NativeContacts.py
+++ tests/test_Action_NativeContacts.py
@@ -1,24 +1,21 @@
 from __future__ import print_function
 import unittest
-from pytraj.base import *
-from pytraj import adict
-from pytraj import io as mdio
+import pytraj as pt
 from pytraj.utils import eq, aa_eq
-from pytraj.decorators import no_test, test_if_having, test_if_path_exists
-from pytraj.testing import cpptraj_test_dir, duplicate_traj
-import pytraj.common_actions as pyca
 
 
 class Test(unittest.TestCase):
     def test_0(self):
         # TODO : assert
-        traj = mdio.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
-        ref = mdio.load("data/Tc5b.nat.crd", traj.top)
-        dslist = pyca.native_contacts(traj, top=traj.top, ref=ref)
-        #print(dslist)
+        traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
 
-        dslist = pyca.native_contacts(traj, top=traj.top)
-        #print(dslist)
+        txt = pt.datafiles.tc5b_trajin + '''
+        nativecontacts
+        '''
+        dslist = pt.native_contacts(traj, top=traj.top)
+        cpp = pt.datafiles.load_cpptraj_output(txt)
+
+        aa_eq(dslist.values, cpp.values)
 
 
 if __name__ == "__main__":
