054ed25d754a1bf95ace46fe1fe0dd3e20a7dad4
Hai Nguyen <hainm.comp@gmail.com>
2015-09-11 16:31:08 -0400

    pt: update replicate_cell
    so we can use:
    
        pt.replicate_cell(traj, direction=('001', '0-10'))

--- tests/test_replicate_cell.py
+++ tests/test_replicate_cell.py
@@ -5,7 +5,7 @@ from pytraj.utils import eq, aa_eq
 
 
 class TestReplicateCell(unittest.TestCase):
-    def test_0(self):
+    def test_vs_cpptraj(self):
         traj = pt.iterload("./data/tz2.ortho.nc", "./data/tz2.ortho.parm7")
         txt = '''
         parm ./data/tz2.ortho.parm7
@@ -16,6 +16,12 @@ class TestReplicateCell(unittest.TestCase):
         cpp_out = pt.datafiles.load_cpptraj_output(txt, dtype='state')
         saved_t0 = cpp_out.datasetlist[0]
         aa_eq(saved_t0.xyz, t0.xyz)
+
+    def test_vs_list_tuple(self):
+        traj = pt.iterload("./data/tz2.ortho.nc", "./data/tz2.ortho.parm7")
+        traj0 = pt.replicate_cell(traj, direction='dir 001 dir 0-10')
+        traj1 = pt.replicate_cell(traj, direction=('001', '0-10'))
+        aa_eq(traj0.xyz, traj1.xyz)
 
 
 if __name__ == "__main__":
