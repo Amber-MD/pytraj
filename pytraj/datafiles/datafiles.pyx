c751fe0d801683aa6cad061904fb9a7a7af8ab51
Hai Nguyen <hainm.comp@gmail.com>
2015-09-11 12:16:24 -0400

    pt: add read_data and write_all_datafiles again

--- pytraj/datafiles/datafiles.pyx
+++ pytraj/datafiles/datafiles.pyx
@@ -10,6 +10,11 @@ cdef class DataFile:
         if self.py_free_mem:
             del self.thisptr
 
+    def read_data(self, filenameIn, arglist, DatasetList datasetlist):
+        return self.thisptr.ReadDataIn(filenameIn.encode(),
+               ArgList(arglist).thisptr[0], datasetlist.thisptr[0])
+
+
 cdef class DataFileList:
     def __cinit__(self, py_free_mem=True):
         self.thisptr = new _DataFileList()
@@ -18,3 +23,6 @@ cdef class DataFileList:
     def __dealloc__(self):
         if self.py_free_mem:
             del self.thisptr
+
+    def write_all_datafiles(self):
+        self.thisptr.WriteAllDF()
