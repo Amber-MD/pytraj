d7b8e9550a072714afea1c5b078724ab45d4d0c5
Hai Nguyen <hainm.comp@gmail.com>
2015-09-12 23:31:51 -0400

    pt: add ./installs/install_cpptraj_current_folder.sh

---
+++ installs/install_cpptraj_current_folder.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# PYTRAJHOME is the root folder of `pytraj`
+export PYTRAJHOME=`pwd`
+cd cpptraj/
+export CPPTRAJHOME=`pwd`
+cd $CPPTRAJHOME
+mkdir lib
+
+# turn off openmp. need to install pytraj with opnmp too. Too complicated.
+make libcpptraj -j8 || bash ./configure -shared gnu || bash ./configure -nomathlib -shared gnu || bash ./configure -amberlib -shared gnu || make libcpptraj -j8 || exit 1
+cd $PYTRAJHOME
