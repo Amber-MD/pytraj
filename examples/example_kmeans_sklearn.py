9e2f9f976659f9fb27edd59e3b30a46bb00b0a39
Hai Nguyen <hainm.comp@gmail.com>
2015-09-14 17:08:35 -0400

    update example_kmeans_sklearn.py to make it less scary

--- examples/example_kmeans_sklearn.py
+++ examples/example_kmeans_sklearn.py
@@ -33,20 +33,14 @@ def do_clustering():
                    n_jobs=-1,
                    return_n_iter=False)
 
-    x0 = data[0]
+    # convert 2D to 3D array
+    x0 = pt.tools.as_3darray(data[0])
     print('cluster index: ', data[1].tolist())
 
     # write output, dump all frames to a single pdb file, each snapshot is seperated by
     # 'MODEL' keyword. good for visualizing in VMD.
-
-    with pt.Trajout('output.pdb',
-                    mode='model',
-                    top=t0.top,
-                    overwrite=True) as trajout:
-        x = x0.reshape(n_clusters, t0.n_atoms, 3)
-        # to avoid extra copying, we use _fast_iterptr (for advanced users)
-        for idx, frame in enumerate(pt.iterframe_from_array(x, t0.n_atoms,
-                                                  range(n_clusters))):
-            trajout.write_frame(idx, frame)
+    # first, need to create a frame iterator for ``pytraj.write_traj``
+    frame_iter = pt.iterframe_from_array(x0, t0.n_atoms, range(n_clusters))
+    pt.write_traj('output.pdb', frame_iter, top=t0.top, mode='model', overwrite=True)
 
 do_clustering()
