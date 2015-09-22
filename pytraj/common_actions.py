f0acd72c357d87e5b70d947b26bf138f7d39ba58
Hai Nguyen <hainm.comp@gmail.com>
2015-09-22 17:33:05 -0400

    simplify

--- pytraj/common_actions.py
+++ pytraj/common_actions.py
@@ -1306,8 +1306,7 @@ def calc_pairwise_rmsd(traj=None,
 
     command = command + " crdset _tmp rmsout mycrazyoutput"
 
-    for frame in fi:
-        dslist[0].append(frame)
+    dslist[0].values = fi
 
     act(command, _top, dslist=dslist)
     # remove dataset coords to free memory
@@ -1816,8 +1815,8 @@ def timecorr(vec0, vec1,
 
     cdslist.add_set("vector", "_vec0")
     cdslist.add_set("vector", "_vec1")
-    cdslist[0].from_array_like(np.asarray(vec0).astype('f8'))
-    cdslist[1].from_array_like(np.asarray(vec1).astype('f8'))
+    cdslist[0].values = np.asarray(vec0).astype('f8')
+    cdslist[1].values = np.asarray(vec1).astype('f8')
 
     _order = "order " + str(order)
     _tstep = "tstep " + str(timestep)
@@ -1848,8 +1847,8 @@ def crank(data0, data1, mode='distance', dtype='ndarray'):
     cdslist.add_set("double", "d0")
     cdslist.add_set("double", "d1")
 
-    cdslist[0].from_array_like(np.asarray(data0))
-    cdslist[1].from_array_like(np.asarray(data1))
+    cdslist[0].values = np.asarray(data0)
+    cdslist[1].values = np.asarray(data1)
 
     act = Analysis_CrankShaft()
     command = ' '.join((mode, 'd0', 'd1'))
@@ -1868,8 +1867,8 @@ def cross_correlation_function(data0, data1, dtype='ndarray'):
     cdslist.add_set("double", "d0")
     cdslist.add_set("double", "d1")
 
-    cdslist[0].from_array_like(np.asarray(data0))
-    cdslist[1].from_array_like(np.asarray(data1))
+    cdslist[0].values = np.asarray(data0)
+    cdslist[1].values = np.asarray(data1)
 
     act = analdict['corr']
     act("d0 d1 out _tmp.out", dslist=cdslist)
@@ -1888,7 +1887,7 @@ def auto_correlation_function(data, dtype='ndarray', covar=True):
     cdslist = CpptrajDatasetList()
     cdslist.add_set("double", "d0")
 
-    cdslist[0].from_array_like(np.asarray(data))
+    cdslist[0].values = np.asarray(data)
 
     act = analdict['autocorr']
     command = "d0 out _tmp.out" + _nocovar
@@ -1910,7 +1909,7 @@ def lifetime(data, command="", dtype='ndarray', *args, **kwd):
     else:
         cdslist.add_set("double", "d0")
 
-    cdslist[0].from_array_like(np.asarray(data))
+    cdslist[0].values = np.asarray(data)
 
     act = Analysis_Lifetime()
     command = " ".join((command, "d0"))
