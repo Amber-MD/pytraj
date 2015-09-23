d570e8d266fa490aadc45335b4fe289c72b21027
Hai Nguyen <hainm.comp@gmail.com>
2015-09-23 15:46:31 -0400

    update average

--- tests/test_average_frame.py
+++ tests/test_average_frame.py
@@ -5,47 +5,39 @@ from pytraj.base import *
 from pytraj import adict
 from pytraj.common_actions import *
 from pytraj.testing import aa_eq
-from pytraj.common_actions import get_average_frame
+from pytraj.common_actions import mean_structure
 from pytraj import Trajectory
 
 
 class TestAverageFrame(unittest.TestCase):
     def test_0(self):
         traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
-        act = adict['average']
-        dslist = DatasetList()
-        act("* crdset s1", traj, dslist=dslist)
-        d0 = dslist[0]
-
-        frame = d0.get_frame()
-
         # make sure we DO reproducing cpptraj output
         f_saved = pt.iterload("./data/avg.Tc5b.pdb", traj.top)[0]
-        aa_eq(frame.coords, f_saved.coords)
 
         # shorter
-        from pytraj.common_actions import get_average_frame
-        #frame2 = get_average_frame("", traj, traj.top)
-        frame2 = get_average_frame(traj)
+        from pytraj.common_actions import mean_structure
+        #frame2 = mean_structure("", traj, traj.top)
+        frame2 = mean_structure(traj)
         aa_eq(frame2.coords, f_saved.coords)
 
-        frame3 = get_average_frame(traj=traj)
+        frame3 = mean_structure(traj=traj)
         aa_eq(frame3.coords, f_saved.coords)
 
         # test list
-        frame4 = get_average_frame(traj=[traj, traj[:3]], top=traj.top)
+        frame4 = mean_structure(traj=[traj, traj[:3]], top=traj.top)
 
         # test iter
-        frame5 = get_average_frame(traj=traj(1, 8, 2), top=traj.top)
+        frame5 = mean_structure(traj=traj(1, 8, 2), top=traj.top)
         f5_saved = pt.iterload(
             "./data/avg.Tc5b.frame_2_to_8_skip_2.pdb", traj.top)[0]
         aa_eq(frame5.coords, f5_saved.coords)
 
         # test iter CA
-        frame5 = get_average_frame(traj[[0, 3, 7]], '@CA', top=traj.top)
+        frame5 = mean_structure(traj[[0, 3, 7]], '@CA', top=traj.top)
 
         # test frame_indices
-        frame6 = get_average_frame(traj, mask='@CA', frame_indices=[0, 3, 7])
+        frame6 = mean_structure(traj, mask='@CA', frame_indices=[0, 3, 7])
         aa_eq(frame5.xyz, frame6.xyz)
 
         xyz_0= pt.get_coordinates(traj(1, 8, 2))
@@ -53,7 +45,7 @@ class TestAverageFrame(unittest.TestCase):
         aa_eq(xyz_0, xyz_1)
 
         # test as traj
-        out_traj = get_average_frame(traj, mask='@CA', frame_indices=[0, 3, 7], restype='traj')
+        out_traj = mean_structure(traj, mask='@CA', frame_indices=[0, 3, 7], restype='traj')
         assert isinstance(out_traj, Trajectory), 'must be Trajectory'
         aa_eq(out_traj.xyz, frame6.xyz)
 
@@ -61,8 +53,8 @@ class TestAverageFrame(unittest.TestCase):
     def test_1(self):
         traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         fa = traj[:]
-        f0 = get_average_frame(fa, "@CA")
-        f1 = get_average_frame(fa, "@CA")
+        f0 = mean_structure(fa, "@CA")
+        f1 = mean_structure(fa, "@CA")
         aa_eq(f0.xyz, f1.xyz)
 
 
