5d87544b0fa3b02ef47bf75e41bb668a319a356c
Hai Nguyen <hainm.comp@gmail.com>
2015-09-25 00:36:51 -0400

    update grid

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,26 +1,41 @@
 from __future__ import print_function
 import unittest
-from pytraj.base import *
-from pytraj import adict
-from pytraj import io as mdio
+import pytraj as pt
 from pytraj.utils import eq, aa_eq
-from pytraj.testing import cpptraj_test_dir, duplicate_traj
+from pytraj.testing import cpptraj_test_dir
 import pytraj.common_actions as pyca
 
 
-class Test(unittest.TestCase):
+class TestGrid(unittest.TestCase):
     def test_0(self):
-        traj = mdio.load_sample_data("tz2")[:]
+        from numpy.testing import assert_almost_equal as a_equal
+        from pytraj.math import Grid
+        import numpy as np
+        nx = ny = nz = 3
+        g = Grid(nx, ny, nz)
+        assert g.size == nx ** 3
+        assert g.nx == g.ny == g.nz == nx
+
+        value = 1000.
+        g[0, 0, 0] = value
+        assert g[0, 0, 0] == value
+        assert g._element(0, 0, 0) == value
+
+        np_arr = g.to_ndarray()
+        a_list = g.tolist()
+        a_equal(np_arr, a_list)
+
+
+class TestGridAction(unittest.TestCase):
+    def test_0(self):
+        traj = pt.load_sample_data("tz2")[:]
         traj.autoimage()
         traj.rmsfit(mask=':1-13')
         d = pyca.calc_grid(traj, " 20 0.5 20 0.5 20 0.5 :WAT@O")
-        #print(d[0])
 
-        # iterator
         d = pyca.calc_grid(
             traj(), " 20 0.5 20 0.5 20 0.5 :WAT@O",
             top=traj.top)
-        #print(d[0])
 
 
 if __name__ == "__main__":
