66119a9fa5a41bb36eefaa4e6bda2fa36a7f52d0
Hai Nguyen <hainm.comp@gmail.com>
2015-09-26 01:19:51 -0400

    test_chain: pt.center(traj).autoimage().superpose()

---
+++ tests/test_chain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import unittest
+import pytraj as pt
+from pytraj.utils import eq, aa_eq
+
+
+class TestChainofCommands(unittest.TestCase):
+    def test_center_autoimage_superpose(self):
+        traj = pt.iterload("./data/tz2.ortho.nc", "./data/tz2.ortho.parm7")
+        t0 = traj[:]
+        t1 = traj[:]
+
+        pt.center(t0)
+        pt.autoimage(t0)
+        pt.superpose(t0)
+
+        aa_eq(pt.center(t1).autoimage().superpose().xyz, t0.xyz)
+
+
+if __name__ == "__main__":
+    unittest.main()
