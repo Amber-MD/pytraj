dc9b08d52e2e2a08621791b0193fce7994d136b0
Hai Nguyen <hainm.comp@gmail.com>
2015-09-26 19:20:58 -0400

    pt: add bit

--- pytraj/datasets/DatasetList.pyx
+++ pytraj/datasets/DatasetList.pyx
@@ -241,9 +241,10 @@ cdef class DatasetList:
     def _add_traj(self, TrajectoryCpptraj traj, name='_traj'):
         cdef _MetaData metadata
         metadata.SetName(name.encode())
+        traj.py_free_mem = False
         (<_Dataset*> traj.thisptr).SetMeta(metadata)
-        self.thisptr.AddCopyOfSet(<_Dataset*> traj.thisptr)
-        #self.thisptr.AddSet(<_Dataset*> traj.thisptr)
+        #self.thisptr.AddCopyOfSet(<_Dataset*> traj.thisptr)
+        self.thisptr.AddSet(<_Dataset*> traj.thisptr)
 
     def add_copy_of_set(self, Dataset dset):
         self.thisptr.AddCopyOfSet(dset.baseptr0)
