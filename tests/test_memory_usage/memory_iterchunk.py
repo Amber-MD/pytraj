9ace17762615f676f58683674b318ca7a7414763
Hai Nguyen <hainm.comp@gmail.com>
2015-09-27 22:15:19 -0400

    memory_iterchunk

---
+++ tests/test_memory_usage/memory_iterchunk.py
@@ -0,0 +1,20 @@
+#import unittest # turn off to avoid lengthy non-output
+# that stops travis
+import os
+import pytraj as pt
+from numpy import max
+from memory_profiler import profile, memory_usage
+
+from glob import glob
+
+traj = pt.load_sample_data('tz2')
+# make fake large trajectory from 10 frames
+for _ in range(9):
+    traj.load(traj.filelist)
+
+@profile
+def test_iterchunk(chunksize=100):
+    for chunk in traj.iterchunk(chunksize=chunksize):
+        pass
+
+test_iterchunk(chunksize=100)
