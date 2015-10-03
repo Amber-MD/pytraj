0940f6988fa5e749ebbf57b13ea6041c7c98148c
Hai Nguyen <hainm.comp@gmail.com>
2015-10-03 03:31:57 -0400

    start working with nastruct

--- tests/test_nastruct.py
+++ tests/test_nastruct.py
@@ -1,46 +1,24 @@
+#!/usr/bin/env python
 from __future__ import print_function
 import unittest
-from pytraj.base import *
-from pytraj import adict
-from pytraj import io as mdio
-from pytraj.utils.check_and_assert import assert_almost_equal
+import numpy as np
+import pytraj as pt
 
-
-class Test(unittest.TestCase):
-    # TODO : add assert
-    # what cpptraj dump in dslist?
-
-    def test_0(self):
+class TestNastruct(unittest.TestCase):
+    def test_nupars(self):
         fn = "./data/Test_NAstruct/adh026.3.pdb"
-        traj = mdio.iterload(fn, fn)
+        traj = pt.iterload(fn, fn)
+        dslist = pt.nastruct(traj)
 
-        dslist = DatasetList()
-        act = adict['nastruct']
-        act("", traj, dslist=dslist)
-        act.print_output()
+        text = '''
+        parm "./data/Test_NAstruct/adh026.3.pdb"
+        trajin "./data/Test_NAstruct/adh026.3.pdb"
+        nastruct
+        '''
 
-        d0 = dslist[0]
-
-        for d0 in dslist:
-            pass
-
-        dsmall = dslist.get_dataset(dtype='float')
-        ds_int = dslist.get_dataset(dtype='integer')
-
-        import numpy as np
-        dsmall = np.asarray(dsmall)
-
-    def test_1(self):
-        from pytraj.common_actions import nastruct
-        fn = "./data/Test_NAstruct/adh026.3.pdb"
-        traj = mdio.iterload(fn, fn)
-        dslist = nastruct(traj, dtype='dataset')
-        dsize = dslist.size
-
-        # dummy loops
-        for i in range(100):
-            dslist = nastruct(traj, dtype='dataset')
-            assert dslist.size == dsize
+        state = pt.load_cpptraj_state(text)
+        state.run()
+        print(state.data)
 
 
 if __name__ == "__main__":
