201cd7208f3413d0d7d12225a26bba4ea21c4dc2
Hai Nguyen <hainm.comp@gmail.com>
2015-10-16 14:49:28 -0400

    update to follow cpptraj

--- tests/test_lowestcurve.py
+++ tests/test_lowestcurve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import unittest
+import numpy as np
 import pytraj as pt
 from pytraj.utils import eq, aa_eq
 from pytraj.analyses.CpptrajAnalyses import Analysis_LowestCurve
@@ -8,16 +9,27 @@ from pytraj.datasets import CpptrajDatasetList
 
 
 class TestLowestCurve(unittest.TestCase):
-    @unittest.skip('not sure how this works yet')
+    #@unittest.skip('not sure how this works yet')
     def test_lowestcurve(self):
-        traj = pt.iterload("./data/tz2.nc", "./data/tz2.parm7")
-        data = pt.rmsd(traj, mask='@CA')
-
+        data = np.loadtxt('data/esurf_vs_rmsd.dat').T
         act = Analysis_LowestCurve()
         dslist = CpptrajDatasetList()
+
+        # add rmsd
         dslist.add_new('double', 'myrmsd')
-        dslist[0].data = data
-        act('points 10 myrmsd', dslist=dslist)
+        dslist[0].data = data[0]
+
+        ## add esurf
+        dslist.add_new('double', 'mysurf')
+        dslist[1].data = data[1]
+
+        #dslist.add_new('xymesh', 'mysurf')
+        #dslist[0]._append_from_array(data.T)
+
+        #pt._verbose()
+        #act('points 10 mysurf', dslist=dslist)
+        act('points 10 myrmsd mysurf', dslist=dslist)
+        print([d for d in dslist])
 
 
 if __name__ == "__main__":
