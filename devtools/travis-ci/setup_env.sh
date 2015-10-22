40f6b65994750d4ca6e901f97b856d4d01719d69
Hai Nguyen <hainm.comp@gmail.com>
2015-10-22 16:07:42 -0400

    update names

--- devtools/travis-ci/setup_env.sh
+++ devtools/travis-ci/setup_env.sh
@@ -1,25 +1,19 @@
-# copied from `ParmEd`
-MINICONDA=Miniconda-latest-Linux-x86_64.sh
-MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget http://repo.continuum.io/miniconda/$MINICONDA
-if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
-    echo "Miniconda MD5 mismatch"
-    exit 1
-fi
-bash $MINICONDA -b
+#!/bin/sh
+
+wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b
 
 export PATH=$HOME/miniconda/bin:$PATH
-# install stable version?
+# install stable version
 pip install conda
-# conda update conda --yes
-conda install --yes conda-build jinja2 anaconda-client pip
-#conda config --add channels http://conda.binstar.org/ambermd/
 
-conda create -y -n myenv python=$PYTHON_VERSION numpy cython h5py
+conda install --yes conda-build jinja2 anaconda-client pip
+
+# create myenv
+conda create -y -n myenv python=$PYTHON_VERSION numpy cython h5py mpi4py libnetcdf
 
 source activate myenv
 conda install --yes anaconda-client coverage python-coveralls nose
-conda install --yes scipy mpi4py libnetcdf
 
 # install other packages here
 pip install git+git://github.com/ParmEd/ParmEd
