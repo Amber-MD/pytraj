7967226ca17613082305c4c5d32efeee92ef0119
Hai Nguyen <hainm.comp@gmail.com>
2015-11-02 00:45:21 -0500

    more test for test_super_dispatch.py

--- tests/test_super_dispatch.py
+++ tests/test_super_dispatch.py
@@ -4,8 +4,32 @@ from __future__ import print_function
 import unittest
 import pytraj as pt
 from pytraj.utils import eq, aa_eq
+from pytraj._get_common_objects import _super_dispatch
+from pytraj.externals.six import string_types
 
 class TestSuperDispatch(unittest.TestCase):
+    def setUp(self):
+        self.traj = pt.iterload("./data/tz2.nc", "./data/tz2.parm7")
+
+    def test_naive(self):
+        # make sure to convert int to Frame
+        def func_convert_ref(traj, ref=0, top=None):
+            assert isinstance(ref, pt.Frame)
+        func = _super_dispatch(func_convert_ref)
+        func(self.traj, ref=-2)
+
+        # make sure to insert correct Topology
+        def func_convert_top(traj, top=None):
+            assert isinstance(top, pt.Topology)
+        func = _super_dispatch(func_convert_top)
+        func(self.traj, top=None)
+
+        # make sure to convert array to Amber mask
+        def func_convert_mask_array(traj, top=None, mask=None):
+            assert isinstance(mask, string_types)
+        func = _super_dispatch(func_convert_mask_array)
+        func(self.traj, mask=[0, 3, 7])
+
     def test_super_dispatch(self):
         traj = pt.iterload("./data/tz2.nc", "./data/tz2.parm7")
 
