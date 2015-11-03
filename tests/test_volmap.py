9665040123668d58b53a90d14ecb4ddabe88406a
Hai Nguyen <hainm.comp@gmail.com>
2015-11-02 19:40:23 -0500

    add keywords for volmap

--- tests/test_volmap.py
+++ tests/test_volmap.py
@@ -6,7 +6,7 @@ import pytraj as pt
 from pytraj.testing import aa_eq
 from pytraj.common_actions import calc_volmap as volmap
 
-cm = "0.5 0.5 0.5 :WAT@O buffer 2.0 centermask !:1-13 radscale 1.36"
+cm = "output/dummy.dat 0.5 0.5 0.5 :WAT@O buffer 2.0 centermask !:1-13 radscale 1.36"
 
 txt = """
 parm data/tz2.ortho.parm7
@@ -25,7 +25,9 @@ class TestVolmap(unittest.TestCase):
         cpp_data = state.data[-1].values
 
         traj = traj.superpose(mask=':1-13').center(':1-13 mass origin')
-        ds = volmap(traj, cm)
+        ds = volmap(traj, mask=':WAT@O', grid_spacing='0.5 0.5 0.5', buffer=2.0,
+                    centermask='!:1-13', radscale=1.36)
+                    
         aa_eq(cpp_data, ds)
 
 
