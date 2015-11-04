7ce52ccc45f862f756dc7e745cb88c2b711feb53
Hai Nguyen <hainm.comp@gmail.com>
2015-11-03 19:36:11 -0500

    fix test

--- tests/test_frames/test_Frame_3.py
+++ tests/test_frames/test_Frame_3.py
@@ -1,7 +1,8 @@
 import unittest
+import pytraj as pt
 from pytraj.base import *
+from pytraj.testing import aa_eq
 from pytraj import io as mdio
-from pytraj.utils.check_and_assert import assert_almost_equal
 import numpy as np
 from array import array
 
@@ -11,7 +12,6 @@ class Test(unittest.TestCase):
         traj = mdio.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         frame0 = traj[0]
         #arr0 = np.asarray(frame0)
-        #print(frame0[0, :])
         assert frame0._buffer2d[0, 0] == frame0[0, 0]
         assert frame0._buffer2d[0, 1] == frame0[0, 1]
         assert frame0._buffer2d[0, 2] == frame0[0, 2]
@@ -19,59 +19,40 @@ class Test(unittest.TestCase):
         assert frame0._buffer2d[19, 0] == frame0[19, 0]
         frame0._buffer2d[19, 0] = 1000.
 
-        # make sure changing _buffer2d will update frame0.coords too
+        # make sure changing _buffer2d will update frame0.xyz too
         assert frame0._buffer2d[19, 0] == frame0[19, 0]
         arr0 = np.asarray(frame0._buffer2d)
-        #print(arr0.shape)
         arr0[19] = [200, 300, 400.]
         assert frame0._buffer2d[19, 0] == frame0[19, 0] == arr0[19, 0]
 
-        #print("try to strip atoms")
         frame1 = frame0.copy()
         frame1.strip_atoms(traj.top("!@CA"))
-        #print(frame1._buffer2d.shape)
 
-        _f = frame0.get_subframe("@CA", traj.top)
+        _f = pt.Frame(frame0, traj.top("@CA"))
         CA_2 = _f[2, :]
-        #print(CA_2)
-        #print(frame1[2, :])
-        assert_almost_equal(CA_2, frame1[2, :])
-        assert_almost_equal(frame1.coords[6:9], frame1[2, :])
-        #frame0[:] == frame0.coords
-        #print(frame0._buffer2d[:, 2])
-        #print(array('d', frame0._buffer2d[2:4, 0]))
-        #print(array('d', frame0._buffer2d[2:4, 1]))
-        # #print CA_2
-        assert frame0.buffer1d.shape == (912, )
+        aa_eq(CA_2, frame1[2, :])
+        assert frame0._buffer1d.shape == (912, )
         assert frame0._buffer2d.shape == (304, 3)
-        assert frame0.buffer1d.is_c_contig() == True
-        #print(array('d', frame0._buffer2d[0, :]))
+        assert frame0._buffer1d.is_c_contig() == True
         frame0._buffer2d[1:3, 0] = array('d', [1., 2.])
         #assert frame0[1:3, 0] == array('d', frame0._buffer2d[1:3, 0])
-        #print(array('d', frame0._buffer2d[1:3, 0]))
-        #print(frame0[1:3, 0])
-        assert_almost_equal(
+        aa_eq(
             frame0[1:3, 0], array('d', frame0._buffer2d[1:3, 0]))
-        assert_almost_equal(
+        aa_eq(
             frame0._buffer2d[1:3, 0], array('d', frame0._buffer2d[1:3, 0]))
 
     def test_1(self):
         traj = mdio.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         frame0 = traj[0]
-        #print(frame0[1:5, 2])
-        #print(np.asarray(frame0._buffer2d[1:5, :]))
         frame0[1:5, 2] = list(range(100, 104))
         frame0[0, :] = list(range(3))
-        #print(frame0._buffer2d[1:5, :])
-        #print(np.asarray(frame0[1:5, :]))
-        #print(list(frame0[1:5, :]))
 
     def test_magic_methods(self):
         traj = mdio.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         frame0 = traj[0]
         frame1 = frame0.copy()
         frame1 += frame1
-        assert 2 * frame0.coords[0] == frame1.coords[0]
+        aa_eq(2 * frame0.xyz[0], frame1.xyz[0])
 
 
 if __name__ == "__main__":
