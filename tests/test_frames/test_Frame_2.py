f4361fbc2bc442a04d1f9bd33ced05156a5a3e02
Hai Nguyen <hainm.comp@gmail.com>
2015-11-03 19:55:08 -0500

    fix

--- tests/test_frames/test_Frame_2.py
+++ tests/test_frames/test_Frame_2.py
@@ -1,8 +1,8 @@
 import unittest
 from array import array
 import numpy as np
-#from numpy.testing import assert_almost_equal
 from pytraj.utils.check_and_assert import assert_almost_equal
+from pytraj.testing import aa_eq
 from pytraj import Frame
 from pytraj.base import *
 from pytraj import io as mdio
@@ -12,7 +12,7 @@ SMALL = 1E-6
 N_ATOMS = 10
 FRAME = Frame(N_ATOMS)
 arr = np.arange(3 * N_ATOMS)
-FRAME.set_from_crd(arr)
+FRAME.xyz[:] = arr.reshape(N_ATOMS, 3)
 FRAME_orig = FRAME.copy()
 
 
@@ -25,23 +25,15 @@ class TestFrame(unittest.TestCase):
         # make sure 0-th frame does not change
         frame0 = traj[0]
         frame0new = trajnew[0]
-        assert (frame0.coords == frame0new.coords) == True
-        #print(frame0[:10])
-        #print(frame0new[:10])
+        aa_eq(frame0.xyz, frame0new.xyz)
 
         frame1 = traj[1]
         frame1new = trajnew[1]
-        #print(frame1[:10])
-        #print(frame1new[:10])
-        assert (frame1.coords == frame1new.coords) == False
 
         # try do-fitting from Python
         # not right yet
         rmsd, mat, v1, v2 = frame1.rmsd(frame0, get_mvv=True)
-        #print(rmsd)
         frame1.trans_rot_trans(v1, mat, v2)
-        #print(frame1[:10])
-        #print(frame1.rmsd_nofit(frame1new))
         assert frame1.rmsd(frame1new) < 1E-3
 
     def test_1(self):
@@ -49,15 +41,8 @@ class TestFrame(unittest.TestCase):
         trajnew = mdio.iterload(
             "./data/md1_prod.fit_to_first.Tc5b.x", "./data/Tc5b.top")
         frame0 = traj[0]
-        assert frame0[0, 2] == frame0.atoms(0)[2]
-        assert_almost_equal(frame0[0, :], frame0.atoms(0))
-        #print(frame0[0, :])
-        #print(frame0.atoms(0))
-
-        #print(frame0[:, 2])
-        framesub = frame0.get_subframe("@CA", traj.top)
-        assert framesub.n_atoms == 20
-        #print(framesub[19, :])
+        assert frame0[0, 2] == frame0.atom(0)[2]
+        assert_almost_equal(frame0[0, :], frame0.atom(0))
 
 
 if __name__ == "__main__":
