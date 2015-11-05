3902ea342e180d81cbe5b7bcb184f4557ab21319
Hai Nguyen <hainm.comp@gmail.com>
2015-11-04 19:15:08 -0500

    update run

--- examples/mpi_examples/run_mpi_examples.sh
+++ examples/mpi_examples/run_mpi_examples.sh
@@ -4,4 +4,5 @@ cores=$1
 mpirun -n $cores python mpi_calc_molsurf_0_single_traj.py || exit 1
 mpirun -n $cores python mpi_load_batch.py || exit 1
 mpirun -n $cores python mpi_search_hbonds_single_traj.py || exit 1
+mpirun -n $cores python mpi_pysander.py || exit 1
 echo "mpi ok"
