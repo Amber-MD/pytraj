f2d2102146c3b53ab7a725cdd40ca725fe7a1dfb
Hai Nguyen <hainm.comp@gmail.com>
2015-11-09 17:51:04 -0500

    more doc

--- pytraj/core/fake_residue.py
+++ pytraj/core/fake_residue.py
@@ -1,5 +1,15 @@
 
 class SimplifiedResidue(object):
+    '''SimplifiedResidue, only has info about name, index, start and end atom index
+
+    Examples
+    --------
+    >>> import pytraj as pt
+    >>> top = pt.datafiles.load_tz2_ortho().top
+    >>> for atom in top.atoms:
+    ...     print(atom.name, atom.residue.name, atom.residue.index)
+
+    '''
     def __init__(self, resname, resid, atoms, start, end):
         self.resname = resname
         self.name = self.resname
