b97cbbba73ac6fcdd4844a98e598b001008647d0
Hai Nguyen <hainm.comp@gmail.com>
2015-11-14 21:26:35 -0500

    test closest

--- tests/test_closest.py
+++ tests/test_closest.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 import unittest
+import numpy as np
 import pytraj as pt
 from pytraj.utils import eq, aa_eq
 
@@ -14,8 +15,25 @@ class TestClosest(unittest.TestCase):
 
         traj = pt.iterload("./data/tz2.ortho.nc", "./data/tz2.ortho.parm7")
         fi, top = pt.closest(traj)
+
+        coords = []
         for frame in fi:
+            coords.append(frame.xyz.copy())
             assert isinstance(frame, pt.Frame), 'must be Frame'
+
+        # make a Trajectory
+        fi, top = pt.closest(traj)
+        xyz = pt.get_coordinates(fi)
+        t0 = pt.Trajectory(xyz=xyz, top=top)
+        aa_eq(np.array(coords), t0.xyz)
+
+        # test write to disk
+        fi, top = pt.closest(traj)
+
+        pt.write_traj('output/fi.nc', fi, top=top)
+        # load back
+        t1 = pt.load('output/fi.nc', top=top)
+        aa_eq(t0.xyz, t1.xyz)
 
 if __name__ == "__main__":
     unittest.main()
