bfdd0127720646edba5f0e5d410d505c8aa3d431
Hai Nguyen <hainm.comp@gmail.com>
2015-11-16 13:58:09 -0500

    more test for atomiccorr

--- tests/test_atomiccorr.py
+++ tests/test_atomiccorr.py
@@ -17,6 +17,15 @@ class TestAtomicCorr(unittest.TestCase):
         data = pt.atomiccorr(traj, ':1-13', byres=True)
         aa_eq(data, state.data[1].values)
 
+    def test_issue_172_cpptraj(self):
+        '''Internal Error: Attempting to assign to Frame with external memory
+        '''
+        traj = pt.iterload("./data/tz2.nc", "./data/tz2.parm7")
+
+        arr_out_of_memory = pt.atomiccorr(traj(0, 8, 2), '@CA')
+        arr_in_memory = pt.atomiccorr(traj[0: 8: 2], '@CA')
+        aa_eq(arr_out_of_memory, arr_in_memory)
+
 
 if __name__ == "__main__":
     unittest.main()
