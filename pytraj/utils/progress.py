133c3ae4cb346423edf62068e724d21ee08bc592
Hai Nguyen <hainm.comp@gmail.com>
2015-11-19 16:46:56 -0500

    no covera for progress bar since we need notebook

---
+++ pytraj/utils/progress.py
@@ -0,0 +1,20 @@
+import uuid
+from IPython.display import HTML, Javascript, display
+
+divid = str(uuid.uuid4())
+
+pb = HTML(
+"""
+<div style="border: 1px solid black; width:500px">
+  <div id="%s" style="background-color:blue; width:0%%">&nbsp;</div>
+</div> 
+""" % divid)
+
+def init_display():
+    display(pb)
+
+def make_bar(idx, max_frames):
+    '''work with jupyter notebook.
+    '''
+    s = "$('div#%s').width('%i%%')" % (divid, 100*(idx+1)/max_frames)
+    display(Javascript(s))
