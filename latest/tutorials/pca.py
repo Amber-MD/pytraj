8c3c5669f2591cb9457cd36e598cba73087b832a
Hai Nguyen <hainm.comp@gmail.com>
2015-11-24 15:06:53 -0500

    update doc

---
+++ latest/tutorials/pca.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# ### load trajectory
+
+# In[9]:
+
+# load pytraj
+import pytraj as pt
+
+# load trajectory to memory
+traj = pt.load('tz2.nc', 'tz2.parm7')
+
+
+# ### Get the data
+
+# In[10]:
+
+# compute pca
+data = pt.pca(traj, mask='!@H=', n_vecs=2)
+
+print('projection values of each frame to first mode = {} \n'.format(data[0][0]))
+print('projection values of each frame to second mode = {} \n'.format(data[0][1]))
+print('eigvenvalues of first two modes', data[1][0])
+print('eigvenvalues of first two modes', data[1][0])
+
+
+# ### plot
+
+# In[14]:
+
+get_ipython().magic('matplotlib inline')
+get_ipython().magic("config InlineBackend.figure_format = 'retina'  # high resolution")
+import matplotlib
+matplotlib.rcParams['savefig.dpi'] = 2 * matplotlib.rcParams['savefig.dpi'] # larger image
+
+projection_data = data[0]
+from matplotlib import pyplot as plt
+
+plt.scatter(projection_data[0], projection_data[1], marker='o', c=range(traj.n_frames), alpha=0.5)
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+cbar = plt.colorbar()
+cbar.set_label('frame #')
+
