d7c9128835ba8464f0364ec54b3433a0f4af034e
Hai Nguyen <hainm.comp@gmail.com>
2015-12-03 16:01:17 -0500

    remove unused code

--- pytraj/parallel/__init__.py
+++ pytraj/parallel/__init__.py
@@ -45,10 +45,7 @@ def _worker_actlist(rank,
     # need to make a copy if lines since python's list is dangerous
     # it's easy to mess up with mutable list
     # do not use lines.copy() since this is not available in py2.7
-    if kwd is not None:
-        frame_indices = kwd.pop('frame_indices', None)
-    else:
-        frame_indices = None
+    frame_indices = kwd.pop('frame_indices', None)
 
     if frame_indices is None:
         my_iter = traj._split_iterators(n_cores, rank=rank)
@@ -87,35 +84,6 @@ def _worker_actlist(rank,
         raise ValueError('must use dtype="dict"')
 
 
-def _worker_state(rank, n_cores=1, traj=None, lines=[], dtype='dict'):
-    # need to make a copy if lines since python's list is dangerous
-    # it's easy to mess up with mutable list
-    # do not use lines.copy() since this is not available in py2.7
-    my_lines = [line for line in lines]
-    from pytraj.utils import split_range
-    from pytraj.core.cpp_core import _load_batch
-
-    mylist = split_range(n_cores, 0, traj.n_frames)[rank]
-    start, stop = mylist
-    crdframes_string = 'crdframes ' + ','.join((str(start + 1), str(stop)))
-
-    for idx, line in enumerate(my_lines):
-        if not line.lstrip().startswith('reference'):
-            my_lines[idx] = ' '.join(('crdaction traj', line, crdframes_string
-                                      ))
-
-    my_lines = ['loadtraj name traj', ] + my_lines
-
-    state = _load_batch(my_lines, traj)
-
-    state.run()
-    if dtype == 'dict':
-        # exclude DatasetTopology and TrajectoryCpptraj
-        return (rank, state.data[2:].to_dict())
-    else:
-        raise ValueError('must use dtype="dict"')
-
-
 def _load_batch_pmap(n_cores=4,
                      lines=[],
                      traj=None,
@@ -128,8 +96,6 @@ def _load_batch_pmap(n_cores=4,
     '''
     if mode == 'multiprocessing':
         from multiprocessing import Pool
-        # pfuncs = partial(_worker_state, n_cores=n_cores, traj=traj, dtype=dtype,
-        #        lines=lines, ref=ref)
         pfuncs = partial(_worker_actlist,
                          n_cores=n_cores,
                          traj=traj,
@@ -146,7 +112,6 @@ def _load_batch_pmap(n_cores=4,
         from mpi4py import MPI
         comm = MPI.COMM_WORLD
         rank = comm.rank
-        # data_chunk = _worker_state(rank, n_cores=size, traj=traj, lines=lines, dtype=dtype)
         data_chunk = _worker_actlist(rank=rank,
                                      n_cores=n_cores,
                                      traj=traj,
