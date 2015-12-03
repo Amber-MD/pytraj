562ee8df683827f2fc6ec6f7669208b6109f3082
Hai Nguyen <hainm.comp@gmail.com>
2015-12-03 16:27:02 -0500

    add test_template_for_doc.py

---
+++ tests/test_template_for_doc.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import unittest
+import pytraj as pt
+from pytraj.utils import eq, aa_eq
+from pytraj import utils
+import doctest
+from pytraj.compat import PY3
+from pytraj import testing
+from pytraj.datafiles import load_samples
+from pytraj.externals import get_pysander_energies
+from pytraj import frame, datafiles, cluster, nucleic_acid_analysis
+from pytraj.core import action_list
+
+
+try:
+    import sander
+    has_sander = True
+except ImportError:
+    has_sander = False
+
+doctest.DONT_ACCEPT_BLANKLINE = False
+
+
+def get_total_errors(modules):
+    return sum([doctest.testmod(mod).failed for mod in modules])
+
+
+class TestDoc(unittest.TestCase):
+    '''testing for light modules
+    '''
+
+    def test_doc(self):
+        modules = [pt.matrix,]
+        assert get_total_errors(
+            modules) == 0, 'doctest: failed_count must be 0'
+
+
+if __name__ == "__main__":
+    unittest.main()
