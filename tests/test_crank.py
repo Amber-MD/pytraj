84ada5a7fe583f766065f611e84dc7fa07d49743
Hai Nguyen <hainm.comp@gmail.com>
2015-12-03 15:17:39 -0500

    crank

--- tests/test_crank.py
+++ tests/test_crank.py
@@ -4,16 +4,24 @@ import pytraj as pt
 from pytraj.utils import eq, aa_eq
 import pytraj.common_actions as pyca
 
+cm = '''
+parm data/tz2.parm7 
+trajin data/tz2.nc
+dihedral phi2 :1@C :2@N :2@CA :2@C type phi
+dihedral phi3 :2@C :3@N :3@CA :3@C type phi
+analyze crankshaft angle phi2 phi3
+'''
 
-class Test(unittest.TestCase):
+class TestCrank(unittest.TestCase):
 
-    def test_0(self):
-        # TODO: assert
-        traj = pt.iterload("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
-
-        d0 = pt.distance(traj, [0, 50])[0]
-        d1 = pt.distance(traj, [10, 100])[0]
-        # too verbose
+    def test_crank(self):
+        traj = pt.iterload("data/tz2.nc", "data/tz2.parm7")
+        dihedrals = pt.dihedral(traj, [':1@C :2@N :2@CA :2@C', ':2@C :3@N :3@CA :3@C'])
+        state = pt.load_cpptraj_state(cm)
+        state.run()
+        # TODO: assert please
+        # cpptraj does not dump data to Dataset
+        pt.crank(dihedrals[0], dihedrals[1], mode='angle')
 
 
 if __name__ == "__main__":
