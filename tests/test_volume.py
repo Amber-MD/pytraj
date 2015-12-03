03ad76ecbc41b620cc6a80af17cd8cce3e86f98c
Hai Nguyen <hainm.comp@gmail.com>
2015-12-03 14:23:35 -0500

    add volume

---
+++ tests/test_volume.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import unittest
+import pytraj as pt
+from pytraj.utils import eq, aa_eq
+
+
+class TestVolume(unittest.TestCase):
+
+    def test_volume(self):
+        traj = pt.iterload("data/tz2.ortho.nc", "data/tz2.ortho.parm7")
+
+
+if __name__ == "__main__":
+    unittest.main()
