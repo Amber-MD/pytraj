ea4e7b3b757657fc3c06743cc82f49b92e95e5fc
Hai Nguyen <hainm.comp@gmail.com>
2015-12-07 16:44:39 -0500

    test read gaussian + skip cover if not has amberhome

--- tests/test_read_gaussian.py
+++ tests/test_read_gaussian.py
@@ -5,12 +5,23 @@ from pytraj.utils import eq, aa_eq
 import pytraj.common_actions as pyca
 
 
-class Test(unittest.TestCase):
+try:
+    import cclib
+    has_cclib = True
+except ImportError:
+    has_cclib = False
 
-    def test_0(self):
-        # TODO : assert
-        traj = pt.tools.read_gaussian_output("./data/gaussian/GF2.log",
-                                             "./data/gaussian/GF2.pdb")
+class TestReadGaussianOutput(unittest.TestCase):
+
+    @unittest.skipIf(not has_cclib, 'skip: does not have cclib')
+    def test_read_gaussian_output(self):
+        import cclib
+        filename = "./data/gaussian/GF2.log"
+        gau = cclib.parser.Gaussian(filename)
+        go = gau.parse()
+
+        traj = pt.tools.read_gaussian_output(filename, "./data/gaussian/GF2.pdb")
+        aa_eq(traj.xyz, go.atomcoords)
 
 
 if __name__ == "__main__":
