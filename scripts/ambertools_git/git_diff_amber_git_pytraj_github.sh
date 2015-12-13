001183271b4dc08a5ff3d6f6cff89b6ba8740566
Hai Nguyen <hainm.comp@gmail.com>
2015-12-13 16:32:58 -0500

    update git diff

--- scripts/ambertools_git/git_diff_amber_git_pytraj_github.sh
+++ scripts/ambertools_git/git_diff_amber_git_pytraj_github.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-pytrajamber=$1
-pytrajgit=$2
-git diff --stat $pytrajamber:AmberTools/src/pytraj pytraj_github/$pytrajgit
+git diff pytraj_github/master master:AmberTools/src/pytraj/ --stat --color
