25d2f5126c544511b00f5b03841376c179af20e2
Hai Nguyen <hainm.comp@gmail.com>
2016-01-04 13:38:28 -0500

    remove http again

--- examples/example_load_mol2_as_trajectory.py
+++
@@ -1,9 +0,0 @@
-# require ParmEd
-# pip install git+git://github.com/ParmEd/ParmEd
-
-import pytraj as pt
-
-# load as trajectory
-traj = pt.load(
-    "http://ambermd.org/tutorials/basic/tutorial4/files/sustiva.mol2")
-print(traj)
