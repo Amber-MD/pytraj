4217e9468a972ac3477a124c692edc0a298bebc5
Hai Nguyen <hainm.comp@gmail.com>
2016-02-25 16:24:30 -0500

    try

--- devtools/travis-ci/install_pytraj.sh
+++ devtools/travis-ci/install_pytraj.sh
@@ -4,13 +4,6 @@
 # python setup.py install --amber-release
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    # git clone https://github.com/Amber-MD/cpptraj
-    git clone https://github.com/hainm/cpptraj
-    cd cpptraj
-    git checkout hai_mactravis
-    ./configure $BUILD_FLAGS clang
-    make libcpptraj -j4
-    cd ../
     python setup.py install --disable-openmp
 else
     python setup.py install
