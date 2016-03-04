88b9e9650d793c090b31bec0f27ecde9cc8dc7d7
Hai Nguyen <hainm.comp@gmail.com>
2016-03-04 18:56:07 -0500

    add linux

--- devtools/travis-ci/setup_env.sh
+++ devtools/travis-ci/setup_env.sh
@@ -9,23 +9,21 @@ fi
 bash miniconda.sh -b
 
 export PATH=$HOME/miniconda/bin:$PATH
-# install stable version
-pip install conda
-
 conda install --yes conda-build jinja2 anaconda-client pip
 
 # create myenv
-conda create -y -n myenv python=$PYTHON_VERSION numpy cython h5py mpi4py libnetcdf
+# conda create -y -n myenv python=$PYTHON_VERSION numpy cython h5py mpi4py libnetcdf
+conda create -y -n myenv python=$PYTHON_VERSION numpy cython h5py libnetcdf
 
 source activate myenv
 conda install --yes anaconda-client coverage pyflakes
 
 # install other packages here
-conda install mdtraj -c omnia --yes
-pip install coveralls
-pip install coverage
-pip install nose
-pip install git+git://github.com/ParmEd/ParmEd
-pip install memory_profiler
-pip install psutil
-pip install cclib
+# conda install mdtraj -c omnia --yes
+# pip install coveralls
+# pip install coverage
+# pip install nose
+# pip install git+git://github.com/ParmEd/ParmEd
+# pip install memory_profiler
+# pip install psutil
+# pip install cclib
