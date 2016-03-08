0098968e7e6a5b584e6eafb3654d3612326dce4c
Hai Nguyen <hainm.comp@gmail.com>
2016-03-08 03:29:34 -0500

    fix

--- devtools/travis-ci/install_pytraj.sh
+++ devtools/travis-ci/install_pytraj.sh
@@ -13,7 +13,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     echo "update @rpath for libcpptraj"
     for x in $(find build/ -name '*.so'); do
         name="libcpptraj.dylib"
-        install_name_tool -change $name $CPPTRAJHOME/lib/$name $x
+        # install_name_tool -change $name $CPPTRAJHOME/lib/$name $x
         # install_name_tool -change "@rpath/$name" "$CPPTRAJHOME/lib/$name" $x
         # install_name_tool -id "@rpath/$name" "$CPPTRAJHOME/lib/$name"
     done
@@ -21,7 +21,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     echo "install pytraj"
     python setup.py install --disable-openmp
     echo "copying libcpptraj"
-    sudo cp $CPPTRAJHOME/lib/libcpptraj.dylib /usr/local/lib/
+    # sudo cp $CPPTRAJHOME/lib/libcpptraj.dylib /usr/local/lib/
 else
     python setup.py install
 fi
