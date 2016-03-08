b097972dbc537036b04da86895722f66b45913fc
Hai Nguyen <hainm.comp@gmail.com>
2016-03-08 01:06:52 -0500

    fix

--- devtools/travis-ci/install_pytraj.sh
+++ devtools/travis-ci/install_pytraj.sh
@@ -3,12 +3,13 @@
 # create this file to hide output
 # python setup.py install --amber-release
 
-cd cpptraj
-export CPPTRAJHOME=`pwd`
-cd ..
-
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     python setup.py build --disable-openmp
+
+    cd cpptraj
+    export CPPTRAJHOME=`pwd`
+    cd ..
+
     for x in $(find build/ -name '*.so'); do
         name = "libcpptraj.dylib"
         install_name_tool -change "@rpath/$name" "$CPPTRAJHOME/lib/$name" $x
