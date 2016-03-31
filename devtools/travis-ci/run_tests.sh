349025b065f93e4d042d5f354d98fe22fc569081
Hai Nguyen <hainm.comp@gmail.com>
2016-03-31 17:27:06 -0400

    fix

--- devtools/travis-ci/run_tests.sh
+++ devtools/travis-ci/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-sh devtools/travis-ci/pyflakes_check.sh
+sh devtools/travis-ci/pyflakes_check.sh || exit 1
 
 PLATFORM=`python -c 'import sys; print(sys.platform)'`
 echo "PLATFORM =" $PLATFORM
@@ -15,28 +15,30 @@ export AMBERHOME=`pwd`
 # go back to tests folder
 cd ..
 
+if [ "$PLATFORM" = "linux" ]; then
+    export LD_LIBRARY_PATH=`pwd`"/cpptraj/lib":$LD_LIBRARY_PATH
+
 # print info
-python run_simple_test.py
+python run_simple_test.py || exit 1
 
 # run tests
 if [ "$PLATFORM" = "linux" ]; then
-    export LD_LIBRARY_PATH=`pwd`"/cpptraj/lib":$LD_LIBRARY_PATH
-    nosetests --with-coverage --cover-package pytraj -vs test_*.py */test_*.py
+    nosetests --with-coverage --cover-package pytraj -vs test_*.py */test_*.py || exit 1
     unset CPPTRAJHOME # for pytraj.tools coverage
-    nosetests --with-coverage --cover-package pytraj -vs test_docs.py
+    nosetests --with-coverage --cover-package pytraj -vs test_docs.py || exit 1
 else
     # osx (window too?), minimal tests
-    nosetests -vs test_*.py */test_*.py
+    nosetests -vs test_*.py */test_*.py || exit 1
 fi
 
 # run examples
 cd ../examples
-python ./run_examples.py
+python ./run_examples.py || exit 1
 cd mpi_examples
 
 # only do mpi test for linux
 if [ "$PLATFORM" = "linux" ]; then
-    sh run_mpi_examples.sh 4 
+    sh run_mpi_examples.sh 4 || exit 1
 fi
 
 # go back to pytraj home
