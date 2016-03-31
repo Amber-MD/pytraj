7c114656befbfc319ad75155c77e522b453fbe2e
Hai Nguyen <hainm.comp@gmail.com>
2016-03-31 15:17:53 -0400

    update test

--- devtools/travis-ci/run_tests.sh
+++ devtools/travis-ci/run_tests.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 python -c 'import pytraj as pt'
+cd tests
 python ./run_simple_test.py
 
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+PLATFORM=`python -c 'import sys; print(sys.platform)'`
+echo "PLATFORM =" $PLATFORM
+
+if [ "$PLATFORM" = "linux" ]; then
     nosetests --with-coverage --cover-package pytraj -vs test_*.py */test_*.py
     unset CPPTRAJHOME # for pytraj.tools coverage
     nosetests --with-coverage --cover-package pytraj -vs test_docs.py
@@ -13,5 +17,5 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     sh run_mpi_examples.sh 4 
 else
     # osx (window too?), minimal tests
-    echo "osx test"
-    nosetests -vs .
+    nosetests -vs test_*.py */test_*.py
+fi
