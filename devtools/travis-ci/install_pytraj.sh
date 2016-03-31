adfde1f80ce4f437121162a022d888f5c0a27c7e
Hai Nguyen <hainm.comp@gmail.com>
2016-03-31 18:46:44 -0400

    disable openmp

--- devtools/travis-ci/install_pytraj.sh
+++ devtools/travis-ci/install_pytraj.sh
@@ -7,7 +7,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     python setup.py install --disable-openmp
 else
     if [ "$COMPILER" == "clang" ]; then
-        CC=clang CXX=clang++ python setup.py install
+        CC=clang CXX=clang++ python setup.py install --disable-openmp
     else
         python setup.py install
     fi
