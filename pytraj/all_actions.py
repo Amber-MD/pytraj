3d16df106c3f6fcc7207d5da6e47e93dfeda5a27
Hai Nguyen <hainm.comp@gmail.com>
2016-04-21 23:47:40 -0400

    minor

--- pytraj/all_actions.py
+++ pytraj/all_actions.py
@@ -3035,8 +3035,8 @@ def strip(obj, mask):
     Notes
     -----
     This method is trying to be smart. If you give it a in-memory Trajectory, it will
-    return a corresponding in-memory Trajectory. If you give out-of-memory TrajectoryIterator,
-    it will give you a FrameIterator object (out-of-memory). If you give it a Topology, it will
+    return a corresponding in-memory Trajectory. If you give it an out-of-memory TrajectoryIterator,
+    it will give you a corresponding FrameIterator object (out-of-memory). If you give it a Topology, it will
     return a new stripped Topology.
 
     Examples
