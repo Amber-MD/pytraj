1d44a1ca591e5176958700f6ffa74da1c65cf0a5
Hai Nguyen <hainm.comp@gmail.com>
2016-04-23 18:25:15 -0400

    update doc

--- scripts/check_libs.py
+++ scripts/check_libs.py
@@ -6,6 +6,7 @@ import warnings
 warnings.filterwarnings('ignore', category=UserWarning)
 
 for module in ['sphinx', 'sander', 'parmed', 'numpy', 'mpi4py',
+               'sklearn',
                'pandas', 'matplotlib', 'seaborn', 'sphinx_bootstrap_theme']:
     try:
         lib = __import__(module)
--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 conda install sphinx numpy matplotlib seaborn pandas jupyter notebook runipy --yes
+conda install scikit-learn --yes
 pip install sphinx_bootstrap_theme mpi4py
