bf5dc182b7a2a4adf63a6ba2e1510c882c72d270
Hai Nguyen <hainm.comp@gmail.com>
2016-05-17 19:39:16 -0400

    wavelet

--- tests/test_wavelet.py
+++ tests/test_wavelet.py
@@ -6,13 +6,30 @@ import pytraj as pt
 from pytraj.utils import eq, aa_eq
 
 
-class Test(unittest.TestCase):
+class TestWavelet(unittest.TestCase):
 
-    def test_0(self):
+    def test_wavelet(self):
         traj = pt.load("./data/DPDP.nc", "data/DPDP.parm7")
         traj.superpose('@C,CA,N', ref=0)
-        data = pt.all_actions._wavelet(traj, 'nb 10 s0 2 ds 0.25 type morlet correction 1.01 chival 0.25 :1-22 name DPDP cluster minpoints 66 epsilon 10.0')
-        print(data)
+        command = 'name mywave nb 10 s0 2 ds 0.25 type morlet correction 1.01 chival 0.25 :1-22 cluster minpoints 66 epsilon 10.0'
+        data = pt.wavelet(traj, command)
+
+        state = pt.load_cpptraj_state("""
+        parm data/DPDP.parm7
+        trajin data/DPDP.nc
+        wavelet name mywave nb 10 s0 2 ds 0.25 type morlet correction 1.01 chival 0.25 :1-22 cluster minpoints 66 epsilon 10.0
+        """)
+
+        state.run()
+
+        # ignore Topology, COORDS dataset
+        cpp_data = state.data[2:].to_dict()
+        print(cpp_data.keys())
+
+        for (k0, v0) in data.items():
+            print(v0, cpp_data[k0])
+            print(pt.tools.rmsd(v0, cpp_data[k0]), flatten=True)
+
         
 if __name__ == "__main__":
     unittest.main()
