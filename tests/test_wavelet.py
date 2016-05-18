8a31f6db6d7c9664c9901f53d3ba067cd275e1c5
Hai Nguyen <hainm.comp@gmail.com>
2016-05-17 21:09:48 -0400

    fix test

--- tests/test_wavelet.py
+++ tests/test_wavelet.py
@@ -4,12 +4,14 @@ from __future__ import print_function
 import unittest
 import pytraj as pt
 from pytraj.utils import eq, aa_eq
+import sys
+import nose.tools as nt
 
 
 class TestWavelet(unittest.TestCase):
 
     def test_wavelet(self):
-        traj = pt.load("./data/DPDP.nc", "data/DPDP.parm7")
+        traj = pt.load("data/DPDP.nc", "data/DPDP.parm7")
         traj.superpose('@C,CA,N', ref=0)
         command = 'name mywave nb 10 s0 2 ds 0.25 type morlet correction 1.01 chival 0.25 :1-22 cluster minpoints 66 epsilon 10.0'
         data = pt.wavelet(traj, command)
@@ -17,19 +19,20 @@ class TestWavelet(unittest.TestCase):
         state = pt.load_cpptraj_state("""
         parm data/DPDP.parm7
         trajin data/DPDP.nc
+        rms @C,CA,N first
         wavelet name mywave nb 10 s0 2 ds 0.25 type morlet correction 1.01 chival 0.25 :1-22 cluster minpoints 66 epsilon 10.0
         """)
 
         state.run()
 
         # ignore Topology, COORDS dataset
-        cpp_data = state.data[2:].to_dict()
-        print(cpp_data.keys())
+        cpp_data = state.data[3:].to_dict()
+        nt.assert_equal(sorted(cpp_data.keys()),
+                        sorted(data.keys()))
 
         for (k0, v0) in data.items():
-            print(v0, cpp_data[k0])
-            print(pt.tools.rmsd(v0, cpp_data[k0]), flatten=True)
-
+            aa_eq(v0, cpp_data[k0])
         
+
 if __name__ == "__main__":
     unittest.main()
