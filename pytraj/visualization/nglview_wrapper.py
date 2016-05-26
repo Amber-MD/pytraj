2c9ce108494e607232c1164ab9edd063f2fcbf85
Hai Nguyen <hainm.comp@gmail.com>
2016-05-26 11:59:55 -0400

    update

--- pytraj/visualization/nglview_wrapper.py
+++ pytraj/visualization/nglview_wrapper.py
@@ -2,7 +2,7 @@ from nglview import NGLWidget, PyTrajTrajectory
 
 class TrajectoryViewer(NGLWidget):
 
-    def add_pytraj_trajectory(self, traj):
+    def add(self, traj):
         """add pytraj's Trajectory or TrajectoryIterator object
         """
         self.add_trajectory(PyTrajTrajectory(traj))
