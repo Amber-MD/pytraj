72c82a8072c79d2e5627e27d7f9d9080c875a645
Hai Nguyen <hainm@users.noreply.github.com>
2016-05-27 02:15:55 -0400

    trajectory viewer, add + notebook (#1253)
    * add notebook
    
    * moreview

--- pytraj/visualization/nglview_wrapper.py
+++ pytraj/visualization/nglview_wrapper.py
@@ -2,7 +2,13 @@ from nglview import NGLWidget, PyTrajTrajectory
 
 class TrajectoryViewer(NGLWidget):
 
-    def add(self, traj):
+    def add(self, traj, *args, **kwargs):
         """add pytraj's Trajectory or TrajectoryIterator object
         """
-        self.add_trajectory(PyTrajTrajectory(traj))
+        from pytraj import Trajectory, TrajectoryIterator
+
+        if isinstance(traj, (Trajectory, TrajectoryIterator)):
+            self.add_trajectory(PyTrajTrajectory(traj), *args, **kwargs)
+        else:
+            # try with your own risk
+            self.add_component(traj, *args, **kwargs)
