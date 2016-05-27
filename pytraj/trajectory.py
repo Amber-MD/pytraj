317fa3e8010017580a0059977169e669d5b6f9a2
Hai Nguyen <hainm@users.noreply.github.com>
2016-05-27 01:04:09 -0400

    remove redundant code (#1252)
    * remove redundant code
    
    * make pyflakes happy

--- pytraj/trajectory.py
+++ pytraj/trajectory.py
@@ -17,7 +17,6 @@ from .topology import Topology
 from pytraj.shared_methods import iterframe_master, my_str_method
 from .cyutils import _fast_iterptr, _fast_iterptr_withbox
 from .frameiter import FrameIterator
-from .c_traj.c_trajout import TrajectoryWriter
 
 __all__ = ['Trajectory']
 
@@ -837,35 +836,6 @@ class Trajectory(SharedTrajectory):
 
         self._xyz = np.ascontiguousarray(self._xyz[:, atm.indices])
         return self
-
-    def save(self,
-             filename="",
-             overwrite=False,
-             *args,
-             **kwd):
-        '''write trajectory to disk with given format.
-
-        See also
-        --------
-        pytraj.write_traj
-
-        Examples
-        --------
-        >>> import pytraj as pt
-        >>> traj = pt.datafiles.load_rna()[:]
-        >>> # write to netcdf file
-        >>> traj.save('output/out.nc', overwrite=True)
-
-        '''
-        # note: we do not reuse _savetraj from shared_methods to avoid
-        # circular import (not sure why)
-        with TrajectoryWriter(filename=filename,
-                     top=self.top,
-                     overwrite=overwrite,
-                     *args,
-                     **kwd) as trajout:
-            for frame in self:
-                trajout.write(frame)
 
     def iterframe(self,
                   start=0,
