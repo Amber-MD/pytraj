4725641ca542895bdc8915cf861f8b187f5d19d1
Hai Nguyen <hainm.comp@gmail.com>
2016-06-07 14:28:02 -0400

    oops, wrong

--- tests/test_pmap.py
+++ tests/test_pmap.py
@@ -118,16 +118,16 @@ class TestNormalPmap(unittest.TestCase):
 class TestParallelMapForTrajectoryIteratorWithTransformation(unittest.TestCase):
     def test_trajectoryiterator_with_transformation(self):
         traj_on_disk = pt.iterload("data/tz2.nc", "data/tz2.parm7")
-        traj_on_mem  = pt.iterload("data/tz2.nc", "data/tz2.parm7")
+        traj_on_mem  = pt.load("data/tz2.nc", "data/tz2.parm7")
 
         traj_on_disk.superpose(mask='@CA', ref=3)
         traj_on_mem.superpose(mask='@CA', ref=3)
 
         rmsd0_dict = pt.pmap(pt.rmsd_nofit, traj_on_disk, mask='@CB', n_cores=2, ref=0)
-        rmsd1_dict = pt.pmap(pt.rmsd_nofit, traj_on_mem, mask='@CB', n_cores=2, ref=0)
+        rmsd1 = pt.rmsd_nofit(traj_on_mem, mask='@CB', ref=0)
 
         aa_eq(pt.tools.dict_to_ndarray(rmsd0_dict),
-              pt.tools.dict_to_ndarray(rmsd1_dict))
+              rmsd1)
 
 @unittest.skipUnless(sys.platform.startswith('linux'), 'pmap for linux')
 class TestParallelMapForMatrix(unittest.TestCase):
