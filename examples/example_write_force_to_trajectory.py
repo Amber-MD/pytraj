2f6854d495a8220ff9eff0a52cfe1b87d9244a53
Hai Nguyen <hainm.comp@gmail.com>
2016-06-16 13:51:34 -0400

    example_write_force_to_trajectory.py

---
+++ examples/example_write_force_to_trajectory.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+from parmed.utils.netcdf import netcdf_file
+import pytraj as pt
+import numpy as np
+
+fh = netcdf_file('data/mdfrc', mmap=False) # change mdfrc to your force filename
+forces = fh.variables['forces']
+
+# do similar thing for velocities
+
+top = pt.load_topology('data/mdfrc.prmtop') # change prmtop to your parm7 filename
+traj = pt.Trajectory(xyz=np.ascontiguousarray(forces[:], dtype='f8'), top=top)
+print('original trajectory', traj)
+
+# strip atom atoms
+# change ':1-100' to your mask (e.g ':WAT')
+new_traj = pt.strip(':1-100', traj)
+print('stripped trajectory', new_traj)
+
+# write trajectory
+new_traj.save('test.nc', overwrite=True)
