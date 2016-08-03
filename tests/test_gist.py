153d7290ce4b3416df107b0294ccb3dfa186a74e
Hai Nguyen <hainm.comp@gmail.com>
2016-08-03 16:39:54 -0400

    minimal support for  gist command

--- tests/test_gist.py
+++ tests/test_gist.py
@@ -2,27 +2,41 @@
 
 from __future__ import print_function
 import unittest
+import numpy as np
 import pytraj as pt
 from pytraj.utils import eq, aa_eq
+from pytraj.testing import cpptraj_test_dir
 
 
 class TestGist(unittest.TestCase):
 
     def test_gist(self):
-        traj = pt.iterload("data/tz2.nc", "data/tz2.parm7")
+        traj = pt.iterload("data/tz2.ortho.nc", "data/tz2.ortho.parm7", frame_slice=(0, 10))
+        traj.autoimage('origin')
 
-        command = "doorder doeij refdens 0.033422885325 gridcntr 1.44 0.67 0.29 griddim 10 12 10 gridspacn 2.0"
+        command = "doorder refdens 0.033422885325 gridcntr 1.5 1.0 0.0 griddim 34 44 36 gridspacn 0.50"
+        pt.all_actions.gist(traj, command)
 
-        state_command = """
-        parm data/tz2.ortho.parm7
-        trajin data/tz2.ortho.nc 1 10
-        autoimage origin
-        gist {}
-        """.format(command)
+        def get_gist_data(filename):
+            try:
+                return np.loadtxt(filename, skiprows=7).transpose()
+            except ValueError:
+                print(filename)
 
-        state = pt.load_cpptraj_state(state_command)
-        state.run()
-        print(state.data[1:].to_dict())
+        gist_outputs = [
+                'gist-gH.dx',
+                'gist-gO.dx',
+                'gist-neighbor-norm.dx',
+                'gist-order-norm.dx',
+        ]
+
+        for filename in gist_outputs:
+            saved_gist_gH_filename = cpptraj_test_dir + '/Test_GIST/' + filename + '.save'
+            saved_data = get_gist_data(saved_gist_gH_filename)
+
+            output = get_gist_data(filename)
+
+            aa_eq(output, saved_data)
 
 if __name__ == "__main__":
     unittest.main()
