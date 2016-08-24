b893d324f74aa636e59caa7787d1f9f2397b94cb
Hai Nguyen <hainm.comp@gmail.com>
2016-08-24 03:39:22 -0400

    more

--- pytraj/core/box.pyx
+++ pytraj/core/box.pyx
@@ -5,7 +5,7 @@ from cython cimport view
 from cython.operator cimport dereference as deref
 from cython.operator cimport preincrement as incr
 
-from pytraj.c_dict import BoxTypeDict, get_key
+from pytraj.core.c_dict import BoxTypeDict, get_key
 from pytraj.externals.six import string_types
 
 
