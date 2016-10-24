193454b04c7eaff37ded0957ffbc3ee4768252fa
Hai Nguyen <hainm.comp@gmail.com>
2016-10-24 13:46:50 -0400

    clena

--- pytraj/topology/topology.pyx
+++ pytraj/topology/topology.pyx
@@ -757,46 +757,6 @@ cdef class Topology:
 
         return d
 
-    def to_dataframe(self):
-        """convert to pandas' DataFrame. (experiment)
-        """
-        import pandas as pd
-        cdef:
-            int n_atoms = self.n_atoms
-            int idx
-            Atom atom
-
-        if pd:
-            labels = ['resid', 'resname', 'atomname', 'atomic_number', 'mass']
-            mass_arr = np.array(self.mass)
-            resid_arr = np.empty(n_atoms, dtype='i')
-            resname_arr = np.empty(n_atoms, dtype='U4')
-            atomname_arr = np.empty(n_atoms, 'U4')
-            atomicnumber_arr = np.empty(n_atoms, dtype='i4')
-
-            for idx, atom in enumerate(self.atoms):
-                # TODO: make faster?
-                resid_arr[idx] = atom.resid
-                resname_arr[idx] = self.residuelist[atom.resid].name
-                atomname_arr[idx] = atom.name
-                atomicnumber_arr[idx] = atom.atomic_number
-
-            arr = np.vstack((resid_arr, resname_arr, atomname_arr,
-                             atomicnumber_arr, mass_arr)).T
-            return pd.DataFrame(arr, columns=labels)
-        else:
-            raise ValueError("must have pandas")
-
-    def to_parmed(self):
-        """try to load to ParmEd's Structure
-        """
-        import parmed as pmd
-        from pytraj.utils import tempfolder
-
-        with tempfolder():
-            self.save("tmp.prmtop", overwrite=True)
-            return pmd.load_file("tmp.prmtop")
-
     property _total_charge:
         def __get__(self):
             return sum([atom.charge for atom in self.atoms])
