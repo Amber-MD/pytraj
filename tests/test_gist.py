fab918f1df722cad92c6f618495351ba0bed2b98
Hai Nguyen <hainm.comp@gmail.com>
2016-10-25 13:35:14 -0400

    only test gist locally

--- tests/test_gist.py
+++ tests/test_gist.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import sys
 import unittest
 import numpy as np
 import pytraj as pt
@@ -8,8 +9,10 @@ from pytraj.utils import eq, aa_eq
 from pytraj.testing import cpptraj_test_dir
 
 
-class TestGist(unittest.TestCase):
+using_travis = 'travis' in sys.prefix
 
+@unittest.skipIf(using_travis, 'skip gist test on travis due to memory error')
+class TestGist(unittest.TestCase):
     def test_gist(self):
         traj = pt.iterload("data/tz2.ortho.nc", "data/tz2.ortho.parm7", frame_slice=(0, 10))
         traj.autoimage('origin')
