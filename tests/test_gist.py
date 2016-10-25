bfa06de280f382b0493ac15a0a5300500d32d65f
Hai Nguyen <hainm.comp@gmail.com>
2016-10-25 14:09:15 -0400

    skip gist

--- tests/test_gist.py
+++ tests/test_gist.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import sys
+import os
 import unittest
-import numpy as np
 import pytraj as pt
-from pytraj.utils import eq, aa_eq
-from pytraj.testing import cpptraj_test_dir
+from pytraj.testing import aa_eq
 
 
-using_travis = 'travis' in sys.prefix
+DO_GIST = os.getenv("DO_GIST", False)
 
-@unittest.skipIf(using_travis, 'skip gist test on travis due to memory error')
+@unittest.skipUnless(DO_GIST, 'only do gist test if setting DO_GIST=True')
 class TestGist(unittest.TestCase):
     def test_gist(self):
         traj = pt.iterload("data/tz2.ortho.nc", "data/tz2.ortho.parm7", frame_slice=(0, 10))
