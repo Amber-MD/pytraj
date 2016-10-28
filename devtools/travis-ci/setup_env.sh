bb00a0ca375d65f07a77acb9167af5da41b9a3df
Hai Nguyen <hainm.comp@gmail.com>
2016-10-27 21:20:26 -0400

    more

--- devtools/travis-ci/setup_env.sh
+++ devtools/travis-ci/setup_env.sh
@@ -4,7 +4,8 @@
 git clone https://github.com/hainm/ambermini_test
 mv ambermini_test ./tests/energies/
 
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+osname=`python -c 'import sys; print(sys.platform)'`
+if [ $osname = "osx" ]; then
     wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-MacOSX-x86_64.sh -O miniconda.sh;
 else
     wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh;
@@ -16,30 +17,10 @@ export PATH=$HOME/miniconda/bin:$PATH
 conda install --yes conda-build jinja2 anaconda-client pip
 
 # create myenv
-conda create -y -n myenv python=$PYTHON_VERSION numpy cython h5py libnetcdf
-
+conda create -y -n myenv python=$PYTHON_VERSION
 source activate myenv
-conda install --yes anaconda-client coverage pyflakes jupyter notebook
+conda update -y conda
+conda install -y numpy cython h5py libnetcdf pyflakes
 
-# install other packages here
-conda install parmed -c ambermd --yes
-conda install pysander -c hainm --yes
-conda install cclib -c omnia --yes
-conda install nglview -c bioconda --yes
-pip install coveralls
-pip install coverage
-pip install pytest-cov
-pip install nose
-pip install memory_profiler
-pip install psutil
-# pip install cclib # install error with current v1.5 (08/24/2016)
-pip install tqdm
-
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    # only test mpi on linux
-    conda install mpi4py --yes
-else
-    # osx
-    brew install open-mpi
-    pip install mpi4py
-fi
+# for testing
+source devtools/ci/install_test.sh
