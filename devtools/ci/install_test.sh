4e824c4ed7e335c5a957408b7b6f5b4c35701fa7
Hai Nguyen <hainm.comp@gmail.com>
2016-10-28 00:40:32 -0400

    try pip install mpi4py

--- devtools/ci/install_test.sh
+++ devtools/ci/install_test.sh
@@ -17,7 +17,8 @@ pip install tqdm
 osname=`python -c 'import sys; print(sys.platform)'`
 if [ $osname = "linux" ]; then
     # only test mpi on linux
-    conda install mpi4py --y
+    # conda install mpi4py --y
+    pip install mpi4py
 else
     # osx
     brew install open-mpi
