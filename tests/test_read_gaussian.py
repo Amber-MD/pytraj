a755ab9ba452165c8c24e3921d31e4f1dbdfb7fb
Hai Nguyen <hainm.comp@gmail.com>
2016-11-09 17:14:07 -0500

    cclib

--- tests/test_read_gaussian.py
+++ tests/test_read_gaussian.py
@@ -5,23 +5,17 @@ from pytraj.utils import aa_eq
 
 
 try:
+    import cclib
     has_cclib = True
 except ImportError:
     has_cclib = False
 
 
-class TestReadGaussianOutput(unittest.TestCase):
+@unittest.skipUnless(has_cclib, 'skip: does not have cclib')
+def test_read_gaussian_output():
+    filename = "./data/gaussian/GF2.log"
+    gau = cclib.parser.Gaussian(filename)
+    go = gau.parse()
 
-    @unittest.skipIf(not has_cclib, 'skip: does not have cclib')
-    def test_read_gaussian_output(self):
-        import cclib
-        filename = "./data/gaussian/GF2.log"
-        gau = cclib.parser.Gaussian(filename)
-        go = gau.parse()
-
-        traj = pt.tools.read_gaussian_output(filename, "./data/gaussian/GF2.pdb")
-        aa_eq(traj.xyz, go.atomcoords)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    traj = pt.tools.read_gaussian_output(filename, "./data/gaussian/GF2.pdb")
+    aa_eq(traj.xyz, go.atomcoords)
