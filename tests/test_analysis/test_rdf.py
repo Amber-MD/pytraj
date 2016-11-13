c052e8e9bfef6d7738d6bcee53c61e2db5c86cd9
Hai Nguyen <hainm.comp@gmail.com>
2016-11-13 11:52:58 -0500

    fix more

--- tests/test_analysis/test_rdf.py
+++ tests/test_analysis/test_rdf.py
@@ -32,86 +32,86 @@ class TestRDF(unittest.TestCase):
             # get data directly from cpptraj
             state.run()
 
-        # get data from pytraj
-        data0 = pt.rdf(traj,
-                       solute_mask=':WAT@O',
-                       bin_spacing=0.5,
-                       maximum=10.0,
-                       solvent_mask=':5@CD')
+            # get data from pytraj
+            data0 = pt.rdf(traj,
+                           solute_mask=':WAT@O',
+                           bin_spacing=0.5,
+                           maximum=10.0,
+                           solvent_mask=':5@CD')
 
-        data01 = pt.rdf(traj,
-                        solvent_mask=':5@CD',
-                        solute_mask=':WAT@O',
-                        bin_spacing=0.5,
-                        maximum=10.0)
+            data01 = pt.rdf(traj,
+                            solvent_mask=':5@CD',
+                            solute_mask=':WAT@O',
+                            bin_spacing=0.5,
+                            maximum=10.0)
 
-        data1 = pt.rdf(traj,
-                       solvent_mask=':5',
-                       solute_mask=':WAT@O',
-                       bin_spacing=0.5,
-                       maximum=10.0,
-                       center_solvent=True)
+            data1 = pt.rdf(traj,
+                           solvent_mask=':5',
+                           solute_mask=':WAT@O',
+                           bin_spacing=0.5,
+                           maximum=10.0,
+                           center_solvent=True)
 
-        data2 = pt.rdf(traj,
-                       solvent_mask=':5',
-                       solute_mask=':WAT@O',
-                       bin_spacing=0.5,
-                       maximum=10.0,
-                       center_solute=True)
+            data2 = pt.rdf(traj,
+                           solvent_mask=':5',
+                           solute_mask=':WAT@O',
+                           bin_spacing=0.5,
+                           maximum=10.0,
+                           center_solute=True)
 
-        data3 = pt.rdf(traj,
-                       solvent_mask=':3',
-                       solute_mask=':WAT@O',
-                       bin_spacing=0.5,
-                       maximum=20.0,
-                       center_solute=False)
+            data3 = pt.rdf(traj,
+                           solvent_mask=':3',
+                           solute_mask=':WAT@O',
+                           bin_spacing=0.5,
+                           maximum=20.0,
+                           center_solute=False)
 
-        data4 = pt.rdf(traj,
-                       solvent_mask=':3',
-                       solute_mask=':WAT@O',
-                       bin_spacing=0.5,
-                       maximum=20.0,
-                       center_solute=False,
-                       image=False)
+            data4 = pt.rdf(traj,
+                           solvent_mask=':3',
+                           solute_mask=':WAT@O',
+                           bin_spacing=0.5,
+                           maximum=20.0,
+                           center_solute=False,
+                           image=False)
 
-        data5 = pt.rdf(traj,
-                       solute_mask=':WAT@O',
-                       bin_spacing=0.25,
-                       maximum=10.0,
-                       solvent_mask=':5@CD')
+            data5 = pt.rdf(traj,
+                           solute_mask=':WAT@O',
+                           bin_spacing=0.25,
+                           maximum=10.0,
+                           solvent_mask=':5@CD')
 
-        # solvent_mask is array
-        solvent_indices = pt.select(':WAT@O', traj.top)
-        data6 = pt.rdf(traj,
-                       solvent_mask=':5@CD',
-                       solute_mask=solvent_indices,
-                       bin_spacing=0.25,
-                       maximum=10.0)
+            # solvent_mask is array
+            solvent_indices = pt.select(':WAT@O', traj.top)
+            data6 = pt.rdf(traj,
+                           solvent_mask=':5@CD',
+                           solute_mask=solvent_indices,
+                           bin_spacing=0.25,
+                           maximum=10.0)
 
-        # volume
-        data7 = pt.rdf(traj,
-                       solvent_mask=':5@CD',
-                       solute_mask=':WAT@O',
-                       bin_spacing=0.25,
-                       maximum=10.0,
-                       volume=True)
+            # volume
+            data7 = pt.rdf(traj,
+                           solvent_mask=':5@CD',
+                           solute_mask=':WAT@O',
+                           bin_spacing=0.25,
+                           maximum=10.0,
+                           volume=True)
 
-        # do assertion
-        aa_eq(data0[1], state.data[1], decimal=7)
-        aa_eq(data1[1], state.data[2], decimal=7)
-        aa_eq(data2[1], state.data[3], decimal=7)
-        aa_eq(data3[1], state.data[4], decimal=7)
-        aa_eq(data4[1], state.data[5], decimal=7)
-        aa_eq(data7[1], state.data[8], decimal=7)
+            # do assertion
+            aa_eq(data0[1], state.data[1], decimal=7)
+            aa_eq(data1[1], state.data[2], decimal=7)
+            aa_eq(data2[1], state.data[3], decimal=7)
+            aa_eq(data3[1], state.data[4], decimal=7)
+            aa_eq(data4[1], state.data[5], decimal=7)
+            aa_eq(data7[1], state.data[8], decimal=7)
 
-        # default solvent mask :WAT@O
-        aa_eq(data01[1], state.data[1], decimal=7)
-        steps = np.loadtxt('output/radial.dat').T[0]
-        aa_eq(data0[0], steps)
+            # default solvent mask :WAT@O
+            aa_eq(data01[1], state.data[1], decimal=7)
+            steps = np.loadtxt('radial.dat').T[0]
+            aa_eq(data0[0], steps)
 
-        steps2 = np.loadtxt('output/radial2.dat').T[0]
-        aa_eq(data5[0], steps2)
-        aa_eq(data6[0], steps2)
+            steps2 = np.loadtxt('radial2.dat').T[0]
+            aa_eq(data5[0], steps2)
+            aa_eq(data6[0], steps2)
 
 
 if __name__ == "__main__":
