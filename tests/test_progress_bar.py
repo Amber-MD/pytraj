c2909651b10bcf3a41b10a1be9fe5fcd69e7976a
Hai Nguyen <hainm.comp@gmail.com>
2016-11-13 10:54:12 -0500

    progress bar

--- tests/test_progress_bar.py
+++ tests/test_progress_bar.py
@@ -4,7 +4,8 @@ from __future__ import print_function
 import unittest
 import pytraj as pt
 from pytraj.utils.progress import ProgressBarTrajectory
-from pytraj.utils import aa_eq
+from pytraj.utils import aa_eq, fn
+from pytraj.testing import tempfolder
 import pytest
 
 try:
@@ -14,43 +15,50 @@ except ImportError:
     tqdm = None
 
 
-class TestProgressLog(unittest.TestCase):
+def test_progress_log():
+    """test_progress_log: simple test, just to make sure it is runnable
+    """
+    traj = pt.datafiles.load_tz2()
 
-    def test_progress_log(self):
-        """test_progress_log: simple test, just to make sure it is runnable
-        """
-        traj = pt.datafiles.load_tz2()
+    p = ProgressBarTrajectory(traj, style='basic', every=20)
+    pt.molsurf(p)
 
-        p = ProgressBarTrajectory(traj, style='basic', every=20)
+    p = ProgressBarTrajectory(traj, style='bar', every=20)
+    pt.molsurf(p)
+
+    p = ProgressBarTrajectory(traj, style='circle', every=20)
+    pt.molsurf(p)
+
+    # need to run on Jupyter notebook
+    if tqdm is not None:
+        p = ProgressBarTrajectory(traj, style='tqdm')
         pt.molsurf(p)
 
-        p = ProgressBarTrajectory(traj, style='bar', every=20)
+        p = ProgressBarTrajectory(traj, style=tqdm_notebook)
         pt.molsurf(p)
 
-        p = ProgressBarTrajectory(traj, style='circle', every=20)
-        pt.molsurf(p)
+    # make sure not loading all coordinates from TrajectoryIterator
 
-        # need to run on Jupyter notebook
-        if tqdm is not None:
-            p = ProgressBarTrajectory(traj, style='tqdm')
-            pt.molsurf(p)
+    traj2 = pt.iterload(fn('tz2.nc'), fn('tz2.parm7'))
+    traj2._size_limit_in_GB = traj2._estimated_GB - 0.001
 
-            p = ProgressBarTrajectory(traj, style=tqdm_notebook)
-            pt.molsurf(p)
+    
+    with pytest.raises(MemoryError):
+        traj2.xyz
 
-        # make sure not loading all coordinates from TrajectoryIterator
+    p2 = ProgressBarTrajectory(traj2)
+    aa_eq(pt.rmsd(p2), pt.rmsd(traj2))
+    
 
-        traj2 = pt.iterload('data/tz2.nc', 'data/tz2.parm7')
-        traj2._size_limit_in_GB = traj2._estimated_GB - 0.001
+def test_pickle_progress_bar():
+    traj = pt.datafiles.load_tz2()
 
-        
-        with pytest.raises(MemoryError):
-            traj2.xyz
-
-        p2 = ProgressBarTrajectory(traj2)
-        aa_eq(pt.rmsd(p2), pt.rmsd(traj2))
-        
-
-
-if __name__ == "__main__":
-    unittest.main()
+    with tempfolder():
+        t2 = ProgressBarTrajectory(traj)
+        pt.to_pickle(t2, 'test.pk')
+        t3 = pt.read_pickle('test.pk')
+        data0 = pt.rmsd(traj)
+        data1 = pt.rmsd(t2)
+        data2 = pt.rmsd(t3)
+        aa_eq(data0, data1)
+        aa_eq(data0, data2)
