f675c3317c5995bec67e1ea9f7c11d00b7f2b488
Hai Nguyen <hainm.comp@gmail.com>
2016-11-13 18:01:46 -0500

    more

--- tests/utils.py
+++ tests/utils.py
@@ -7,6 +7,6 @@ def fn(name):
 tc5b_trajin = fn('Tc5b.x')
 tc5b_top = fn('Tc5b.top')
 tz2_trajin  = fn('tz2.nc')
-tz2_top = fn('tz2.top')
+tz2_top = fn('tz2.parm7')
 tz2_ortho_trajin = fn('tz2.ortho.nc')
-tz2_ortho_top = fn('tz2.ortho.top')
+tz2_ortho_top = fn('tz2.ortho.parm7')
