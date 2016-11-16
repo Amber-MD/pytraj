3fd7657c72b3a786856e93cf9b60467c43de03b4
Hai Nguyen <hainm.comp@gmail.com>
2016-11-16 17:00:28 -0500

    fix miniconda path and use py3.5

--- devtools/ci/run_docker_build.sh
+++ devtools/ci/run_docker_build.sh
@@ -3,6 +3,9 @@
 FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/../../"; pwd;)
 CONDA=/root/miniconda/bin/conda
 DOCKER_IMAGE=centos:5
+MINICONDA_URL=https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# MINICONDA_ROOT=/opt/conda
+MINICONDA_ROOT=/root/miniconda3
 
 docker info
 cat << EOF | docker run -i \
@@ -25,9 +28,9 @@ yum -y install gcc \
                which
 
 # Embarking on 1 case(s).
-    wget http://repo.continuum.io/miniconda/Miniconda-3.7.0-Linux-x86_64.sh -O miniconda.sh;
+    wget $MINICONDA_URL -O miniconda.sh;
     bash miniconda.sh -b
-    export PATH=/root/miniconda/bin:\$PATH
+    export PATH=$MINICONDA_ROOT/bin:\$PATH
     $CONDA update --yes --all
     $CONDA install --yes conda-build anaconda-client
     $CONDA info
@@ -39,6 +42,6 @@ yum -y install gcc \
     cd -
     $CONDA build /cpptraj_recipe --quiet || exit 1
     $CONDA build /feedstock_root/devtools/conda-recipe/pytraj --quiet || exit 1
-    cp /opt/conda/conda-bld/linux-64/pytraj*  /feedstock_root/
-    cp /opt/conda/conda-bld/linux-64/libcpptraj* /feedstock_root/
+    cp $MINICONDA_ROOT/conda-bld/linux-64/pytraj*  /feedstock_root/
+    cp $MINICONDA_ROOT/conda-bld/linux-64/libcpptraj* /feedstock_root/
 EOF
