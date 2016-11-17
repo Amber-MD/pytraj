c47860d90df6a402a05b1dad73909520149194df
Hai Nguyen <hainm.comp@gmail.com>
2016-11-16 21:11:57 -0500

    unzip

--- devtools/ci/test_pip_build.sh
+++ devtools/ci/test_pip_build.sh
@@ -2,7 +2,7 @@
 
 ./devtools/mkrelease
 wget https://github.com/amber-md/cpptraj/archive/master.zip
-unzip master.zip
+unzip master
 mv cpptraj-master cpptraj
 export CPPTRAJHOME=`pwd`/cpptraj/
 python scripts/install_libcpptraj.py -openmp
