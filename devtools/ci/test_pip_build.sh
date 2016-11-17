06dfeac43bfb5fa54c62031f3a2ca69a1750c4b8
Hai Nguyen <hainm.comp@gmail.com>
2016-11-16 22:46:24 -0500

    more

--- devtools/ci/test_pip_build.sh
+++ devtools/ci/test_pip_build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 ./devtools/mkrelease
-wget https://github.com/amber-md/cpptraj/archive/master.zip
-unzip master >& log
-mv cpptraj-master cpptraj
+git clone https://github.com/amber-md/cpptraj
 export CPPTRAJHOME=`pwd`/cpptraj/
 python scripts/install_libcpptraj.py -openmp
 (cd dist && python ../scripts/build_wheel.py *gz)
