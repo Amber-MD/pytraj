151a972817848e37a42df0af02a638c8e7022dd9
Hai Nguyen <hainm.comp@gmail.com>
2016-11-17 09:23:29 -0500

    reor test_pairdist

--- tests/test_analysis/test_pairdist.py
+++ tests/test_analysis/test_pairdist.py
@@ -3,38 +3,37 @@
 from __future__ import print_function
 import unittest
 import pytraj as pt
-from pytraj.utils import aa_eq
+from pytraj.testing import aa_eq, tempfolder
 
+# local
+from utils import fn
 
-class TestPairDist(unittest.TestCase):
+def test_pairdist():
+    traj = pt.iterload(fn("/tz2.crd"), fn("tz2.parm7"))
 
-    def test_general(self):
-        traj = pt.iterload("./data/tz2.crd", "./data/tz2.parm7")
+    for (mask, delta) in [('*', 0.1), ('@CA', '0.2')]:
+        data = pt.pairdist(traj,
+                           delta=delta,
+                           mask=mask,
+                           dtype='ndarray')
+        data0 = data[0].T
+        data1 = data[1].T
 
-        for (mask, delta) in [('*', 0.1), ('@CA', '0.2')]:
-            data = pt.pairdist(traj,
-                               delta=delta,
-                               mask=mask,
-                               dtype='ndarray')
-            data0 = data[0].T
-            data1 = data[1].T
+        txt = '''
+        parm {0}
+        trajin {1}
+        pairdist out test.out mask {2} delta {3}
+        '''.format(fn('tz2.parm7'), fn('tz2.crd'), mask, delta)
 
-            txt = '''
-            parm ./data/tz2.parm7
-            trajin ./data/tz2.crd
-            pairdist out test.out mask {0} delta {1}
-            '''.format(mask, delta)
+        # take data, skip DatasetTopology
+        state = pt.load_cpptraj_state(txt)
+        with tempfolder():
+            state.run()
+        cpp_data = state.data[1:].values
+        cpp_distance, cpp_Pr = cpp_data[0].T
+        _, cpp_std = cpp_data[1].T
 
-            # take data, skip DatasetTopology
-            cpp_data = pt.datafiles.load_cpptraj_output(txt)[1:].values
-            cpp_distance, cpp_Pr = cpp_data[0].T
-            _, cpp_std = cpp_data[1].T
-
-            aa_eq(data0[0], cpp_distance)  # distance
-            aa_eq(data1[0], cpp_distance)  # distance
-            aa_eq(data0[1], cpp_Pr, decimal=2)  # Pr
-            aa_eq(data1[1], cpp_std, decimal=2)  # std
-
-
-if __name__ == "__main__":
-    unittest.main()
+        aa_eq(data0[0], cpp_distance)  # distance
+        aa_eq(data1[0], cpp_distance)  # distance
+        aa_eq(data0[1], cpp_Pr, decimal=2)  # Pr
+        aa_eq(data1[1], cpp_std, decimal=2)  # std
