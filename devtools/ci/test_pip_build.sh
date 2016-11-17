0af766c919cc33a5d1ce4596e71439d2c16bf632
Hai Nguyen <hainm.comp@gmail.com>
2016-11-16 22:57:30 -0500

    auditwheel

--- devtools/ci/test_pip_build.sh
+++ devtools/ci/test_pip_build.sh
@@ -3,5 +3,6 @@
 ./devtools/mkrelease
 git clone https://github.com/amber-md/cpptraj
 export CPPTRAJHOME=`pwd`/cpptraj/
+pip install auditwheel
 python scripts/install_libcpptraj.py -openmp
 (cd dist && python ../scripts/build_wheel.py *gz)
