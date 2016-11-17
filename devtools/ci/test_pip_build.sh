2998802d16013fa7759b3faa6c1735fe6b54099d
Hai Nguyen <hainm.comp@gmail.com>
2016-11-16 22:20:09 -0500

    no verbose for unzip

--- devtools/ci/test_pip_build.sh
+++ devtools/ci/test_pip_build.sh
@@ -2,7 +2,7 @@
 
 ./devtools/mkrelease
 wget https://github.com/amber-md/cpptraj/archive/master.zip
-unzip master
+unzip master >& log
 mv cpptraj-master cpptraj
 export CPPTRAJHOME=`pwd`/cpptraj/
 python scripts/install_libcpptraj.py -openmp
