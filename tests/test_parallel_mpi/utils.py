ece064c0e1eacd22f650ddb84dfaeb18236102dc
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 14:08:54 -0500

    fix mpi

---
+++ tests/test_parallel_mpi/utils.py
@@ -0,0 +1,14 @@
+import os
+
+def fn(name):
+    # Note: this function use different path compared to
+    # the one in ../utils.py
+    # return absolute dir of ./data/name
+    return os.path.dirname(__file__) + '/../data/' + name
+
+tc5b_trajin = fn('Tc5b.x')
+tc5b_top = fn('Tc5b.top')
+tz2_trajin  = fn('tz2.nc')
+tz2_top = fn('tz2.parm7')
+tz2_ortho_trajin = fn('tz2.ortho.nc')
+tz2_ortho_top = fn('tz2.ortho.parm7')
