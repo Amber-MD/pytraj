a05f26fc9c09f0a462d50a3934d33d658879f240
Hai Nguyen <hainm.comp@gmail.com>
2016-11-18 21:56:30 -0500

    - add manylinux-extra, change print to logging

--- scripts/build_wheel.py
+++ scripts/build_wheel.py
@@ -9,6 +9,7 @@ import os
 import sys
 import subprocess
 from glob import glob
+import logging
 
 class PipBuilder(object):
     '''
@@ -29,36 +30,52 @@ class PipBuilder(object):
     Build wheel package from conda package?
     '''
     REQUIRED_PACKAGES = ['auditwheel']
-    SUPPORTED_VERSION = ['2.7', '3.4', '3.5']
+    SUPPORTED_VERSIONS = ['2.7', '3.4', '3.5']
+    MANY_LINUX_PYTHONS = dict(
+       (py_version, '/opt/python/cp{py}-cp{py}m/bin/python'.format(py=py_version.replace('.', '')))
+       for py_version in SUPPORTED_VERSIONS
+    )
 
     def __init__(self,
                  tarfile,
                  pytraj_home,
                  python_versions,
+                 use_manylinux=False,
                  cpptraj_dir=''):
         self.miniconda_root = self.find_miniconda_root()
         is_osx = sys.platform.startswith('darwin')
         self.python_versions = python_versions
+        self.use_manylinux = use_manylinux
         if cpptraj_dir:
             self.cpptraj_dir = os.path.abspath(cpptraj_dir)
         else:
             self.cpptraj_dir = ''
-        self.python_exe_paths = dict((python_version,
-                                      '/'.join((self.miniconda_root,
-                                                '/envs/pytraj' + python_version,
-                                                'bin/python'))
-                                      ) for python_version in self.python_versions)
+        if self.use_manylinux:
+            self.python_exe_paths = dict((py_version,
+                PipBuilder.MANY_LINUX_PYTHONS[py_version]) 
+                for py_version in self.python_versions)
+        else:
+            self.python_exe_paths = dict((py_version,
+                                          '/'.join((self.miniconda_root,
+                                                    '/envs/pytraj' + py_version,
+                                                    'bin/python'))
+                                          ) for py_version in self.python_versions)
         self.repair_exe = (pytraj_home + '/scripts/misc/fix_rpath_pip_osx.py' if is_osx else
                            'auditwheel repair')
 
     def run(self):
         self.check_cpptraj_and_required_libs()
         for python_version in self.python_versions:
-            env = 'pytraj' + python_version
-            env_path = self.miniconda_root + '/envs/' + env
-            if not os.path.exists(env_path):
-                self.create_env(python_version)
-            print('building pytraj for Python {}'.format(python_version))
+            if not self.use_manylinux:
+                logging.info('Using conda for creating env')
+                # use conda to create a new env
+                env = 'pytraj' + python_version
+                env_path = self.miniconda_root + '/envs/' + env
+                if not os.path.exists(env_path):
+                    self.create_env(python_version)
+                logging.info('building pytraj for Python {}'.format(python_version))
+            else:
+                logging.info('Using python version from manylinux')
             self.build_original_wheel(python_version)
             self.repair_wheel(python_version)
             self.validate_install(python_version)
@@ -78,7 +95,7 @@ class PipBuilder(object):
         env = 'pytraj' + python_version
         sys.stdout.write('creating {} env'.format(env))
         cmlist = 'conda create -n {} python={} numpy nomkl --yes'.format(env, python_version)
-        print(cmlist)
+        logging.info(cmlist)
         subprocess.check_call(cmlist.split())
     
     def _get_wheel_file(self, python_version):
@@ -93,7 +110,7 @@ class PipBuilder(object):
         python_exe = self.python_exe_paths[py_version]
         env = 'pytraj' + py_version
         # e.g: change 2.7 to 27
-        print('Testing pytraj build')
+        logging.info('Testing pytraj build')
         version = py_version.replace('.', '')
         cwd = os.getcwd()
         pattern = cwd + '/wheelhouse/pytraj-*-cp{}-*.whl'.format(version)
@@ -103,35 +120,43 @@ class PipBuilder(object):
         except subprocess.CalledProcessError:
             pass
         subprocess.check_call('{} -m pip install {}'.format(python_exe, whl_file).split())
-        try:
-            print('Installing numpy')
-            subprocess.check_call('{} -c "import numpy"'.format(python_exe), shell=True)
-        except subprocess.CalledProcessError:
-            subprocess.check_call('conda install numpy nomkl -y -n {}'.format(env),
-                                  shell=True)
-        output = subprocess.check_output('{} -c "import pytraj as pt; print(pt)"'
-                                       .format(python_exe, whl_file),
-                                       shell=True)
-        output = output.decode()
-        expected_output = 'envs/{env}/lib/python{py_version}/site-packages/pytraj'.format(env=env,
-                py_version=py_version)
-        assert expected_output in output
-        print('PASSED: build test for {}'.format(whl_file))
+        if self.use_manylinux:
+            output = subprocess.check_output('{} -c "import pytraj as pt; logging.info(pt)"'
+                                           .format(python_exe, whl_file),
+                                           shell=True)
+            output = output.decode()
+            logging.info('Testing pytraj python={}'.format(py_version))
+            logging.info(output)
+        else:
+            try:
+                logging.info('Installing numpy')
+                subprocess.check_call('{} -c "import numpy"'.format(python_exe), shell=True)
+            except subprocess.CalledProcessError:
+                subprocess.check_call('conda install numpy nomkl -y -n {}'.format(env),
+                                      shell=True)
+            output = subprocess.check_output('{} -c "import pytraj as pt; logging.info(pt)"'
+                                           .format(python_exe, whl_file),
+                                           shell=True)
+            output = output.decode()
+            expected_output = 'envs/{env}/lib/python{py_version}/site-packages/pytraj'.format(env=env,
+                    py_version=py_version)
+            assert expected_output in output
+            logging.info('PASSED: build test for {}'.format(whl_file))
 
     def check_cpptraj_and_required_libs(self):
         pytraj_home = os.path.abspath(os.path.dirname(__file__).strip('scripts'))
         cpptraj_home = os.environ.get("CPPTRAJHOME", '')
         if not cpptraj_home:
-            print('CPPTRAJHOME is not yet set')
+            logging.info('CPPTRAJHOME is not yet set')
             ext = 'so' if not sys.platform.startswith('darwin') else 'dylib'
             if not self.cpptraj_dir:
                 self.cpptraj_dir = pytraj_home + '/cpptraj/'
             suggested_libcpptraj = self.cpptraj_dir + '/lib/libcpptraj.' + ext
-            print('Looking for {}'.format(suggested_libcpptraj))
+            logging.info('Looking for {}'.format(suggested_libcpptraj))
             if os.path.exists(suggested_libcpptraj):
-                print('Found')
+                logging.info('Found')
                 os.environ['CPPTRAJHOME'] = self.cpptraj_dir
-                print('CPPTRAJHOME is set to {}'.format(self.cpptraj_dir))
+                logging.info('CPPTRAJHOME is set to {}'.format(self.cpptraj_dir))
             else:
                 raise EnvironmentError('Must set CPPTRAJHOME')
 
@@ -147,14 +172,16 @@ if __name__ == '__main__':
     parser.add_argument('tarfile')
     parser.add_argument('--py', default=None, help='Python version. Default: build all versions (2.7, 3.4, 3.5)')
     parser.add_argument('--cpptraj-dir', default='', help='cpptraj dir, optional')
+    parser.add_argument('--manylinux-docker', action='store_true', help='If specified, use Python versions from manylinux')
     args = parser.parse_args()
 
     tarfile = os.path.abspath(args.tarfile)
-    python_versions = PipBuilder.SUPPORTED_VERSION if args.py is None else [args.py,]
+    python_versions = PipBuilder.SUPPORTED_VERSIONS if args.py is None else [args.py,]
     # pytraj tar file
     pytraj_home = os.path.dirname(__file__).strip('scripts')
     builder = PipBuilder(tarfile=tarfile,
                          pytraj_home=pytraj_home,
                          python_versions=python_versions,
+                         use_manylinux=args.manylinux_docker,
                          cpptraj_dir=args.cpptraj_dir)
     builder.run()
