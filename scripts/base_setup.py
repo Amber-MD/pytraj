4a31177064f586a1456e92fc1f8bfe5230135a64
Hai Nguyen <hainm.comp@gmail.com>
2016-12-08 12:04:51 -0500

    remove unused versions stuff

--- scripts/base_setup.py
+++ scripts/base_setup.py
@@ -60,22 +60,8 @@ def compiler_env_info(message):
 # Numpy is licensed under the BSD license,
 
 def git_version():
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
     try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        out = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
         GIT_REVISION = out.strip().decode('ascii')
     except OSError:
         GIT_REVISION = "Unknown"
@@ -106,9 +92,9 @@ def get_version_info():
             from pytraj.__version__ import git_revision as GIT_REVISION
         except ImportError:
             # FIXME pytraj
-            # raise ImportError("Unable to import git_revision. Try removing " \
-            #                  "numpy/version.py and the build directory " \
-            #                  "before building.")
+            raise ImportError("Unable to import git_revision. Try removing " \
+                              "pytraj/version.py and the build directory " \
+                              "before building.")
             pass
     else:
         GIT_REVISION = "Unknown"
