53149769c09b422db7ebecbe1e5fd7390e218e33
Hai Nguyen <hainm.comp@gmail.com>
2016-12-12 16:25:18 -0500

    weird

--- setup.py
+++ setup.py
@@ -160,9 +160,9 @@ else:
         for fn in libcpptraj_files:
             print("Copying {} to pytraj".format(fn))
             shutil.copy(fn, 'pytraj/')
-            old = os.path.join('pytraj', fn)
-            new = os.path.join('pytraj', 'cpptraj.lib')
-            os.rename(old, new)
+        old = os.path.join('pytraj', fn)
+        new = os.path.join('pytraj', 'cpptraj.lib')
+        os.rename(old, new)
     else:
         possible_libcpptraj_files = []
 
