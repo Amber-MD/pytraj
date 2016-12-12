5345bad23e5649faea0255aebb85fbde47ae260d
Hai <hainm.comp@gmail.com>
2016-12-12 05:44:45 +0000

    win

---
+++ devtools/win/setup_msys.sh
@@ -0,0 +1,3 @@
+pacman -S unzip make
+pacman -S --noconfirm --needed mingw-w64-x86_64-openblas
+pacman -S --noconfirm --needed  mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-ncurses mingw-w64-x86_64-readline
