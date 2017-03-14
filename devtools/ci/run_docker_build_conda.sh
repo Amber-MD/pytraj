1a114fac172b5fa73e8f711f10d9b0c8a1f4b99d
HN <nhai.qn@gmail.com>
2017-03-14 00:42:12 -0400

    more

---
+++ devtools/ci/run_docker_build_conda.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/../../"; pwd;)
+DOCKER_IMAGE=hainm/pytraj-build-box
+
+docker info
+cat << EOF | docker run -i \
+                        -v ${FEEDSTOCK_ROOT}:/feedstock_root \
+                        -a stdin -a stdout -a stderr \
+                        ${DOCKER_IMAGE}\
+                        bash || exit $?
+
+set -x
+cd /feedstock_root/
+
+export PATH=\$HOME/miniconda3/bin:\$PATH
+
+for pyver in 2.7 3.4 3.5; do
+    conda build devtools/conda-recipe/pytraj --py \$pyver
+    tarfile=`conda build devtools/conda-recipe/pytraj --py \$pyver --output`
+
+    build_dir=dist/conda/linux-64
+    if [ ! -d $build_dir ]; then
+        mkdir -p $build_dir
+    fi
+    cp $tarfile $build_dir
+done
+EOF
