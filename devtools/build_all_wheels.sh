507e38702a92a203581ad5ad5ad9dd3eac58606b
HN <nhai.qn@gmail.com>
2017-03-14 18:25:16 -0400

    functionize

--- devtools/build_all_wheels.sh
+++ devtools/build_all_wheels.sh
@@ -5,34 +5,49 @@
 # add cpptraj folder
 python -c "import auditwheel" || exit 1
 
-if [ ! -d cpptraj ]; then
-    git clone https://github.com/amber-md/cpptraj
-else
-    (cd cpptraj && git pull && git clean -fdx .)
-fi
+function main(){
+    clone_or_update_cpptraj
+    # pip_linux
+    # pip_osx
+    conda_linux
+}
 
-# PIP BUILD
-#     LINUX
-sh devtools/ci/run_docker_build_wheels.sh
-
-#     OSX
-(cd cpptraj && git clean -fdx .)
-export CPPTRAJHOME=`pwd`/cpptraj
-python scripts/install_libcpptraj.py
-(cd dist && python ../scripts/build_wheel.py pytraj*.tar.gz)
-
-# CONDA BUILD
-#     LINUX
-sh devtools/ci/run_docker_build_conda.sh
-
-#     OSX
-for pyver in 2.7 3.4 3.5; do
-    conda build devtools/conda-recipe/pytraj --py $pyver
-    tarfile=`conda build devtools/conda-recipe/pytraj --py $pyver --output`
-
-    build_dir=dist/conda/osx-64
-    if [ ! -d $build_dir ]; then
-        mkdir -p $build_dir
+function clone_or_update_cpptraj(){
+    if [ ! -d cpptraj ]; then
+        git clone https://github.com/amber-md/cpptraj
+    else
+        (cd cpptraj && git pull && git clean -fdx .)
     fi
-    cp $tarfile $build_dir
-done
+}
+
+function pip_linux(){
+    sh devtools/ci/run_docker_build_wheels.sh
+}
+
+
+function pip_osx(){
+    (cd cpptraj && git clean -fdx .)
+    export CPPTRAJHOME=`pwd`/cpptraj
+    python scripts/install_libcpptraj.py
+    (cd dist && python ../scripts/build_wheel.py pytraj*.tar.gz)
+}
+
+function conda_linux(){
+    sh devtools/ci/run_docker_build_conda.sh
+}
+
+
+function conda_osx(){
+    for pyver in 2.7 3.4 3.5; do
+        conda build devtools/conda-recipe/pytraj --py $pyver
+        tarfile=`conda build devtools/conda-recipe/pytraj --py $pyver --output`
+    
+        build_dir=dist/conda/osx-64
+        if [ ! -d $build_dir ]; then
+            mkdir -p $build_dir
+        fi
+        cp $tarfile $build_dir
+    done
+}
+
+main "$@"
