9d98caa571b7b264ef4908509c531e3afcd7e2b8
HN <nhai.qn@gmail.com>
2017-07-12 01:30:38 -0400

    rpath

--- scripts/misc/fix_rpath_pip_osx.py
+++ scripts/misc/fix_rpath_pip_osx.py
@@ -11,6 +11,15 @@ from auditwheel import wheeltools
 # 3. Double-check: wheel unpack your_new.whl
 
 # Note: your_new.whl will be in ./wheelhouse folder
+def get_so_files(folder):
+    # all python extensions needed to be fixed
+    level = 6
+    all_so_files = []
+    cmd = "find {} -type f -name '*.so'".format(folder)
+    output = subprocess.check_output(cmd, shell=True)
+    return [fn for fn in output.decode().split('\n') if fn]
+
+
 def get_dylibs(fn):
     output = subprocess.check_output([
         'otool',
@@ -19,25 +28,25 @@ def get_dylibs(fn):
     ]).decode()
     return [line.split()[0] for line in output.split('\n') if line]
 
-def copy_libcpptraj_to_pytraj_lib(libcpptraj, python_version):
-    pytraj_lib = 'pytraj/lib'
+
+def copy_and_update_libs(pytraj_dir, libcpptraj, python_version):
+    """ Copy libcpptraj.dylib and libnetcdf.7.dylib to pytraj/lib folder
+    Update their ids to @rpath/{libcpptraj.dylib, libnetcdf.7.dylib}
+
+    Also add loader_path to .so files in pytraj folder.
+    """
+    pytraj_lib = os.path.join(pytraj_dir, 'lib')
     new_libcpptraj = pytraj_lib + '/libcpptraj.dylib'
-    LIBCPPTRAJ_RPATH = '@rpath/python{}/site-packages/pytraj/lib/libcpptraj.dylib'.format(python_version)
+
     try:
         os.mkdir(pytraj_lib)
     except OSError:
         pass
     shutil.copy(libcpptraj, pytraj_lib)
-    os.system('install_name_tool -id {} {}'.format(LIBCPPTRAJ_RPATH, new_libcpptraj))
-    netcdf_libs = [lib for lib in get_dylibs(new_libcpptraj) if 'libnetcdf.7.dylib' in lib]
-
-    if netcdf_libs:
-        fn  = netcdf_libs[0]
-        subprocess.check_call(['install_name_tool', '-change', 
-                               fn,
-                               '@rpath/libnetcdf.7.dylib',
-                               new_libcpptraj])
-    return LIBCPPTRAJ_RPATH
+    netcdf_lib = [lib for lib in get_dylibs(new_libcpptraj) if 'libnetcdf.7.dylib' in lib][0]
+    shutil.copy(netcdf_lib, pytraj_lib)
+    os.system('install_name_tool -id {} {}'.format('@rpath/libcpptraj.dylib', new_libcpptraj))
+    os.system('install_name_tool -id {} {}'.format('@rpath/libnetcdf.7.dylib', netcdf_lib))
 
 
 def main(pkg_name, whl_name, libcpptraj, python_version):
@@ -46,7 +55,7 @@ def main(pkg_name, whl_name, libcpptraj, python_version):
     except OSError:
         pass
     with wheeltools.InWheel(whl_name, out_wheel='wheelhouse/{}'.format(whl_name)):
-        LIBCPPTRAJ_RPATH = copy_libcpptraj_to_pytraj_lib(libcpptraj, python_version)
+        LIBCPPTRAJ_RPATH = copy_and_update_libs(libcpptraj, python_version)
         for root, dirs, files in os.walk(pkg_name):
             for fn in (root + '/' +  _ for _ in files):
                 if fn.endswith('.so'):
