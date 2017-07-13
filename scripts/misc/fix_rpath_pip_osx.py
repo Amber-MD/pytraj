4e11ed4f4a73a2e889ae34b146ce9bf76e9ac9f2
HN <nhai.qn@gmail.com>
2017-07-13 00:27:53 -0400

    pack libnetcdf and fix rpath

--- scripts/misc/fix_rpath_pip_osx.py
+++ scripts/misc/fix_rpath_pip_osx.py
@@ -11,35 +11,65 @@ from auditwheel import wheeltools
 # 3. Double-check: wheel unpack your_new.whl
 
 
-def get_dylibs(fn):
+def get_dylibs(fn, local_only=False):
+    # local_only: /usr/local
     output = subprocess.check_output([
         'otool',
         '-L',
         fn
     ]).decode()
-    return [line.split()[0] for line in output.split('\n') if line]
+    lines = [line.split()[0] for line in output.split('\n') if line]
+    if local_only:
+        return [line for line in lines if '/usr/local' in line]
+    else:
+        return lines
 
+
+def update_id(fn, sudo=False):
+    # e.g: @rpath/libcpptraj.dylib
+    basename = os.path.basename(fn)
+    rpath = '@rpath/{}'.format(basename)
+    cmd = 'install_name_tool -id {} {}'.format(rpath, fn)
+    if sudo:
+        cmd = 'sudo '  + cmd
+    os.system(cmd)
+    return rpath
 
 def copy_and_update_libs(pytraj_dir, libcpptraj):
-    """ Copy libcpptraj.dylib and libnetcdf.7.dylib to pytraj/lib folder
-    Update their ids to @rpath/{libcpptraj.dylib, libnetcdf.7.dylib}
+    """ Copy libcpptraj.dylib and libnetcdf.{version}.dylib to pytraj/lib folder
+    Update their ids to @rpath/{libcpptraj.dylib, libnetcdf.{version}.dylib}
 
     Also add loader_path to .so files in pytraj folder.
     """
     pytraj_lib = os.path.join(pytraj_dir, 'pytraj_3rd_party')
+    netcdf_lib = [lib for lib in get_dylibs(libcpptraj) if 'libnetcdf' in lib][0]
+    netcdf_basename = os.path.basename(netcdf_lib)
     new_libcpptraj = pytraj_lib + '/libcpptraj.dylib'
-    new_libnetcdf = pytraj_lib + '/libnetcdf.7.dylib'
+    new_libnetcdf = os.path.join(pytraj_lib, netcdf_basename)
 
     try:
         os.mkdir(pytraj_lib)
     except OSError:
         pass
     shutil.copy(libcpptraj, new_libcpptraj)
-    netcdf_lib = [lib for lib in get_dylibs(libcpptraj) if 'libnetcdf.7.dylib' in lib][0]
+    rpath_libnetcdf = '@rpath/{}'.format(netcdf_basename)
     print('netcdf_lib', netcdf_lib)
     shutil.copy(netcdf_lib, new_libnetcdf)
     os.system('install_name_tool -id {} {}'.format('@rpath/pytraj_3rd_party/libcpptraj.dylib', new_libcpptraj))
-    os.system('sudo install_name_tool -id {} {}'.format('@rpath/pytraj_3rd_party/libnetcdf.7.dylib', new_libnetcdf))
+    os.system('install_name_tool -change {} {} {}'.format(netcdf_lib, rpath_libnetcdf,  new_libcpptraj))
+    os.system('install_name_tool -add_rpath @loader_path/ {}'.format(new_libcpptraj))
+    os.system('sudo install_name_tool -id {} {}'.format(rpath_libnetcdf, new_libnetcdf))
+
+    for fn in get_dylibs(netcdf_lib, local_only=True):
+        if netcdf_basename not in fn:
+            new_fn = os.path.join(pytraj_lib, os.path.basename(fn))
+            shutil.copy(fn, new_fn)
+            rpath = update_id(new_fn, sudo=True)
+            os.system('sudo install_name_tool -change {} {} {}'.format(fn, rpath, new_libnetcdf))
+
+            for fn2 in get_dylibs(fn, local_only=True):
+                rpath = '@rpath/{}'.format(os.path.basename(fn2))
+                os.system('sudo install_name_tool -change {} {} {}'.format(fn2, rpath, new_fn))
 
 
 def pack(pkg_name, whl_name, libcpptraj):
@@ -55,25 +85,30 @@ def pack(pkg_name, whl_name, libcpptraj):
 
         lib_dir = os.path.join(pkg_name, 'lib')
 
+        so_files = []
         for root, dirs, files in os.walk(pkg_name):
             for fn in (os.path.join(root,  _) for _ in files):
                 if fn.endswith('.so'):
+                    so_files.append(os.path.abspath(fn))
                     libcpptraj_dir = ''
                     for lib in get_dylibs(fn):
                         if 'libcpptraj' in lib:
                             libcpptraj_dir = lib
                             break
                     relpath = os.path.relpath(pkg_dir, os.path.abspath(os.path.dirname(fn)))
-                    print('relpath', relpath)
                     subprocess.check_call(['install_name_tool', '-change', 
                                            libcpptraj_dir,
                                            rpath_libcpptraj,
                                            fn])
                     loader_path = '@loader_path/{}/'.format(relpath)
-                    print('loader_path', loader_path)
                     subprocess.check_call(['install_name_tool', '-add_rpath', 
                                            loader_path,
                                            fn])
+        print("rpath")
+        for fn in glob(os.path.join(pkg_dir, 'pytraj_3rd_party/*dylib')):
+            print(os.path.basename(fn))
+            print("    ", get_dylibs(fn)[1:])
+        print(get_dylibs(so_files[0])[1:])
 
 def main():
     import argparse
