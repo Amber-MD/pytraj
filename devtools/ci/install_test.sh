958cc861d433969ce9d973c608b6db63a4162025
Hai Nguyen <hainm@users.noreply.github.com>
2017-09-27 19:01:27 -0400

    Update install_test.sh

--- devtools/ci/install_test.sh
+++ devtools/ci/install_test.sh
@@ -17,8 +17,8 @@ source devtools/ci/install_ambertools.sh
 osname=`python -c 'import sys; print(sys.platform)'`
 if [ $osname = "linux" ]; then
     # only test mpi on linux
-    # conda install mpi4py --y
-    pip install mpi4py
+    conda install mpi4py --y
+    # pip install mpi4py # compiling error (09/27/2017)
 else
     # osx
     brew install open-mpi
