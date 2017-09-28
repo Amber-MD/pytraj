e75ca960ae460dffc1440bbc70bf439f247c5683
HN <nhai.qn@gmail.com>
2017-09-28 19:13:02 -0400

    fix version

--- devtools/ci/setup_env.sh
+++ devtools/ci/setup_env.sh
@@ -16,18 +16,20 @@ fi
 bash miniconda.sh -b
 
 export PATH=$HOME/miniconda3/bin:$PATH
-conda install --yes conda-build jinja2 anaconda-client pip numpy nomkl
-pip install auditwheel
+conda install --yes conda-build=3.0.19 jinja2 anaconda-client pip numpy=1.13.1 nomkl
+pip install auditwheel==1.7.0
 pip install cython==$cython_version
 
 # create myenv
 conda create -y -n myenv python=$PYTHON_VERSION
 source activate myenv
 # conda install -y numpy=1.13.1 nomkl h5py=2.7.0 libnetcdf=4.4.1  pyflakes
-conda install -y libnetcdf=4.4.1  pyflakes
-pip install numpy
-pip install h5py
-conda install -y hdf4 # temporary install to fix error "libmfhdf.so.0: cannot open shared object file"
+conda install -y conda=4.3.25
+conda install -y libnetcdf=4.4.1
+pip install pyflakes==1.6.0
+pip install numpy==1.13.1
+pip install h5py==2.7.0
+conda install -y hdf4=4.2.12 # temporary install to fix error "libmfhdf.so.0: cannot open shared object file"
 pip install cython==$cython_version
-pip install auditwheel
-pip install mock
+pip install auditwheel==1.7.0
+pip install mock==2.0.0
