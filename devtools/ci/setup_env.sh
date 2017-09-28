f9c4dd1d5f45317dae04839b03863f09522d1aa6
HN <nhai.qn@gmail.com>
2017-09-28 19:22:44 -0400

    pip-requirements.txt

--- devtools/ci/setup_env.sh
+++ devtools/ci/setup_env.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cython_version=0.26
 miniconda_version=4.3.21
 # download ambermini_test for energy calculation
 git clone https://github.com/hainm/ambermini_test
@@ -26,10 +25,5 @@ source activate myenv
 # conda install -y numpy=1.13.1 nomkl h5py=2.7.0 libnetcdf=4.4.1  pyflakes
 conda install -y conda=4.3.25
 conda install -y libnetcdf=4.4.1
-pip install pyflakes==1.6.0
-pip install numpy==1.13.1
-pip install h5py==2.7.0
 conda install -y hdf4=4.2.12 # temporary install to fix error "libmfhdf.so.0: cannot open shared object file"
-pip install cython==$cython_version
-pip install auditwheel==1.7.0
-pip install mock==2.0.0
+pip install -r pip-requirements.txt
