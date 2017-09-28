ae1f0b52427bb806b9cd535db0a096dc38cb2a3c
Hai Nguyen <hainm@users.noreply.github.com>
2017-09-27 22:28:30 -0400

    Update setup_env.sh

--- devtools/ci/setup_env.sh
+++ devtools/ci/setup_env.sh
@@ -23,7 +23,7 @@ pip install cython==$cython_version
 conda create -y -n myenv python=$PYTHON_VERSION
 source activate myenv
 conda update -y conda
-conda install -y numpy nomkl h5py libnetcdf pyflakes
+conda install -y numpy=1.13.1 nomkl h5py=2.7.0 libnetcdf=4.4.1-1  pyflakes
 conda install -y hdf4 # temporary install to fix error "libmfhdf.so.0: cannot open shared object file"
 pip install cython==$cython_version
 pip install auditwheel
