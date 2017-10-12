15044e036ea10088433f618b7a32465e5dfd79fd
HN <nhai.qn@gmail.com>
2017-10-12 00:37:46 -0400

    pypy

--- devtools/travis-ci/install_pytraj.sh
+++ devtools/travis-ci/install_pytraj.sh
@@ -3,19 +3,25 @@
 # create this file to hide output
 # python setup.py install --amber-release
 
+if [ "$PYPY" = "true" ]; then
+    python=pypy
+else
+    python=python
+fi
+
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    python setup.py install --disable-openmp
+    $python setup.py install --disable-openmp
 else
     if [ "$TEST_SETUP" == 'true' ]; then
         echo "TEST_SETUP"
     else
         if [ "$USE_OPENMP" == 'false' ]; then 
-            python setup.py install --disable-openmp
+            $python setup.py install --disable-openmp
         else
             # pytraj will pick compiler based on COMPILER env
             # or using default (gnu for linux, clang for osx)
-            python setup.py install
+            $python setup.py install
         fi
     fi
 fi
