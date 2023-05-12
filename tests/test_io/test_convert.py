3d3c98aa4a9e78ee5b988c01c0358bcf659f952b
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-05-12 12:10:31 -0400

    Get rid of this warning: VisibleDeprecationWarning: Creating an ndarray from ragged nested sequences (which is a list-or-tuple of lists-or-tuples-or ndarrays with different lengths or shapes) is deprecated.

--- tests/test_io/test_convert.py
+++ tests/test_io/test_convert.py
@@ -1,13 +1,14 @@
 from __future__ import print_function
 import unittest
 import pytraj as pt
+import numpy as np
 from utils import fn
 
 
 class Test(unittest.TestCase):
     def test_0(self):
-        mask = pt.utils.convert.array2d_to_cpptraj_maskgroup([[0, 3],
-                                                              [5, 6, 8]])
+        mask = pt.utils.convert.array2d_to_cpptraj_maskgroup(np.array([[0, 3],
+                                                              [5, 6, 8]], dtype=object))
         assert mask == '@1,4 @6,7,9'
 
 
