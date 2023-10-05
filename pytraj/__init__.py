93a8044e26cf4c259c437d49b65bd05c91decc44
sastrys1 <sastrys1@gene.com>
2023-10-04 21:38:45 -0700

    removing closest_atom traces from count_in_voxel branch to separate features cleanly

--- pytraj/__init__.py
+++ pytraj/__init__.py
@@ -125,7 +125,6 @@ from .all_actions import center_of_mass
 from .all_actions import diffusion
 from .all_actions import dihedral
 from .all_actions import distance
-from .all_actions import closest_atom
 from .all_actions import count_in_voxel
 from .all_actions import distance_to_point
 from .all_actions import distance_to_reference
