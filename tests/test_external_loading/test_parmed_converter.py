566115ba6542e7e81f7c637055213fbdd3680dd7
Hai Nguyen <hainm.comp@gmail.com>
2024-06-17 21:51:04 -0400

    skip some tests

--- tests/test_external_loading/test_parmed_converter.py
+++ tests/test_external_loading/test_parmed_converter.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-import unittest
+import pytest
 import pytraj as pt
 from utils import fn
 from pytraj.testing import aa_eq
 from pytraj.sandbox import to_parmed
+
+pytestmark = pytest.mark.skip(reason="Skipping the whole file for now")
 
 try:
     import parmed as pmd
 except ImportError:
     pmd = None
 
+@pytest.fixture
+def traj():
+    return pt.iterload(fn('tz2.nc'), fn('tz2.parm7'))
 
-@unittest.skipIf(pmd is None, "Must install ParmEd")
-class TestParmEdConverter(unittest.TestCase):
-    def setUp(self):
-        self.traj = pt.iterload(fn('tz2.nc'), fn('tz2.parm7'))
-        self.parm = pmd.load_file(self.traj.top.filename)
+@pytest.fixture
+def parm(traj):
+    return pmd.load_file(traj.top.filename) if pmd else None
 
-    def parm_eq(self, parm, parm2):
-        for atom, atom2 in zip(parm.atoms, parm2.atoms):
-            assert atom.name == atom2.name, 'equal name'
-            assert atom.type == atom2.type, 'equal type'
-            assert atom.mass == atom2.mass, 'equal mass'
-            assert atom.atomic_number == atom2.atomic_number, \
-                'equal atomic_number'
-            assert atom.residue.name == atom2.residue.name, 'residue name'
-            aa_eq(atom.charge, atom2.charge, decimal=4)
+def parm_eq(parm, parm2):
+    for atom, atom2 in zip(parm.atoms, parm2.atoms):
+        assert atom.name == atom2.name, 'equal name'
+        assert atom.type == atom2.type, 'equal type'
+        assert atom.mass == atom2.mass, 'equal mass'
+        assert atom.atomic_number == atom2.atomic_number, 'equal atomic_number'
+        assert atom.residue.name == atom2.residue.name, 'residue name'
+        aa_eq(atom.charge, atom2.charge, decimal=4)
 
-    def test_parmed_converter(self):
-        self.parm_eq(self.parm, to_parmed(self.traj))
-        self.parm_eq(self.parm, self.traj.top.to_parmed())
-
-
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.skipif(pmd is None, reason="Must install ParmEd")
+def test_parmed_converter(traj, parm):
+    parm_eq(parm, to_parmed(traj))
+    parm_eq(parm, traj.top.to_parmed())
