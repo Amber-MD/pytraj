6056fa2830433d56a9a6966170f09c1acf30f421
hainm <hainm.comp@gmail.com>
2025-11-25 14:13:46 -0500

    test

--- tests/test_analysis/test_infrared_spectrum.py
+++ tests/test_analysis/test_infrared_spectrum.py
@@ -1,25 +1,30 @@
 import pytraj as pt
-from pytraj.testing import aa_eq, tempfolder
-from utils import tz2_ortho_trajin, tz2_ortho_top
-
+from pytraj.testing import aa_eq, tempfolder, cpptraj_test_dir
 
 def test_infrared_spectrum():
-    cm = """
-    parm {}
-    trajin {}
-    infrared_spectrum :1-10
-    """.format(tz2_ortho_top, tz2_ortho_trajin)
+    parm_file = f"{cpptraj_test_dir}/Test_systemVF/systemVF.parm7"
+    traj_file = f"{cpptraj_test_dir}/Test_systemVF/systemVF.nc"
 
+    cm = f"""
+    parm {parm_file}
+    trajin {traj_file}
+    infraredspec IR out irspec.dat maxlag 5 tstep 0.1 rawout raw.dat
+    """
+    pt._verbose()
     with tempfolder():
         # Run cpptraj state
         state = pt.datafiles.load_cpptraj_state(cm).run()
         cpptraj_results = state.data[:]
 
         # Run pytraj's infrared_spectrum
-        traj = pt.iterload(tz2_ortho_trajin, tz2_ortho_top)
+        traj = pt.iterload(traj_file, parm_file)
         pytraj_results = pt.infrared_spectrum(
             traj,
-            mask=":1-10",
+            mask="IR",
+            out="irspec.dat",
+            maxlag=5,
+            tstep=0.1,
+            rawout="raw.dat",
         )
 
         # Compare results
