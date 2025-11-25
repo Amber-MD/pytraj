89b83af187bdc4dbe8918f5eabbfa272fc064bbe
hainm <hainm.comp@gmail.com>
2025-11-25 12:17:25 -0500

    add toroidal_diffusion

--- pytraj/all_actions.py
+++ pytraj/all_actions.py
@@ -3276,3 +3276,43 @@ def permute_dihedrals(traj, filename, options=''):
 
     state.data._pop(0)
     state.data._pop(0)
+
+@super_dispatch()
+def toroidal_diffusion(traj=None, mask="", mass=False, out=None, diffout=None, time=1.0, options="", dtype='dataset', top=None, frame_indices=None):
+    """Calculate diffusion using the toroidal-view-preserving scheme.
+
+    Parameters
+    ----------
+    traj : Trajectory-like
+    mask : str, atom mask
+        Mask to select molecules for the calculation.
+    mass : bool, default False
+        Use center of mass if True, else geometric center.
+    out : str, optional
+        Output filename for average diffusion sets.
+    diffout : str, optional
+        Output filename for diffusion results.
+    time : float, default 1.0
+        Time between frames (ps).
+    options : str, optional
+        Additional cpptraj options for future extensibility.
+    dtype : str, default 'dataset'
+        Output data type.
+    top : Topology, optional
+    frame_indices : array-like, optional
+
+    Returns
+    -------
+    DatasetList or ndarray
+    """
+    command = (CommandBuilder()
+               .add("out", out, condition=out is not None)
+               .add("diffout", diffout, condition=diffout is not None)
+               .add("mass", condition=mass)
+               .add("time", str(time), condition=time != 1.0)
+               .add(mask, condition=bool(mask))
+               .add(options, condition=bool(options))
+               .build())
+
+    action_datasets, _ = do_action(traj, command, c_action.Action_ToroidalDiffusion)
+    return get_data_from_dtype(action_datasets, dtype=dtype)
