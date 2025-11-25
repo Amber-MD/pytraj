3e9a74305fd672db0085e81d15acfc4b6c0d9551
hainm <hainm.comp@gmail.com>
2025-11-25 13:34:43 -0500

    test

--- tests/test_analysis/test_ene_decomp.py
+++ tests/test_analysis/test_ene_decomp.py
@@ -1,14 +1,18 @@
 import pytraj as pt
-from pytraj.testing import aa_eq, tempfolder
-from utils import tz2_ortho_trajin, tz2_ortho_top
+from pytraj.testing import aa_eq, tempfolder, cpptraj_test_dir
 
 
 def test_ene_decomp():
-    cm = """
-    parm {}
-    trajin {}
-    ene_decomp :1-10
-    """.format(tz2_ortho_top, tz2_ortho_trajin)
+    # Use cpptraj_test_dir to locate the test files
+    parm_file = f"{cpptraj_test_dir}/Test_Ewald/nacl.box.parm7"
+    traj_file = f"{cpptraj_test_dir}/Test_Ewald/nacl.box.rst7"
+
+    # Test 1: Basic energy decomposition
+    cm = f"""
+    parm {parm_file}
+    trajin {traj_file}
+    enedecomp ATM * out decomp.nacl.box.dat
+    """
 
     with tempfolder():
         # Run cpptraj state
@@ -16,11 +20,56 @@ def test_ene_decomp():
         cpptraj_results = state.data[:]
 
         # Run pytraj's ene_decomp
-        traj = pt.iterload(tz2_ortho_trajin, tz2_ortho_top)
+        traj = pt.iterload(traj_file, parm_file)
         pytraj_results = pt.ene_decomp(
             traj,
-            mask=":1-10",
+            mask="*",
         )
 
         # Compare results
         aa_eq(pytraj_results, cpptraj_results)
+
+    # Test 2: Energy decomposition with savecomponents
+    cm_savecomponents = f"""
+    parm {parm_file}
+    trajin {traj_file}
+    enedecomp ATN * out decomp.components.nacl.box.dat savecomponents
+    """
+
+    with tempfolder():
+        # Run cpptraj state
+        state = pt.datafiles.load_cpptraj_state(cm_savecomponents).run()
+        cpptraj_results_savecomponents = state.data[:]
+
+        # Run pytraj's ene_decomp with savecomponents
+        traj = pt.iterload(traj_file, parm_file)
+        pytraj_results_savecomponents = pt.ene_decomp(
+            traj,
+            mask="*",
+            savecomponents=True,
+        )
+
+        # Compare results
+        aa_eq(pytraj_results_savecomponents, cpptraj_results_savecomponents)
+
+    # Test 3: Energy decomposition with a specific atom mask
+    cm_mask = f"""
+    parm {parm_file}
+    trajin {traj_file}
+    enedecomp ATM @4 out decomp.mask.nacl.box.dat
+    """
+
+    with tempfolder():
+        # Run cpptraj state
+        state = pt.datafiles.load_cpptraj_state(cm_mask).run()
+        cpptraj_results_mask = state.data[:]
+
+        # Run pytraj's ene_decomp with a specific mask
+        traj = pt.iterload(traj_file, parm_file)
+        pytraj_results_mask = pt.ene_decomp(
+            traj,
+            mask="@4",
+        )
+
+        # Compare results
+        aa_eq(pytraj_results_mask, cpptraj_results_mask)
